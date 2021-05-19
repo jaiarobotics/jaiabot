2b8d9a80d8c2106af6846357a88e71a44a4629cd
Toby Schneider <toby@gobysoft.org>
2021-05-19 17:17:06 -0400

    Began simulator development

---
+++ src/bin/simulator/simulator.cpp
@@ -0,0 +1,79 @@
+// Copyright 2021:
+//   JaiaRobotics LLC
+// File authors:
+//   Toby Schneider <toby@gobysoft.org>
+//
+//
+// This file is part of the JaiaBot Hydro Project Binaries
+// ("The Jaia Binaries").
+//
+// The Jaia Binaries are free software: you can redistribute them and/or modify
+// them under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+//
+// The Jaia Binaries are distributed in the hope that they will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with the Jaia Binaries.  If not, see <http://www.gnu.org/licenses/>.
+
+#include <goby/middleware/marshalling/protobuf.h>
+// this space intentionally left blank
+#include <goby/moos/middleware/moos_plugin_translator.h>
+#include <goby/zeromq/application/multi_thread.h>
+
+#include "config.pb.h"
+#include "jaiabot/groups.h"
+#include "jaiabot/messages/example.pb.h"
+
+using goby::glog;
+namespace si = boost::units::si;
+namespace config = jaiabot::config;
+namespace groups = jaiabot::groups;
+namespace zeromq = goby::zeromq;
+namespace middleware = goby::middleware;
+
+namespace jaiabot
+{
+namespace apps
+{
+class SimulatorTranslation : public goby::moos::Translator
+{
+  public:
+    SimulatorTranslation(const goby::apps::moos::protobuf::GobyMOOSGatewayConfig& cfg)
+        : goby::moos::Translator(cfg)
+    {
+        // subscribe to NAV* and create $GPRMC message, etc. Publish it on UDP output
+        // subscribe to goby::middleware::frontseat::groups::desired_course and publish to DESIRED*
+    }
+};
+
+class Simulator : public zeromq::MultiThreadApplication<config::Simulator>
+{
+  public:
+    Simulator();
+};
+
+} // namespace apps
+} // namespace jaiabot
+
+int main(int argc, char* argv[])
+{
+    return goby::run<jaiabot::apps::Simulator>(
+        goby::middleware::ProtobufConfigurator<config::Simulator>(argc, argv));
+}
+
+// Main thread
+jaiabot::apps::Simulator::Simulator()
+{
+    glog.add_group("main", goby::util::Colors::yellow);
+
+    goby::apps::moos::protobuf::GobyMOOSGatewayConfig sim_cfg;
+    *sim_cfg.mutable_app() = cfg().app();
+    *sim_cfg.mutable_interprocess() = cfg().interprocess();
+    *sim_cfg.mutable_moos() = cfg().moos();
+    launch_thread<jaiabot::apps::SimulatorTranslation>(sim_cfg);
+}
