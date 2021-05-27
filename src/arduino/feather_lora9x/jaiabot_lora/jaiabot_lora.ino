603b2bc00a1a2a7b2528f73df4d4e0ad901fb05c
Toby Schneider <toby@gobysoft.org>
2021-05-27 15:22:37 -0400

    Continued work on jaiabot_lora transmitter code

--- src/arduino/feather_lora9x/jaiabot_lora/jaiabot_lora.ino
+++ src/arduino/feather_lora9x/jaiabot_lora/jaiabot_lora.ino
@@ -1,8 +1,9 @@
 #include <SPI.h>
 #include <RH_RF95.h>
+#include <RHDatagram.h>
 #include <pb_decode.h>
 #include <pb_encode.h>
-#include <base64.hpp> 
+#include <base64.hpp>
 #include "jaiabot/messages/nanopb/feather.pb.h"
 
 /* for feather32u4 */
@@ -14,27 +15,45 @@
 
 // Singleton instance of the radio driver
 RH_RF95 rf95(RFM95_CS, RFM95_INT);
+uint8_t this_address = 2;
+RHDatagram rh_manager(rf95, this_address);
 
 // ASCII Serial Protocol
 // Prefix byte + ':' + Data
 // P:[Base64 Encoded jaiabot_protobuf_LoRaMessage Protobuf Data]
 // D:[Debug String message]
 
-// 8 bits -> 6 bits
-#define B64_OVERHEAD 1.333333333
 // prefix + ':'
 #define SERIAL_HEADER_SIZE 2
 // new line
 #define EOL_SIZE 1
 
-constexpr int JB_MAX_SERIAL_LINE_LENGTH = SERIAL_HEADER_SIZE + ceil(jaiabot_protobuf_LoRaMessage_size*B64_OVERHEAD) + EOL_SIZE;
+// 8 bits -> 6 bits
+constexpr int JB_MAX_SERIAL_LINE_LENGTH = SERIAL_HEADER_SIZE + ceil(jaiabot_protobuf_LoRaMessage_size * 3) / 4 + EOL_SIZE;
 char serial_buffer[JB_MAX_SERIAL_LINE_LENGTH] = {0};
 
 jaiabot_protobuf_LoRaMessage outgoing_msg = jaiabot_protobuf_LoRaMessage_init_default;
+jaiabot_protobuf_LoRaMessage incoming_msg = jaiabot_protobuf_LoRaMessage_init_default;
 
 bool data_to_send = false;
 
-void setup() 
+void print_msg(const jaiabot_protobuf_LoRaMessage& msg)
+{
+  Serial.print("src: ");
+  Serial.print(msg.src);
+  Serial.print(", dest: ");
+  Serial.print(msg.dest);
+  Serial.print(", data: ");
+  for (int i = 0, n = msg.data.size; i < n; ++i)
+  {
+    Serial.print(msg.data.bytes[i], HEX);
+    Serial.print(" ");
+  }
+  Serial.println();
+  delay(10);
+}
+
+void setup()
 {
   pinMode(RFM95_RST, OUTPUT);
   digitalWrite(RFM95_RST, HIGH);
@@ -49,7 +68,7 @@ void setup()
   Serial.println("D:JaiaBot LoRa");
 
   jaiabot_protobuf_LoRaMessage lora_msg{};
-  
+
 
   // manual reset
   digitalWrite(RFM95_RST, LOW);
@@ -57,7 +76,7 @@ void setup()
   digitalWrite(RFM95_RST, HIGH);
   delay(10);
 
-  while (!rf95.init()) {
+  while (!rh_manager.init()) {
     Serial.println("D:LoRa radio init failed");
     Serial.println("D:Uncomment '#define SERIAL_DEBUG' in RH_RF95.cpp for detailed debug info");
     while (1);
@@ -70,81 +89,99 @@ void setup()
     while (1);
   }
   Serial.print("D:Set Freq to: "); Serial.println(RF95_FREQ);
-  
+
   // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
 
   // The default transmitter power is 13dBm, using PA_BOOST.
-  // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then 
+  // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then
   // you can set transmitter powers from 5 to 23 dBm:
   rf95.setTxPower(5, false);
+
 }
 
 int16_t packetnum = 0;  // packet counter, we increment per xmission
 
 void lora_tx()
 {
-  Serial.println("D:Transmitting..."); // Send a message to rf95_server
+  if (outgoing_msg.src != this_address)
+  {
+    this_address = outgoing_msg.src;
+    Serial.print("D:Updated this address to ");
+    Serial.println(this_address);
+    delay(10);
+    rh_manager.setThisAddress(this_address);
+
+    delay(10);
+  }
   Serial.println("D:Sending...");
   delay(10);
-  rf95.send((uint8_t *)outgoing_msg.data.bytes, outgoing_msg.data.size);
-
-
-  Serial.println("D:Waiting for packet to complete..."); 
+  rh_manager.sendto((uint8_t *)outgoing_msg.data.bytes, outgoing_msg.data.size, outgoing_msg.dest);
+  Serial.println("D:Waiting for packet to complete...");
   delay(10);
-  rf95.waitPacketSent();
+  if (rh_manager.waitPacketSent(1000 /*ms*/))
+    Serial.println("D:...send complete");
+  else
+    Serial.println("D:...timeout on send");
   data_to_send = false;
+  delay(10);
 }
 
 void lora_rx()
 {
-  uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
-  uint8_t len = sizeof(buf);
+  incoming_msg = jaiabot_protobuf_LoRaMessage_init_default;
+  uint8_t size = sizeof(incoming_msg.data.bytes);
+  uint8_t src = 0;
+  uint8_t dest = 0;
+  uint8_t id = 0;
+  uint8_t flags = 0;
 
-  Serial.println("D:Waiting for reply...");
-  if (rf95.waitAvailableTimeout(1000))
-  { 
-    // Should be a reply message for us now   
-    if (rf95.recv(buf, &len))
-   {
-      Serial.print("D:Got reply: ");
-      Serial.println((char*)buf);
+  while (rh_manager.waitAvailableTimeout(100))
+  {
+    if (rh_manager.recvfrom(incoming_msg.data.bytes, &size, &src, &dest, &id, &flags))
+    {
+      incoming_msg.data.size = size;
+      incoming_msg.src = src;
+      incoming_msg.dest = dest;
+      incoming_msg.id = id;
+      incoming_msg.has_id = true;
+      incoming_msg.flags = flags;
+      incoming_msg.has_flags = true;
+
+      Serial.print("D:incoming - ");
+      print_msg(incoming_msg);
       Serial.print("D:RSSI: ");
-      Serial.println(rf95.lastRssi(), DEC);    
+      Serial.println(rf95.lastRssi(), DEC);
     }
     else
     {
       Serial.println("D:Receive failed");
     }
   }
-  else
-  {
-    Serial.println("D:No reply, is there a listener around?");
-  }
 }
 
 void loop()
-{  
-  if(data_to_send)
-  {
-    lora_tx();
-    lora_rx();
-  }
+{
+  lora_rx();
   
+  if (data_to_send)
+    lora_tx();
+
+
   while (Serial.available() > 0) {
     // read prefix
     auto prefix = Serial.read();
     auto colon = Serial.read();
-    if(colon != ':')
+    if (colon != ':')
     {
-      Serial.println("D:Malformed message, expected colon after prefix");
-      Serial.println(prefix);
+      Serial.print("D:Malformed message, expected colon after prefix ");
+      Serial.print(prefix);
       Serial.println(colon);
     }
     else
     {
       auto bytes_read = Serial.readBytesUntil('\n', serial_buffer, JB_MAX_SERIAL_LINE_LENGTH);
 
-      switch(prefix)
+      switch (prefix)
       {
         // Protobuf message (base 64 encoded)
         case 'P':
@@ -162,17 +199,11 @@ void loop()
           }
           else
           {
-            Serial.print("D:src: ");
-            Serial.print(outgoing_msg.src);
-            Serial.print(", dest: ");
-            Serial.print(outgoing_msg.dest);
-            Serial.print(", data: ");
-            for(int i = 0, n = outgoing_msg.data.size; i < n; ++i)
-              Serial.print(static_cast<char>(outgoing_msg.data.bytes[i]));
-            Serial.println();
+            Serial.print("D:outgoing - ");
+            print_msg(outgoing_msg);
             data_to_send = true;
           }
-          
+
           break;
         // Debug message (string)
         case 'D':
@@ -183,7 +214,7 @@ void loop()
           break;
       }
     }
-  }  
+  }
 }
 
 // from feather.pb.c - would be better to just add the file to the sketch
