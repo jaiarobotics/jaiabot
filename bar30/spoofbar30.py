ee09d1d3b37048e419b5994ec362702179e4f868
Ed Sanville <edsanville@gmail.com>
2021-07-19 19:03:45 -0400

    Added bar30 python scripts

---
+++ bar30/spoofbar30.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+from time import sleep
+from datetime import datetime
+import random
+import sys
+
+targetFile = sys.argv[1]
+delay = 1.0
+
+while True:
+    now = datetime.utcnow()
+    p_mbar = random.uniform(1300, 1400)
+    t_celsius = random.uniform(20, 25)
+
+    line = '%s,%9.2f,%7.2f\n' % (now.strftime('%Y-%m-%dT%H:%M:%SZ'), p_mbar, t_celsius)
+
+    open(targetFile, 'w').write(line)
+
+    sleep(delay)
