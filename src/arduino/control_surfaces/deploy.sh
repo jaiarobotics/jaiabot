0bc312f64ec289d86da91570e2077abb0c1fedbd
Ed Sanville <edsanville@gmail.com>
2021-08-13 20:22:46 +0000

    Send ControlSurfaces message to the Arduino

--- src/arduino/control_surfaces/deploy.sh
+++ src/arduino/control_surfaces/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 arduino-cli compile -b adafruit:avr:feather32u4
-arduino-cli upload -p /dev/ttyACM0 -b adafruit:avr:feather32u4
+arduino-cli upload -v -p /dev/ttyACM0 -b adafruit:avr:feather32u4
