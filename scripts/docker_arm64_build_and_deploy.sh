05c8eacd5632182e0d4381c758d978167487f7fd
Jason Webster <jason@jaia.tech>
2021-09-08 08:46:26 -0400

    Build and Deploy script

---
+++ scripts/docker_arm64_build_and_deploy.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+script_dir=$(dirname $0)
+
+if [ -z "$1" ]
+    then
+        echo "Enter a host to deploy to"
+        exit
+fi
+
+cd ${script_dir}/..
+
+LAST_BUILD_DATE=$(date -r arm64_build +%s)
+ONE_WEEK_LATER=$(expr "$LAST_BUILD_DATE" + 604800)
+
+NOW=$(date +%s)
+
+if [ $NOW -ge $ONE_WEEK_LATER ];
+    then
+        echo "****It's been a while since we updated packages in the container, so let's take care of that now."
+        sudo docker run -v `pwd`:/home/ubuntu/jaiabot -w /home/ubuntu/jaiabot -t gobysoft/jaiabot-ubuntu-arm64:20.04.1 bash -c "apt update && apt upgrade -y && ./scripts/arm64_build.sh"
+    else
+        echo "****Up to date - let's get straight to compiling, shall we?"
+        sudo docker run -v `pwd`:/home/ubuntu/jaiabot -w /home/ubuntu/jaiabot -t gobysoft/jaiabot-ubuntu-arm64:20.04.1 bash -c "./scripts/arm64_build.sh"
+fi  
+
+echo "Time to rsync"
+rsync -aP build/bin build/lib ubuntu@$1:/home/ubuntu/jaiabot/build
