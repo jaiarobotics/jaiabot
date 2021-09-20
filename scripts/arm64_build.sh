07fbda65dffb64825eb5fa939814a876e940f58e
Ed Sanville <edsanville@gmail.com>
2021-09-20 16:59:54 -0400

    pid-control app

--- scripts/arm64_build.sh
+++ scripts/arm64_build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+NPROC=$((`nproc` - 2))
+
 script_dir=$(dirname $0)
 
 mkdir -p ${script_dir}/../arm64_build
@@ -9,4 +11,4 @@ export CC=/usr/bin/clang
 export CXX=/usr/bin/clang++
 
 (set -x; cmake .. -DCMAKE_SYSTEM_NAME=Linux -DCMAKE_SYSTEM_PROCESSOR=aarch64 -DCMAKE_C_FLAGS="-target aarch64-linux-gnu" -DCMAKE_CXX_FLAGS="-target aarch64-linux-gnu")
-(set -x; make -j`nproc`)
+(set -x; make -j${NPROC})
