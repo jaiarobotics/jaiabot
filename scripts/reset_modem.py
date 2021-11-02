963a0c390fdaf790620600e6903ec64d958346c5
Ed Sanville <edsanville@gmail.com>
2021-11-02 18:10:54 -0400

    Allow modem reset script to take device name as command-line parameter

--- scripts/reset_modem.py
+++ scripts/reset_modem.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3
 
 import serial
+import sys
 
-s = serial.Serial('/dev/ttyUSB0', baudrate=9600)
+try:
+  device_name = sys.argv[1]
+except IndexError:
+  device_name = '/dev/ttyUSB0'
+
+s = serial.Serial(device_name, baudrate=9600)
 
 s.write(b'+++')
 s.read_until(b'OK\r')
