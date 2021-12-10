d674271acb07be54476a9672bdfd0789bb40bbdd
Jason Webster <jason@jaia.tech>
2021-12-10 15:09:59 -0500

    added other necessary packages

--- scripts/setup_runtime_tools.sh
+++ scripts/setup_runtime_tools.sh
@@ -1,1 +1,2 @@
-sudo apt install goby3-apps goby3-gui
+sudo apt install goby3-apps goby3-gui goby3-moos parallel moos-ivp-apps moos-ivp-gui libmoos-ivp opencpn
+echo "export PATH=$PATH:$(realpath $PWD/../build/bin)" >> ~/.bashrc
