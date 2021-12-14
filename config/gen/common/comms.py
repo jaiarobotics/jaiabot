0661aaa638c65c8465a6783d0fbe8fb182b997a6
Ed Sanville <edsanville@gmail.com>
2021-12-14 15:25:45 -0500

    Added config directory

---
+++ config/gen/common/comms.py
@@ -0,0 +1,36 @@
+from common import is_simulation, is_runtime
+from common import udp
+
+subnet_mask=0xFF00
+subnet_index={'wifi': 0}
+num_modems_in_subnet=(0xFFFF ^ subnet_mask)+1
+
+# Broadcast is modem id = 0 in Goby, so increment vehicle id by 1 to get base modem id
+def base_modem_id(vehicle_id):
+    return vehicle_id + 1
+
+def wifi_modem_id(vehicle_id):
+    return base_modem_id(vehicle_id) + subnet_index['wifi']*num_modems_in_subnet
+
+def wifi_ip_addr(vehicle_id):
+    if is_simulation():
+        return "127.0.0.1"
+    else:
+        raise Exception("not implemented")
+    
+def wifi_remotes(this_vehicle_id, number_vehicles):
+    first_vehicle_id=0
+    remotes=''
+    for vehicle_id in list(range(first_vehicle_id, number_vehicles+first_vehicle_id+1)):
+        if this_vehicle_id != vehicle_id:
+            remotes+='remote { modem_id: ' + str(base_modem_id(vehicle_id)) + ' ip: "' + wifi_ip_addr(vehicle_id)  + '" port: ' + str(udp.wifi_udp_port(vehicle_id)) + ' } \n'
+    return remotes
+
+
+# first id is hub id
+hub_vehicle_id=0
+
+def wifi_mac_slots(vehicle_id):
+    slots = 'slot { src: ' + str(wifi_modem_id(vehicle_id)) + ' slot_seconds: 1 max_frame_bytes: 128 }\n'
+    return slots
+
---
+++ config/gen/common/vehicle.py
@@ -0,0 +1,35 @@
+from common import is_simulation, is_runtime
+import common.comms
+
+def bot_index_to_vehicle_id(bot_index):
+    return bot_index+common.comms.hub_vehicle_id+1
+
+def simulator_port(vehicle_i):
+    return 55000 + vehicle_id
+
+def moos_port(vehicle_id):
+    return 9000 + vehicle_id
+
+def moos_simulator_port(vehicle_id):
+    return 9100 + vehicle_id
+
+def gpsd_device(vehicle_id):
+    if is_simulation():
+        return "udp://127.0.0.1:" + str(gpsd_simulator_udp_port(vehicle_id))
+    else:
+        # vehicle id to port mapping
+        ports = {
+            0: '/dev/ttyAMA2',
+            1: '/dev/ttyAMA2'
+        }
+        return ports.get(vehicle_id, '/dev/ttyAMA2')
+
+def gpsd_simulator_udp_port(vehicle_id):
+    return 32000 + vehicle_id
+
+def gpsd_port(vehicle_id):
+    if is_simulation():
+        return 32000 + vehicle_id
+    else:
+        default_gpsd_port=2947
+        return default_gpsd_port
