a30239a98fbb48ef144ea015ce4110ce39ddca33
Jason Webster <jason@jaia.tech>
2021-12-16 14:14:54 -0500

    made reset modem more robust

--- scripts/reset_modem.py
+++ scripts/reset_modem.py
@@ -16,4 +16,13 @@ s.read_until(b'OK\r')
 s.write(b'ATAP=0\r')
 s.read_until(b'OK\r')
 
+s.write(b'ATCM=FFFFFFFFFFF7FFFF\r')
+s.read_until(b'OK\r')
+
+s.write(b'ATHP=00\r')
+s.read_until(b'OK\r')
+
+s.write(b'ATID=0007\r')
+s.read_until(b'OK\r')
+
 print('Modem reset to transparent mode.')
