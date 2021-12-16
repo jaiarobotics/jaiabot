eb570dbc0900d0fbd57785a1c0bac9c04307a049
Jason Webster <jason@jaia.tech>
2021-12-16 13:47:27 -0500

    refactored simulation remotes

--- scripts/setup_runtime_tools.sh
+++ scripts/setup_runtime_tools.sh
@@ -1,2 +1,2 @@
-sudo apt install goby3-apps goby3-gui goby3-moos parallel moos-ivp-apps moos-ivp-gui libmoos-ivp opencpn
+sudo apt install goby3-apps goby3-gui goby3-moos parallel moos-ivp-apps moos-ivp-gui libmoos-ivp opencpn i2c-tools
 echo "export PATH=$PATH:$(realpath $PWD/../build/bin)" >> ~/.bashrc
