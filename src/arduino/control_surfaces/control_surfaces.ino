bc740e873037e8be1a630967122ee2cb770a0f21
Jason Webster <jason@jaia.tech>
2022-02-02 21:45:39 -0500

    changed pinout of arduino

--- src/arduino/control_surfaces/control_surfaces.ino
+++ src/arduino/control_surfaces/control_surfaces.ino
@@ -23,10 +23,10 @@ constexpr int BITS_IN_BYTE = 8;
 
 Servo rudder_servo, port_elevator_servo, stbd_elevator_servo, motor_servo;
 
-constexpr int STBD_ELEVATOR_PIN = 7;
-constexpr int PORT_ELEVATOR_PIN = 5;
-constexpr int RUDDER_PIN = 6;
-constexpr int MOTOR_PIN = 8;
+constexpr int STBD_ELEVATOR_PIN = 2;
+constexpr int RUDDER_PIN = 3;
+constexpr int PORT_ELEVATOR_PIN = 4;
+constexpr int MOTOR_PIN = 5;
 
 // The timeout
 unsigned long t_last_command = 0;
