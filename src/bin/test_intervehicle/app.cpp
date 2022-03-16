ae6ad61e6e1ce71ce928a7fb62ff5e89f61b438f
Toby Schneider <toby@gobysoft.org>
2022-03-16 23:07:01 +1100

    Stub application to send command from hub to bot

---
+++ src/bin/test_intervehicle/app.cpp
@@ -0,0 +1,67 @@
+// Copyright 2021:
+//   JaiaRobotics LLC
+// File authors:
+//   Toby Schneider <toby@gobysoft.org>
+//
+//
+// This file is part of the JaiaBot Project Binaries
+// ("The Jaia Binaries").
+//
+// The Jaia Binaries are free software: you can redistribute them and/or modify
+// them under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+//
+// The Jaia Binaries are distributed in the hope that they will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with the Jaia Binaries.  If not, see <http://www.gnu.org/licenses/>.
+
+#include <goby/middleware/marshalling/protobuf.h>
+// this space intentionally left blank
+#include <goby/zeromq/application/single_thread.h>
+
+#include "config.pb.h"
+#include "jaiabot/groups.h"
+#include "jaiabot/messages/jaia_dccl.pb.h"
+
+using goby::glog;
+namespace si = boost::units::si;
+using ApplicationBase = goby::zeromq::SingleThreadApplication<jaiabot::config::SingleThreadPattern>;
+
+namespace jaiabot
+{
+namespace apps
+{
+class SingleThreadPattern : public ApplicationBase
+{
+  public:
+    SingleThreadPattern() : ApplicationBase(1.0 / (10.0 * si::seconds)) {}
+
+  private:
+    void loop() override;
+};
+} // namespace apps
+} // namespace jaiabot
+
+int main(int argc, char* argv[])
+{
+    return goby::run<jaiabot::apps::SingleThreadPattern>(
+        goby::middleware::ProtobufConfigurator<jaiabot::config::SingleThreadPattern>(argc, argv));
+}
+
+void jaiabot::apps::SingleThreadPattern::loop()
+{
+    jaiabot::protobuf::Command command;
+    command.set_bot_id(0);
+    command.set_time_with_units(goby::time::SystemClock::now<goby::time::MicroTime>());
+    command.set_type(jaiabot::protobuf::Command::STOP);
+
+    glog.is_debug2() && glog << group("main") << "Sending command: " << command.ShortDebugString()
+                             << std::endl;
+    intervehicle().publish_dynamic(
+        command, goby::middleware::DynamicGroup(jaiabot::groups::hub_command, command.bot_id()));
+}
