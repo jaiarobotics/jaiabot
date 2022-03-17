88df666865a4b9a53e72e7db46bad6bd0d5814a1
Jason Webster <jason@jaia.tech>
2022-03-17 14:34:15 -0400

    removed old scripts

--- src/web/remote_test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-SERVER=$1
-
-rsync -a --delete --exclude dist --exclude node_modules --exclude package-lock.json ./ ${SERVER}:jaia_web/
-ssh -t optiplex 'cd jaia_web; ./build.sh; cd server; ./app.py'
--- src/web/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-./build.sh
-pushd server
-./app.py -s $1
-popd server
-
