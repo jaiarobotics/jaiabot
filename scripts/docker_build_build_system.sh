c6ce8482d3e10a2368bdeca6c903ba3bb3490b23
Ed Sanville <edsanville@gmail.com>
2022-03-19 17:58:44 -0400

    Don't use cache when building docker build system image

--- scripts/docker_build_build_system.sh
+++ scripts/docker_build_build_system.sh
@@ -5,4 +5,4 @@ set -e
 script_dir=$(dirname $0)
 jaia_root=${script_dir}/..
 
-docker build -t build_system ${jaia_root}/.docker/focal/arm64
+docker build --no-cache -t build_system ${jaia_root}/.docker/focal/arm64
