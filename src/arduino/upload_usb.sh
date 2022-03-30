7cc06c689dc52aa6c526d66677ce2cbdbbeee496
Ed Sanville <edsanville@gmail.com>
2022-03-30 13:58:49 -0400

    Use correct relative path for libraries directory

--- src/arduino/upload_usb.sh
+++ src/arduino/upload_usb.sh
@@ -4,6 +4,6 @@ PORT=/dev/ttyUSB0
 MYFQBN=arduino:avr:nano:cpu=atmega328old
 sketch=$1
 
-arduino-cli compile -v --upload --libraries ../libraries -p ${PORT} -b ${MYFQBN} --output-dir $sketch/build $sketch
+arduino-cli compile -v --upload --libraries libraries -p ${PORT} -b ${MYFQBN} --output-dir $sketch/build $sketch
 
 echo "Done!"
