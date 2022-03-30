d547465dc9c32ebe380d0e5fe5a93a8cc7ac5c98
Ed Sanville <edsanville@gmail.com>
2022-03-30 14:07:29 -0400

    Rsync the build/arm64/include directory Use "--libraries libraries" when compiling Arduino sketches in upload.sh

--- src/arduino/upload.sh
+++ src/arduino/upload.sh
@@ -3,7 +3,7 @@
 MYFQBN=arduino:avr:nano:cpu=atmega328old
 sketch=$1
 
-arduino-cli compile -v --fqbn $MYFQBN --output-dir $sketch/build $sketch
+arduino-cli compile -v --libraries libraries --fqbn $MYFQBN --output-dir $sketch/build $sketch
 
 sudo "/home/ubuntu/.arduino15/packages/arduino/tools/avrdude/6.3.0-arduino17/bin/avrdude" "-C/home/ubuntu/.arduino15/packages/arduino/tools/avrdude/6.3.0-arduino17/etc/avrdude.conf" -C./avrdude.conf -v -patmega328p -cjaiabot "-Uflash:w:${sketch}/build/${sketch}.ino.with_bootloader.hex:i"
 
