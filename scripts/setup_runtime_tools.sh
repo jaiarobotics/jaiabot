aee266e8699d03d27571c2c3831d00c18a3a93b9
Ed Sanville <edsanville@gmail.com>
2022-04-13 11:42:21 -0400

    Alter standard launch to control real-world vehicle

--- scripts/setup_runtime_tools.sh
+++ scripts/setup_runtime_tools.sh
@@ -1,5 +1,5 @@
 echo "Installing apt packages"
-sudo apt install goby3-apps goby3-gui goby3-moos parallel moos-ivp-apps moos-ivp-gui libmoos-ivp opencpn i2c-tools
+sudo apt install goby3-apps goby3-gui goby3-moos parallel moos-ivp-apps moos-ivp-gui libmoos-ivp opencpn i2c-tools libgoby3-moos libgoby3-moos-dev
 echo "Installing pip packages"
 pip install --upgrade pip
 pip install python-dateutil plotly pyQt5 h5py
