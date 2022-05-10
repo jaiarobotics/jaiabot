497b90ce67c64aa1f9ffa0d0c49f0788e576dd30
Ed Sanville <edsanville@gmail.com>
2022-05-10 13:33:30 -0400

    build_messages.sh:   Added "ln -s /usr/include/goby goby" AXUI.jsx:   Deepcopy the hard-coded missions, so we don't add waypoints to them by accident

--- src/web/server/build_messages.sh
+++ src/web/server/build_messages.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+if [ ! -d goby ]; then
+    ln -s /usr/include/goby goby
+fi
+
 protoc -I/usr/local/include/ -Iproto_include/ --python_out=. proto_include/goby/middleware/protobuf/*.proto proto_include/jaiabot/messages/*.proto
 
 echo "✅ Successfully built messages"
