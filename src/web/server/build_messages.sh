2c3b85f3068516e47260862a45fdacbee96f1d3d
Ed Sanville <edsanville@gmail.com>
2022-05-12 17:31:35 -0400

    build_messages.sh:   Fix the softlink to goby proto files

--- src/web/server/build_messages.sh
+++ src/web/server/build_messages.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-if [ ! -d goby ]; then
-    ln -s /usr/include/goby goby
+if [ ! -d proto_include/goby ]; then
+    ln -s /usr/include/goby proto_include/goby
 fi
 
 protoc -I/usr/local/include/ -Iproto_include/ --python_out=. proto_include/goby/middleware/protobuf/*.proto proto_include/jaiabot/messages/*.proto
