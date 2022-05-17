11985cfd8049cbbdf703ef2999ab0b7a75dd9053
Ben <benashlieb@gmail.com>
2022-05-17 11:42:20 -0400

    calibration in .proto

--- scripts/calibration/jaiabot_calibration.py
+++ scripts/calibration/jaiabot_calibration.py
@@ -1,37 +1,48 @@
 #!/usr/bin/env python3
 
+#talks to the serial moniter
 import serial
-import time
-import os
-import json
 
-#arduino = serial.Serial('/dev/cu.usbserial-1430', 19200,timeout = .1)
+import time
+
+#talks to the terminal
+import os
+
+#hels with protobuf processing
+import google.protobuf.json_format
+
+#the actual protobuf format
+import bounds_pb2
+
+#arduino = serial.Serial('/dev/cu.usbserial-143230', 19200,timeout = .1)
 arduino = serial.Serial('/etc/jaiabot/dev/arduino', 19200,timeout = .1)
 
+#reads from the arduino serial moniter
 def rd():
     data = arduino.readline()
     return data
 
+#writes to the serail moniter and send the command - \n is the newline command
 def write(command):
     command = str(command)
-    command = str(command + "\n")
+    command = command+'\n'
     arduino.write(bytes(command, 'utf_8'))
 
+#if you input a value higher than 500 you are leaving the current region and entering the next limit, either center or one fo the extremes
 def wingInsurance(adjustment):
     x = True
-    adjustment = int(adjustment)
     while x is True:
         if adjustment > 500 or adjustment < -500:
             print("this value is too high.")
-            adjustment = input("How many microseconds to adjust by? values between -500 and 500 only. ")
+            adjustment = int(input("How many microseconds to adjust by? values between -500 and 500 only. "))
         else:
-            return str(adjustment)
-
+            return adjustment
+#betcha cant guess what thid doesss
 def inputcommand():
     y = True
     while y is True:
         z = True
-        adjustment = input("How many microseconds to adjust by? Values between -500 and 500 only. ")
+        adjustment = int(input("How many microseconds to adjust by? Values between -500 and 500 only. "))
         adjustment = wingInsurance(adjustment)
         write(adjustment)
         while z is True:
@@ -42,6 +53,7 @@ def inputcommand():
                 z = False
                 y = False
             elif confirmation == "NO":
+                write("N")
                 z = False
             else:
                 print("Please enter yes or no")
@@ -64,14 +76,14 @@ def calibrateWingSurface():
 
                 write("X")
 
-                #formatting the string to be a nice json file
+                #formatting the string to be a nice readable string
                 output = rd()
                 output = str(output)
                 output = output.replace("b","")
                 output = output.replace("'","")
                 output = output.replace("/","")
                 output = output.replace("r","")
-                output = output.replace("\","")
+                output = output.replace("\\","")
                 ourput = output.replace("n","")
                 output = output.split()
 
@@ -86,6 +98,8 @@ def calibrateWingSurface():
                           "lower":output[1],
                           "center":output[2]}
 
+                return limits
+
                 #releases from the answer loop and ends calibration
                 letitend = False
                 y = False
@@ -99,8 +113,9 @@ def calibrateWingSurface():
             else:
                 print("Please enter Yes or No")
                 
-    return limits
-
+    
+#makes sure the motor isnt sent such a low value it inevitably crashes. the minimum speed may be tweaked if the tails proved a significant amount of resistance,
+#25 was determined without a tail mount
 def motorInsurance(adjustment, currentSpeed):
     x = True
     while x is True:
@@ -153,13 +168,14 @@ def inputMotorCommand():
                 z = False
             else:
                 print("Please enter yes or no")
-                
+    #reformats the value output from the arduino serial moniter to be nice and pretty          
     value = str(value)
     value = value.replace("b","")
     value = value.replace("'","")
     print(value)
     return value
 
+#is basically the same thing as above, but with slightly different prompts.
 def inputHaltCommand():
     y = True
     currentSpeed = 0
@@ -202,6 +218,7 @@ def inputHaltCommand():
             else:
                 print("Please enter yes or no")
 
+    #reformats the value output from the arduino serial moniter to be nice and pretty
     value = str(value)
     value = value.replace("b","")
     value = value.replace("'","")
@@ -209,43 +226,53 @@ def inputHaltCommand():
     return value
 
 def calibrateMotor():
-    print("calibrating forward start")
-    startup = inputMotorCommand()
-    print("calibrating reverse start")
-    startdown = inputMotorCommand()
-    print("calibrating forward halt")
-    haltup = inputHaltCommand()
-    print("calibrating reverse halt")
-    haltdown = inputHaltCommand()
+    y = True
 
-    letitend = True
-    while letitend == True:
-        finish = input("Would you like to stop calibrating the motor? Yes/No ")
-        finish = finish.upper()
-        if finish == "YES":
-            motorParameters = {"Forward Start":startup,
-                               "Reverse Start":startdown,
-                               "Forward Halt":haltup,
-                               "Reverse Halt":haltdown}   
+    while y is True:
 
-            #releases from the answer loop and ends calibration
-            letitend = False
-            y = False
-                
-        elif finish == "NO":
-            #releases from the answer loop and restarts calibration
-            letitend = False
-            write("Z")
-        else:
-            print("Please enter Yes or No")
-    return motorParameters
+        #calibrates the forward, backward, forward halt, and backward halt limitations
+        print(rd())
+        print("calibrating forward start")
+        startup = inputMotorCommand()
+        print("calibrating reverse start")
+        startdown = inputMotorCommand()
+        print("calibrating forward halt")
+        haltup = inputHaltCommand()
+        print("calibrating reverse halt")
+        haltdown = inputHaltCommand()
 
+        #my scripts are like meseeks, not made for this world. existance is pain, and they would like to be free
+        letitend = True
+        while letitend == True:
+            finish = input("Would you like to stop calibrating the motor? Yes/No ")
+            finish = finish.upper()
+            if finish == "YES":
+                write("C")
+                motorParameters = {"forwardStart":startup,
+                                   "reverseStart":startdown,
+                                   "forwardHalt":haltup,
+                                   "reverseHalt":haltdown}
+                return motorParameters
+
+                #releases from the answer loop and ends calibration
+                letitend = False
+                y = False
+                    
+            elif finish == "NO":
+                #releases from the answer loop and restarts calibration
+                letitend = False
+                write("Z")
+            else:
+                print("Please enter Yes or No")
+
+#starts with the assumption that someone may have accidentally typed ./JAIABOT_calibration.py
 misclick = True
 while misclick == True:
-    permission = input("would you like to calibrate the jaibot? Yes/No ")
+    permission = input("would you like to calibrate the jaiabot? Yes/No ")
     permission = permission.upper()
     if permission == "YES":
         os.system('cd ~/jaiabot/src/arduino/; ./upload_usb_old.sh JAIABOT_calibration_arduino/')
+
         #ask the user to calibrate bounds
         StrbBounds = calibrateWingSurface()
         PortBounds = calibrateWingSurface()
@@ -254,19 +281,22 @@ while misclick == True:
         MotorBounds = calibrateMotor()
         print("Motor Calibration Complete")
 
-        finale = {"Strb Bounds":StrbBounds,
-                  "Port Bounds":PortBounds,
-                  "Rudder Bounds":RudderBounds,
-                  "Motor Bounds":MotorBounds}
+        #compiles the overall ouputs into a dictionary to be converted to a proto format
+        finale = {"strb":StrbBounds,
+                  "port":PortBounds,
+                  "rudder":RudderBounds,
+                  "motor":MotorBounds}
 
+        #displays the results of the user's hard work and writes it intoa proto format
         print(finale)
-        overall = json.dumps(finale)
+        overall = google.protobuf.json_format.ParseDict(finale, bounds_pb2.Bounds())
 
-        #write bounds to a file
-        records = open("bounds.json", "w")
-        records.write(overall)
-        os.system('sudo mv bounds.json /etc/jaiabot/')
-       
+        #write bounds to a .pb.cfg file and moves it to /etc/jaiabot
+        records = open("output_bounds.pb.cfg", "w")
+        records.write(str(overall))
+        os.system('sudo mv output_bounds.pb.cfg /etc/jaiabot/')
+
+        #desperately begs the user to upload control_surfaces and release it from its task
         please = True
         while please == True:
             upload = input("Would you like to upload control_surfaces? Yes/No ")
@@ -276,7 +306,7 @@ while misclick == True:
                 os.system("cd ~/jaiabot/src/arduino/; ./upload_usb_old.sh control_surfaces")
                 please = False
             elif upload == "NO":
-                #leaves calibration running
+                #leaves JAIABOT_calibration running
                 please = False
                 calibrating = False
             else:
