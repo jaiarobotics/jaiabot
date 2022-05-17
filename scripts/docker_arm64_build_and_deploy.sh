ab5e06132355ff8323350b483d07f2a2ddcd737b
Toby Schneider <toby@gobysoft.org>
2022-05-17 15:06:53 +1000

    Add arduino uploading to docker script

--- scripts/docker_arm64_build_and_deploy.sh
+++ scripts/docker_arm64_build_and_deploy.sh
@@ -17,14 +17,21 @@ echo "🟢 Building jaiabot apps"
 docker run -v `pwd`:/home/ubuntu/jaiabot -w /home/ubuntu/jaiabot -t build_system bash -c "./scripts/arm64_build.sh"
 
 if [ -z "$1" ]
-    then
-        echo "             -----------"
-        echo "Not Deploying as you didn't specify any targets"
-    else
-        for var in "$@"
-	    do
-    		echo "🟢 Uploading to "$var
-		    rsync -zaP --delete --force --relative ./src/python ./build/arm64/bin ./build/arm64/lib ./build/arm64/include ./build/arm64/share/ ./config ./scripts ./src/arduino ubuntu@"$var":/home/ubuntu/jaiabot/
-	    done
+then
+    echo "             -----------"
+    echo "Not Deploying as you didn't specify any targets"
+else
+    for var in "$@"
+    do
+    	echo "🟢 Uploading to "$var
+	rsync -zaP --delete --force --relative ./src/python ./build/arm64/bin ./build/arm64/lib ./build/arm64/include ./build/arm64/share/ ./config ./scripts ./src/arduino ubuntu@"$var":/home/ubuntu/jaiabot/
+
+        if [ ! -z "$jaiabot_arduino_type" ]; then
+            echo "🟢 Loading arduino type $jaiabot_arduino_type on "$var
+            ssh ubuntu@"$var" "/home/ubuntu/jaiabot/build/arm64/share/jaiabot/arduino/jaiabot_runtime/$jaiabot_arduino_type/upload.sh"
+        fi
+    done
+
+    
 fi
 
