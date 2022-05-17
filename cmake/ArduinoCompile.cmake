d3eecc613e3c7b38da9c981090efdaeb003d3981
Toby Schneider <toby@gobysoft.org>
2022-05-17 10:28:48 +1000

    Fix paths in installed upload.sh scripts

--- cmake/ArduinoCompile.cmake
+++ cmake/ArduinoCompile.cmake
@@ -2,6 +2,7 @@ set(ARDUINO_SOURCE_DIR ${project_SRC_DIR}/arduino)
 set(ARDUINO_BINARY_DIR ${project_SHARE_DIR}/jaiabot/arduino)
 set(ARDUINO_SERIAL_PORT /etc/jaiabot/dev/arduino CACHE STRING 
   "Serial port for Arduino flashing" )
+set(ARDUINO_INSTALL_DIR ${CMAKE_INSTALL_PREFIX}/share/jaiabot/arduino)
 
 # cache sketches so we can make a dependency chain to ensure we don't run
 # arduino-cli in parallel (it can't handle that...)
@@ -20,8 +21,12 @@ function(arduino_sketch sketchname nickname fqbn avrdude_programmer baudrate)
   string(SUBSTRING "${fqbn}" ${cpu_pos} -1 cpu)
   
   set(outdir ${ARDUINO_BINARY_DIR}/${sketchname}/${nickname})
-  set(hex_output_with_bootloader ${outdir}/${sketchname}.ino.with_bootloader.hex)
-  set(hex_output ${outdir}/${sketchname}.ino.hex)
+
+  set(hex_name_with_bootloader ${sketchname}.ino.with_bootloader.hex)
+  set(hex_name ${sketchname}.ino.hex)
+  
+  set(hex_output_with_bootloader ${outdir}/${hex_name_with_bootloader})
+  set(hex_output ${outdir}/${hex_name})
 
   add_custom_command(OUTPUT ${ARDUINO_SOURCE_DIR}/${sketchname}/jaiabot
     DEPENDS ${project_INC_DIR}/jaiabot
@@ -47,19 +52,21 @@ function(arduino_sketch sketchname nickname fqbn avrdude_programmer baudrate)
   if(baudrate)
     set(baudrate_flag "-b${baudrate}")
     set(serial_port_flag "-P${ARDUINO_SERIAL_PORT}")
-    set(memop_flag "-Uflash:w:${hex_output}:i")
+    set(hex ${hex_name})
   else()
-    set(memop_flag "-Uflash:w:${hex_output_with_bootloader}:i")
+    set(hex ${hex_name_with_bootloader})
   endif()
 
-  # write an upload script for convenience of repeated or docker-based uploads
-  file(WRITE ${outdir}/tmp/upload.sh
-    "#!/bin/bash" "\n"
-    "exec avrdude -patmega328p -C+${ARDUINO_BINARY_DIR}/avrdude.conf -c${avrdude_programmer} ${memop_flag} ${baudrate_flag} ${serial_port_flag} -v -D -V" "\n"
-    )
-  # work around lack of CHMOD command in this version of CMake
-  file (COPY ${outdir}/tmp/upload.sh DESTINATION ${outdir} FILE_PERMISSIONS OWNER_EXECUTE OWNER_WRITE OWNER_READ)
-  file (REMOVE_RECURSE ${outdir}/tmp)
+  
+  # use an upload script for convenience of repeated or docker-based uploads
+  set(sharedir ${ARDUINO_BINARY_DIR})
+  configure_file(${ARDUINO_SOURCE_DIR}/upload.sh.in ${outdir}/upload.sh @ONLY)
+  
+  set(sharedir ${ARDUINO_INSTALL_DIR})
+  configure_file(${ARDUINO_SOURCE_DIR}/upload.sh.in ${outdir}/upload_installed.sh @ONLY)
+  install(PROGRAMS ${outdir}/upload_installed.sh DESTINATION ${ARDUINO_INSTALL_DIR}/${sketchname}/${nickname} RENAME upload.sh)
+  install(FILES ${hex_output} ${hex_output_with_bootloader} DESTINATION ${ARDUINO_INSTALL_DIR}/${sketchname}/${nickname})
+
   
   add_custom_target(arduino_upload_${nickname}
     DEPENDS arduino_compile_${nickname}
