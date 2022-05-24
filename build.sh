22f291004dec38b6510a57529c7e83bc8b34c2db
Toby Schneider <toby@gobysoft.org>
2022-05-24 21:26:36 +1000

    Install clang-format hooks in build.sh and docker_arm64_build_and_deploy.sh if not already installed

--- build.sh
+++ build.sh
@@ -8,13 +8,18 @@ if [ -z "${JAIABOT_MAKE_FLAGS}" ]; then
     JAIABOT_MAKE_FLAGS=
 fi
 
+script_dir=$(dirname $0)
+
 ARCH=$(dpkg --print-architecture)
 
 set -e -u
-mkdir -p build/${ARCH}
+mkdir -p ${script_dir}/build/${ARCH}
+
+# install clang-format hook if not installed
+[ ! -e ${script_dir}/.git/hooks/pre-commit ] && ${script_dir}/scripts/clang-format-hooks/git-pre-commit-format install
 
 echo "Configuring..."
-cd build/${ARCH}
+cd ${script_dir}/build/${ARCH}
 (set -x; cmake ../.. ${JAIABOT_CMAKE_FLAGS})
 echo "Building..."
 (set -x; cmake --build . -- -j`nproc` ${JAIABOT_MAKE_FLAGS} $@)
