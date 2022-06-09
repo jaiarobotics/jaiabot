8c9da93e77e7f723370404112e73d1b36be4f963
Ed Sanville <edsanville@gmail.com>
2022-06-08 23:19:48 -0400

    log_analysis:   Added Arduino motor microseconds to analyze.py   Added get_logs.py script to autoconvert and download h5 files from a period of time in the past

--- scripts/log_analysis/push_logs.sh
+++ scripts/log_analysis/push_logs.sh
@@ -3,24 +3,7 @@
 
 DEST_HOSTNAME=optiplex
 
-set -e
-
-# Convert goby files to h5 files
-GOBY_FILES=`find /var/log/jaiabot/ -name '*.goby'`
-
-for GOBY_FILE in ${GOBY_FILES}
-do
-    H5_FILE=${GOBY_FILE%.goby}.h5
-    if [ ! -e ${H5_FILE} ]; then
-        set -x
-        goby_log_tool --input_file ${GOBY_FILE} --output_file ${H5_FILE} --format HDF5
-        set +x
-    else
-        echo ${H5_FILE} exists.
-    fi
-done
-
-set -x
+set -ex
 
 ssh ${DEST_HOSTNAME} mkdir -p '${HOME}'/jaia-logs/`hostname`
 rsync -zaP /var/log/jaiabot/ ${DEST_HOSTNAME}:jaia-logs/`hostname`
