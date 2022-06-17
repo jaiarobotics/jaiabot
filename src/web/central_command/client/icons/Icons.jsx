9a39715f380f5599e4f86b4b723c2407e5d32892
Ed Sanville <edsanville@gmail.com>
2022-06-16 21:13:19 -0400

    Central Command:   Change start, waypoint, stop, and bot icons

--- src/web/central_command/client/icons/Icons.jsx
+++ src/web/central_command/client/icons/Icons.jsx
@@ -1,28 +1,37 @@
 import { Icon as OlIcon, Style as OlStyle } from 'ol/style'
 import startUnselected from "../icons/start-unselected.svg"
+export {startUnselected}
 export const startUnselectedStyle = new OlStyle({ image: new OlIcon({ src: startUnselected }) })
 
 import startSelected from "../icons/start-selected.svg"
+export {startSelected}
 export const startSelectedStyle = new OlStyle({ image: new OlIcon({ src: startSelected }) })
 
 import waypointUnselected from "../icons/waypoint-unselected.svg"
+export {waypointUnselected}
 export const waypointUnselectedStyle = new OlStyle({ image: new OlIcon({ src: waypointUnselected }) })
 
 import waypointSelected from "../icons/waypoint-selected.svg"
+export {waypointSelected}
 export const waypointSelectedStyle = new OlStyle({ image: new OlIcon({ src: waypointSelected }) })
 
 import stopUnselected from "../icons/stop-unselected.svg"
+export {stopUnselected}
 export const stopUnselectedStyle = new OlStyle({ image: new OlIcon({ src: stopUnselected }) })
 
 import stopSelected from "../icons/stop-selected.svg"
+export {stopSelected}
 export const stopSelectedStyle = new OlStyle({ image: new OlIcon({ src: stopSelected }) })
 
 import botSelected from "../icons/bot-selected.svg"
+export {botSelected}
 export const botSelectedStyle = new OlStyle({ image: new OlIcon({ src: botSelected }) })
 
 import botUnselected from "../icons/bot-unselected.svg"
+export {botUnselected}
 export const botUnselectedStyle = new OlStyle({ image: new OlIcon({ src: botUnselected }) })
 
 import botRemoteControl from "../icons/bot-remoteControl.svg"
+export {botRemoteControl}
 export const botRemoteControlStyle = new OlStyle({ image: new OlIcon({ src: botRemoteControl }) })
 
