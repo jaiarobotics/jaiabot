c847e5fb06a886f5d22c745b896cfbe3f3c56c68
Ed Sanville <edsanville@gmail.com>
2022-07-08 13:24:35 -0400

    Start jaiaplot API project

---
+++ src/plot/app.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+
+import argparse
+from flask import Flask, send_from_directory, Response, request
+import json
+import logging
+
+import jaialogs
+
+# Arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("-p", dest='port', type=int, default=40010, help="Port to serve the jaiaplot interface")
+parser.add_argument("-l", dest='logLevel', type=str, default='WARNING', help="Logging level (CRITICAL, ERROR, WARNING, INFO, DEBUG)")
+args = parser.parse_args()
+
+# Setup logging module
+logLevel = getattr(logging, args.logLevel.upper())
+logging.basicConfig(level=logLevel)
+logging.getLogger('werkzeug').setLevel('WARN')
+
+app = Flask(__name__)
+
+####### Static files
+root = './client'
+
+@app.route('/<path>', methods=['GET'])
+def getStaticFile(path):
+    return send_from_directory(root, path)
+
+@app.route('/', methods=['GET'])
+def getRoot():
+    return getStaticFile('index.html')
+
+####### API endpoints
+
+def JSONResponse(obj):
+    return Response(json.dumps(obj), mimetype='application/json')
+
+
+@app.route('/logs', methods=['GET'])
+def getLogs():
+    return JSONResponse(jaialogs.get_logs())
+
+@app.route('/paths', methods=['GET'])
+def getFields():
+    log_names = request.args.get('log')
+    root_path = request.args.get('root_path')
+    return JSONResponse(jaialogs.get_fields(log_names, root_path))
+
+@app.route('/series', methods=['GET'])
+def getSeries():
+    log_names = request.args.get('log')
+    series_names = request.args.get('path')
+    return JSONResponse(jaialogs.get_series(log_names, series_names))
+
+@app.route('/map', methods=['GET'])
+def getMap():
+    log_names = request.args.get('log')
+    return JSONResponse(jaialogs.get_map(log_names))
+
+
+if __name__ == '__main__':
+    logging.warning(f'Serving on port {args.port}')
+    app.run(host='0.0.0.0', port=args.port, debug=False)
---
+++ src/plot/jaialogs.py
@@ -0,0 +1,238 @@
+from dataclasses import dataclass, field
+from email.policy import default
+import glob
+import h5py
+import logging
+import json
+import re
+import math
+import copy
+
+INT32_MAX = (2 << 30) - 1
+UINT32_MAX = (2 << 31) - 1
+
+LOG_DIR='logs'
+
+# Utility functions
+
+def get_root_item_path(path, root_item=''):
+    '''Get the path to a root_item associated with that path'''
+    components = path.split('/')
+    components = components[:2] + [root_item]
+    return '/'.join(components)
+
+
+def h5_get_series(dataset):
+    '''Get a JSON-serializable representation of an h5 dataset'''
+    raw = list(dataset)
+    dtype = dataset.dtype
+
+    def from_float(x):
+        x = float(x)
+        if math.isnan(x):
+            return None
+        return x
+
+    def from_int32(x):
+        if x == INT32_MAX:
+            return None
+        return int(x)
+
+    def from_uint32(x):
+        if x == UINT32_MAX:
+            return None
+        return int(x)
+
+    dtype_proc = {
+        'f': from_float,
+        'i': from_int32,
+        'u': from_uint32
+    }
+
+    proc = dtype_proc[dtype.kind]
+
+    return [proc(x) for x in raw]
+
+
+def h5_get_hovertext(dataset):
+    '''Get the hovertext for an h5 dataset'''
+
+    # Get the enum value names
+    try:
+        enum_names = dataset.attrs['enum_names']
+        enum_values = dataset.attrs['enum_values']
+        enum_dict = { int(enum_values[index]): enum_names[index] for index in range(0, len(enum_values))}
+        return enum_dict
+
+    except KeyError:
+        return None
+
+
+# Path descriptions
+
+try:
+    path_descriptions = json.load(open('jaia_paths.json'))
+except FileNotFoundError:
+    path_descriptions = {}
+
+
+def jaia_get_description(path):
+    for description in path_descriptions:
+        if 'path' in description:
+            if description['path'] == path:
+                return description
+        
+        if 'path_regex' in description:
+            if re.match(description['path_regex'], path):
+                return description
+    
+    return None
+
+def get_title_from_path(path):
+    components = path.split('/')
+    components = components[1:]
+    components[0] = components[0].split('.')[-1]
+    return '/'.join(components)
+
+# Data fetch functions
+
+def get_logs():
+    '''Get list of available logs'''
+    return glob.glob(LOG_DIR + '/*.h5')
+
+
+def get_fields(log_names, root_path='/'):
+    '''Get a list of the fields below a root path in a set of logs'''
+    fields = set()
+
+    if log_names is None:
+        return []
+
+    if root_path is None or root_path == '':
+        root_path = '/'
+    else:
+        # h5py doesn't like initial slashes, unless it's the root path
+        root_path = root_path.lstrip('/')
+
+    log_names = log_names.split(',')
+    
+    for log_name in log_names:
+        h5_file = h5py.File(log_name)
+        try:
+            fields = fields.union(h5_file[root_path].keys())
+        except AttributeError:
+            # If this path is a dataset, it has no "keys()"
+            pass
+    
+    series = list(fields)
+    series.sort()
+
+    return series
+
+
+@dataclass
+class Series:
+    utime: list
+    y_values: list
+    hovertext: dict
+
+    def __init__(self, log=None, path=None, scheme=1, invalid_values=set()) -> None:
+        self.utime = []
+        self.y_values = []
+        self.hovertext = {}
+
+        if log:
+            series = zip(h5_get_series(log[get_root_item_path(path, '_utime_')]), h5_get_series(log[get_root_item_path(path, '_scheme_')]), h5_get_series(log[path]))
+            series = filter(lambda pt: pt[1] == scheme and pt[2] not in invalid_values, series)
+            self.utime, schemes, self.y_values = zip(*series)
+
+            self.hovertext = h5_get_hovertext(log[path]) or {}
+
+    def __add__(self, other_series: 'Series'):
+        r = copy.copy(self)
+        r.utime += list(other_series.utime)
+        r.y_values += list(other_series.y_values)
+        r.hovertext.update(other_series.hovertext)
+        return r
+
+    def sort(self):
+        self.utime, self.y_values = zip(*sorted(zip(self.utime, self.y_values)))
+
+
+def get_series(log_names, paths):
+    '''Get a series'''
+    if log_names is None or paths is None:
+        return []
+
+    series_list = []
+
+    log_names = log_names.split(',')
+    paths = [path.lstrip('/') for path in paths.split(',')]
+
+    # Open all our logs
+    logs = [h5py.File(log_name) for log_name in log_names]
+
+    # Get the series from the logs
+    for path in paths:
+        series_description = jaia_get_description(path) or {}
+        invalid_values = set(series_description.get('invalid_values', []))
+
+        series = Series()
+
+        for log in logs:
+            try:
+                series += Series(log=log, path=path, scheme=1, invalid_values=invalid_values)
+            except KeyError as e:
+                logging.warn(e)
+                continue
+
+        series.sort()
+
+        title = get_title_from_path(path)
+        y_axis_title = title
+        units = series_description.get('units')
+
+        if units is not None:
+            y_axis_title += f'\n({units})'
+
+        series_list.append({
+            'path': path,
+            'title': title,
+            'y_axis_title': y_axis_title,
+            '_utime_': series.utime,
+            'series_y': series.y_values,
+            'hovertext': series.hovertext
+        })
+
+    
+    return series_list
+
+
+def get_map(log_names):
+    if log_names is None:
+        return []
+
+    log_names = log_names.split(',')
+
+    # Open all our logs
+    logs = [h5py.File(log_name) for log_name in log_names]
+
+    TPV_lat_path = 'goby::middleware::groups::gpsd::tpv/goby.middleware.protobuf.gpsd.TimePositionVelocity/location/lat'
+    TPV_lon_path = 'goby::middleware::groups::gpsd::tpv/goby.middleware.protobuf.gpsd.TimePositionVelocity/location/lon'
+
+    lat_series = Series()
+    lon_series = Series()
+
+    for log in logs:
+        lat_series += Series(log=log, path=TPV_lat_path, invalid_values=[0])
+        lon_series += Series(log=log, path=TPV_lon_path, invalid_values=[0])
+
+    lat_series.sort()
+    lon_series.sort()
+
+    points = []
+
+    for i, lat in enumerate(lat_series.y_values):
+        points.append([lat_series.utime[i], lat_series.y_values[i], lon_series.y_values[i]])
+
+    return points
