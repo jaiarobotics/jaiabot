6bcf78af907b491a541b133f58860e771353ed63
Toby Schneider <toby@gobysoft.org>
2022-07-20 12:58:30 +1000

    Add jaiabot extensions to coroner messages, enumerated list of desired health messages, and began implementing the coroner reports as enums

--- src/bin/health/app.cpp
+++ src/bin/health/app.cpp
@@ -36,6 +36,19 @@ namespace jaiabot
 {
 namespace apps
 {
+std::map<std::string, jaiabot::protobuf::Error> create_process_to_not_responding_error_map()
+{
+#define MAKE_ENTRY(APP_UCASE)                            \
+    {                                                    \
+        boost::to_lower_copy(std::string(#APP_UCASE)),   \
+            protobuf::ERROR__NOT_RESPONDING__##APP_UCASE \
+    }
+    // only explicitly list external apps; apps built in this repo are added via -DJAIABOT_HEALTH_PROCESS_MAP_ENTRIES
+    return {MAKE_ENTRY(GOBYD),       MAKE_ENTRY(GOBY_LIAISON), MAKE_ENTRY(GOBY_GPS),
+            MAKE_ENTRY(GOBY_LOGGER), MAKE_ENTRY(GOBY_CORONER), JAIABOT_HEALTH_PROCESS_MAP_ENTRIES};
+#undef MAKE_ENTRY
+}
+
 class Health : public ApplicationBase
 {
   public:
@@ -43,10 +56,15 @@ class Health : public ApplicationBase
 
   private:
     void loop() override;
+    void health(goby::middleware::protobuf::ThreadHealth& health) override;
+
     void restart_services() { system("systemctl restart jaiabot"); }
+    void process_coroner_report(const goby::middleware::protobuf::VehicleHealth& vehicle_health);
 
   private:
     goby::time::SteadyClock::time_point next_check_time_;
+    goby::middleware::protobuf::ThreadHealth last_health_;
+    const std::map<std::string, jaiabot::protobuf::Error> process_to_not_responding_error_;
 };
 } // namespace apps
 } // namespace jaiabot
@@ -61,7 +79,8 @@ jaiabot::apps::Health::Health()
     : ApplicationBase(1.0 * boost::units::si::hertz),
       next_check_time_(goby::time::SteadyClock::now() +
                        goby::time::convert_duration<goby::time::SteadyClock::duration>(
-                           cfg().auto_restart_init_grace_period_with_units()))
+                           cfg().auto_restart_init_grace_period_with_units())),
+      process_to_not_responding_error_(create_process_to_not_responding_error_map())
 {
     // handle restart/reboot/shutdown commands since we run this app as root
     interprocess().subscribe<jaiabot::groups::powerstate_command>(
@@ -92,13 +111,43 @@ jaiabot::apps::Health::Health()
 
     interprocess().subscribe<goby::middleware::groups::health_report>(
         [this](const goby::middleware::protobuf::VehicleHealth& vehicle_health) {
-            if (vehicle_health.state() != goby::middleware::protobuf::HEALTH__FAILED)
-            {
-                // increase next check time every time we get an report where it's not FAILED
-                next_check_time_ += goby::time::convert_duration<goby::time::SteadyClock::duration>(
-                    cfg().auto_restart_timeout_with_units());
-            }
+            process_coroner_report(vehicle_health);
         });
+}
+
+void jaiabot::apps::Health::process_coroner_report(
+    const goby::middleware::protobuf::VehicleHealth& vehicle_health)
+{
+    if (vehicle_health.state() != goby::middleware::protobuf::HEALTH__FAILED)
+    {
+        // increase next check time every time we get an report where it's not FAILED
+        next_check_time_ += goby::time::convert_duration<goby::time::SteadyClock::duration>(
+            cfg().auto_restart_timeout_with_units());
+    }
+
+    last_health_.Clear();
+    auto& jaiabot_health = *last_health_.MutableExtension(jaiabot::protobuf::jaiabot_thread);
+
+    for (const auto& proc : vehicle_health.process())
+    {
+        if (proc.main().has_error() &&
+            proc.main().error() == goby::middleware::protobuf::ERROR__PROCESS_DIED)
+        {
+            auto it = process_to_not_responding_error_.find(proc.main().name());
+            if (it != process_to_not_responding_error_.end())
+            {
+                jaiabot_health.add_error(it->second);
+            }
+            else
+            {
+                glog.is_warn() &&
+                    glog << "App: " << proc.main().name()
+                         << " is not responding but has not been mapped to an ERROR enumeration"
+                         << std::endl;
+                jaiabot_health.add_error(protobuf::ERROR__NOT_RESPONDING__UNKNOWN_APP);
+            }
+        }
+    }
 }
 
 void jaiabot::apps::Health::loop()
@@ -114,4 +163,13 @@ void jaiabot::apps::Health::loop()
             restart_services();
         }
     }
+}
+
+void jaiabot::apps::Health::health(goby::middleware::protobuf::ThreadHealth& health)
+{
+    health = last_health_;
+    health.set_name(this->app_name());
+
+    // track
+    health.set_state(goby::middleware::protobuf::HEALTH__OK);
 }
