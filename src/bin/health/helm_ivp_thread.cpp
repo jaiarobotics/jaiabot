cd6628d18b97b7324c26c12b7f55c0a75e87d026
Matthew Ferro <matt.ferro@jaia.tech>
2022-07-26 16:29:25 -0400

    Adding copyright to helm_ivp_thread.cpp

--- src/bin/health/helm_ivp_thread.cpp
+++ src/bin/health/helm_ivp_thread.cpp
@@ -1,3 +1,24 @@
+// Copyright 2021:
+//   JaiaRobotics LLC
+// File authors:
+//   Matt Ferro <matt.ferro@jaia.tech>
+//
+//
+// This file is part of the JaiaBot Project Binaries
+// ("The Jaia Binaries").
+//
+// The Jaia Binaries are free software: you can redistribute them and/or modify
+// them under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+//
+// The Jaia Binaries are distributed in the hope that they will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with the Jaia Binaries.  If not, see <http://www.gnu.org/licenses/>.
 
 #include <boost/units/io.hpp>
 
