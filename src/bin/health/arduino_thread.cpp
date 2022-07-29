8e29e502c1718d44f0091685e40654a96c307d65
Matthew Ferro <matt.ferro@jaia.tech>
2022-07-29 17:15:33 -0400

    Add dccl units

--- src/bin/health/arduino_thread.cpp
+++ src/bin/health/arduino_thread.cpp
@@ -31,14 +31,14 @@ using goby::glog;
 
 jaiabot::apps::ArduinoStatusThread::ArduinoStatusThread(
     const jaiabot::config::ArduinoStatusConfig& cfg)
-    : HealthMonitorThread(cfg, "arduino_status", 30.0 / 60.0 * boost::units::si::hertz)
+    : HealthMonitorThread(cfg, "arduino_status", 4.0 / 60.0 * boost::units::si::hertz)
 {
     interprocess().subscribe<jaiabot::groups::arduino>(
         [this](const jaiabot::protobuf::ArduinoResponse& arduino_response) {
             last_arduino_report_time_ = goby::time::SteadyClock::now();
             arduino_is_responding_ = true;
             status_.set_code(arduino_response.code());
-            status_.set_thermocouple_temperature_c(arduino_response.thermocouple_temperature_c());
+            status_.set_thermocouple_temperature(arduino_response.thermocouple_temperature_c());
             status_.set_vcccurrent(arduino_response.vcccurrent());
             status_.set_vccvoltage(arduino_response.vccvoltage());
             status_.set_vvcurrent(arduino_response.vvcurrent());
