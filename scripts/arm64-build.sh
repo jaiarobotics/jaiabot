f0b845ea94fb91b0825b4d9d4c2fec533d682631
Edward Sanville <edsanville@gmail.com>
2022-08-02 02:18:03 -0400

    Add optional JAIA_BUILD_NPROC to control the number of processes that the build scripts use (#106)
    * Adding script to build and upload arduino
    
    * Updating comments
    
    * Added an optional JAIA_BUILD_NPROC environment variable, to control the number of parallel processes
    
    * Use JAIA_BUILD_NPROC when using the Arduino deploy script
    
    Co-authored-by: Matthew Ferro <matt.ferro@jaia.tech>
    Co-authored-by: Toby Schneider <tsaubergine@users.noreply.github.com>

--- scripts/arm64-build.sh
+++ scripts/arm64-build.sh
@@ -11,6 +11,13 @@ cd ${script_dir}/../build/arm64
 export CC=/usr/bin/clang
 export CXX=/usr/bin/clang++
 
+# Allow user to set nproc for their system, if desired
+if [ -z "${JAIA_BUILD_NPROC}" ]; then
+    JAIA_BUILD_NPROC=`nproc`
+fi
+
+echo "Building with ${JAIA_BUILD_NPROC} parallel processes..."
+
 (set -x; cmake ../.. -DCMAKE_SYSTEM_NAME=Linux -DCMAKE_SYSTEM_PROCESSOR=aarch64 -DCMAKE_C_FLAGS="-target aarch64-linux-gnu" -DCMAKE_CXX_FLAGS="-target aarch64-linux-gnu")
-(set -x; make -j`nproc`)
+(set -x; time make -j${JAIA_BUILD_NPROC})
 (set -x; chmod -R ugo+r *)
