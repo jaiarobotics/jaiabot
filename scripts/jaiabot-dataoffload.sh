9ed466edc3b4aa6471017f7e270d0f235d740831
Toby Schneider <toby@gobysoft.org>
2022-08-09 14:10:17 +1000

    Add timeout to rsync

--- scripts/jaiabot-dataoffload.sh
+++ scripts/jaiabot-dataoffload.sh
@@ -3,14 +3,17 @@
 set -e
 
 log_dir="$1"
-
 if [[ -z "${log_dir}" || ! -e "${log_dir}" ]]; then
     echo "Must specify valid log directory as first command line parameter"
     exit 1;
 fi
 
-rsync -aP --exclude='*.txt*' --exclude="*latest.goby" ${log_dir}/ jaia@hub0:/var/log/jaiabot/bot_offload
+set -u
 
+
+rsync -aP --timeout=15 --exclude='*.txt*' --exclude="*latest.goby" ${log_dir}/ jaia@hub0:/var/log/jaiabot/bot_offload
+
+echo "Removing old log files..."
 # compress debug logs, omitting goby_intervehicle_subscriptions_bot.pb.txt
 find ${log_dir} -type f -name '*[0-9].txt' -execdir xz -- {} \;
 
