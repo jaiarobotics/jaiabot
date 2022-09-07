b52902173de6170b6ca5622611db972a010347e1
Toby Schneider <tsaubergine@users.noreply.github.com>
2022-09-08 02:59:08 +1000

    Allow EvActivate from Failed (#188)
    Co-authored-by: Jason Webster <81530374+jason-jaia@users.noreply.github.com>
    Co-authored-by: ferrom-jaia <109534520+ferrom-jaia@users.noreply.github.com>

--- src/bin/mission_manager/machine.h
+++ src/bin/mission_manager/machine.h
@@ -367,6 +367,12 @@ struct Failed : boost::statechart::state<Failed, PreDeployment>,
     using StateBase = boost::statechart::state<Failed, PreDeployment>;
     Failed(typename StateBase::my_context c) : StateBase(c) {}
     ~Failed() {}
+
+    // allow Activate from Failed in case an error resolves itself
+    // while the vehicle is powered on (e.g. GPS fix after several minutes).
+    // If Activate is sent and the vehicle still has an error,
+    // SelfTest will simply fail again and we'll end up back here in Failed (as desired)
+    using reactions = boost::mpl::list<boost::statechart::transition<EvActivate, SelfTest>>;
 };
 
 struct WaitForMissionPlan
