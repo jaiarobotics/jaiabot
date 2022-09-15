881a80b6d183b02e3929087f4f0393efb172a9a5
Jason Webster <jason@jaia.tech>
2022-09-15 17:28:12 -0400

    added screen

--- scripts/setup-tools-runtime.sh
+++ scripts/setup-tools-runtime.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Installing apt packages"
-sudo apt-get install goby3-apps goby3-gui goby3-moos parallel moos-ivp-apps moos-ivp-gui libmoos-ivp opencpn i2c-tools libgoby3-moos libgoby3-moos-dev python3-pip libxcb-xinerama0 ntp
+sudo apt-get install goby3-apps goby3-gui goby3-moos parallel moos-ivp-apps moos-ivp-gui libmoos-ivp opencpn i2c-tools libgoby3-moos libgoby3-moos-dev python3-pip libxcb-xinerama0 ntp screen
 echo "Installing pip packages"
 pip install --upgrade pip
 pip install python-dateutil plotly pyQt5 h5py geopandas matplotlib
