9472cefe3710350ab76993b1c0f408d5223b1930
oceanzus <case@oceanz.org>
2022-10-02 20:54:28 -0400

    Adds working first version.

--- src/web/central_command/client/components/MissionSettings.jsx
+++ src/web/central_command/client/components/MissionSettings.jsx
@@ -13,12 +13,15 @@ export class MissionSettingsPanel extends React.Component {
 
         this.state = {
             goal: props.goal,
-            mission_params: props.mission_params
+            mission_params: props.mission_params,
+            bot_list: props.bot_list
         }
 
         this.onClose = props.onClose
         this.onChange = props.onChange
         this.onMissionApply = props.onMissionApply
+        this.onMissionChangeEditMode = props.onMissionChangeEditMode
+        this.onMissionChangeBotList = props.onMissionChangeBotList
     }
 
     componentDidUpdate() {
@@ -30,6 +33,7 @@ export class MissionSettingsPanel extends React.Component {
 
         let taskOptionsPanel
         let taskType = this.state.goal.task?.type
+        let missionType = this.state.mission_params?.mission_type
 
         switch (taskType) {
             case 'DIVE':
@@ -43,10 +47,60 @@ export class MissionSettingsPanel extends React.Component {
                 break;
         }
 
+        let botListPanel
+
         return (
             <div className="MissionSettingsPanel">
-                Mission Settings
+                Mission Settings<hr/>
                 <div>
+                    <div>
+                        <table>
+                            <tbody>
+                            <tr hidden>
+                                <td>Bot Selection List:</td>
+                                <td>
+                                    <select multiple name="mission_bot_selection" id="mission-bot-selection" onChange={evt => self.changeMissionBotSelection() }>
+                                        <option value="0">Bot 0</option>
+                                        <option value="1">Bot 1</option>
+                                        <option value="2">Bot 2</option>
+                                        <option value="3">Bot 3</option>
+                                    </select>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Mission Edit Mode:</td>
+                                <td>
+                                    <select name="mission_type" id="mission-type" defaultValue={missionType ?? "editing"} onChange={evt => self.changeMissionEditMode(evt.target.value) }>
+                                        <option value="editing">Editing</option>
+                                        <option value="polygon-grid">Polygon</option>
+                                        <option value="lines">Lines</option>
+                                    </select>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Bot Count</td>
+                                <td><input type="number" className="NumberInput" name="num_bots" defaultValue={this.state.mission_params.num_bots} onChange={this.changeMissionParameter.bind(this)} /></td>
+                            </tr>
+                            <tr>
+                                <td>Goals per Bot</td>
+                                <td><input type="number" className="NumberInput" name="num_goals" defaultValue={this.state.mission_params.num_goals} onChange={this.changeMissionParameter.bind(this)} /></td>
+                            </tr>
+                            <tr>
+                                <td>Mission Spacing</td>
+                                <td><input type="number" className="NumberInput" name="spacing" defaultValue={this.state.mission_params.spacing} onChange={this.changeMissionParameter.bind(this)} /> m</td>
+                            </tr>
+                            <tr>
+                                <td>Mission Orientation</td>
+                                <td><input id='missionOrientation' type="number" className="NumberInput" name="orientation" defaultValue={this.state.mission_params.orientation} /> deg</td>
+                            </tr>
+                            <tr hidden>
+                                <td>Use Max Line Length</td>
+                                <td><input type="checkbox" className="RadioInput" name="use_max_length" defaultValue={this.state.mission_params.use_max_length} onChange={this.changeMissionParameter.bind(this)} /></td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <hr/>
                     Task
                     <select name="GoalType" id="GoalType" onChange={evt => self.changeTaskType(evt.target.value) } defaultValue={taskType ?? "NONE"}>
                         <option value="NONE">None</option>
@@ -54,31 +108,30 @@ export class MissionSettingsPanel extends React.Component {
                         <option value="SURFACE_DRIFT">Surface Drift</option>
                         <option value="STATION_KEEP">Station Keep</option>
                     </select>
-                    <div>
-                        <table>
-                            <tbody>
-                            <tr>
-                                <td>Mission Spacing</td>
-                                <td><input type="number" className="NumberInput" name="spacing" defaultValue={this.state.mission_params.spacing} onChange={this.changeMissionParameter.bind(this)} /> m</td>
-                            </tr>
-                            <tr>
-                                <td>Mission Orientation</td>
-                                <td><input type="number" className="NumberInput" name="orientation" defaultValue={this.state.mission_params.orientation} onChange={this.changeMissionParameter.bind(this)} /> deg</td>
-                            </tr>
-                            </tbody>
-                        </table>
-                    </div>
 
                     { taskOptionsPanel }
 
+                    <hr/>
                     <div className='HorizontalFlexbox'>
                         <button onClick={this.closeClicked.bind(this)}>Close</button>
                         <button onClick={this.applyMissionClicked.bind(this)}>Apply</button>
                     </div>
 
+                    <hr/>
+                    Survey Stats
                     <div id="surveyPolygonResults">
-                        <div id="surveyPolygonResultArea"></div>
-                        <div id="surveyPolygonResultPerimeter"></div>
+                        <table>
+                            <tbody>
+                            <tr className="missionStats">
+                                <td>Area (km^2): </td>
+                                <td><div id="surveyPolygonResultArea"></div></td>
+                            </tr>
+                            <tr className="missionStats">
+                                <td>Perimeter (km): </td>
+                                <td><div id="surveyPolygonResultPerimeter"></div></td>
+                            </tr>
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
@@ -99,7 +152,7 @@ export class MissionSettingsPanel extends React.Component {
     changeTaskType(taskType) {
         let {goal} = this.state
 
-        if (taskType == goal.task?.type) {
+        if (taskType === goal.task?.type) {
             return
         }
 
@@ -205,6 +258,50 @@ export class MissionSettingsPanel extends React.Component {
 
     applyMissionClicked() {
         this.onMissionApply?.()
+    }
+
+    changeMissionBotSelection() {
+        const selected = document.querySelectorAll('#mission-bot-selection option:checked');
+        const missionBots = Array.from(selected).map(el => el.value);
+        // let missionBots = document.getElementById('mission-bot-selection').val();
+        // console.log(missionBots);
+        let {mission_params} = this.state;
+        mission_params.selected_bots = missionBots;
+        this.setState({mission_params});
+        this.onMissionChangeBotList?.()
+    }
+
+    changeMissionEditMode(missionEditMode) {
+        // console.log(missionEditMode);
+        let {mission_params} = this.state;
+
+        if (missionEditMode === mission_params?.mission_type) {
+            return
+        }
+
+        switch(missionEditMode) {
+            case 'polygon-grid':
+                mission_params.mission_type = missionEditMode
+                break;
+            case 'lines':
+                mission_params.mission_type = missionEditMode
+                break;
+            case 'editing':
+                mission_params.mission_type = missionEditMode
+                break;
+            default:
+                mission_params.mission_type = 'editing'
+                break;
+        }
+
+        this.setState({mission_params});
+
+        this.onMissionChangeEditMode?.()
+
+    }
+
+    applyMissionEditMode() {
+        this.onMissionChangeEditMode?.()
     }
 
     applyClicked() {
