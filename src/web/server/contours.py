949d30154958315f58bc2f9d9e74c10a4d0b0ae8
Edward Sanville <edsanville@gmail.com>
2022-11-05 16:09:00 -0400

    Added depth-contours endpoint to the JDV API

--- src/web/server/contours.py
+++ src/web/server/contours.py
@@ -173,12 +173,31 @@ def getContourGeoJSON(meshPoints, contourCount=10):
     return geojson(linestrings)
 
 
+def taskPacketsToContours(taskPackets):
+    """Return a GeoJSON containing contours for a set of task packets
+
+    Args:
+        taskPackets (iterable): An iterable of task packets as represented by dicts
+
+    Returns:
+        dict: a GeoJSON dictionary
+    """
+    meshPoints = []
+    for taskPacket in taskPackets:
+        if 'dive' in taskPacket:
+            dive_packet = taskPacket['dive']
+            meshPoints.append([dive_packet['start_location']['lon'], dive_packet['start_location']['lat'], dive_packet['depth_achieved']])
+
+    return getContourSegmentsForMeshPoints(meshPoints)
+
+
 if __name__ == '__main__':
     meshPoints = [
         [10, 10, 1],
-        [15, 10, 2],
+        [15, 15, 2],
         [20, 10, 2],
         [17, 10, 2]
     ]
 
     json.dump(getContourGeoJSON(meshPoints), open(os.path.expanduser('~/test.json'), 'w'))
+
