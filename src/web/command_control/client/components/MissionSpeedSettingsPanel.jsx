c4260efa7be52d4e1bf4be4f54605e96558f4be7
ferrom-jaia <109534520+ferrom-jaia@users.noreply.github.com>
2022-11-09 14:30:47 -0500

    Bug/speed protection (#272)
    * Using a slider to adjust speeds for mission, added comment for increasing speed, and a confirm popup if we do increase speed past max
    
    * Changing rpm -> throttle in pid control, added m/s to mission speeds, protected simulated heading error
    
    * Fixed lagging issue with sliders and updated comments
    
    Co-authored-by: Jason Webster <81530374+jason-jaia@users.noreply.github.com>

--- src/web/command_control/client/components/MissionSpeedSettingsPanel.jsx
+++ src/web/command_control/client/components/MissionSpeedSettingsPanel.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Settings } from './Settings.jsx'
+import Slider from '@mui/material/Slider';
+import Typography from '@mui/material/Typography';
 
 let SPEED_SETTING_KEY = 'mission.plan.speeds'
 
@@ -9,34 +11,124 @@ export default class MissionSpeedSettingsPanel extends React.Component {
         super(props)
 
         const SPEED_DEFAULTS = {
-            transit: 5.0,
-            stationkeep_outer: 2.0
+            transit: 2.0,
+            stationkeep_outer: 1.5
         }
 
         this.speeds = Settings.read(SPEED_SETTING_KEY) ?? SPEED_DEFAULTS
+
+        this.state = {
+            go_over_max_value: false,
+            safe_speed_watch: 3,
+            speeds: {
+                transit: this.speeds.transit,
+                stationkeep_outer: this.speeds.stationkeep_outer
+            },
+            speed_max: 3 
+        }
     }
 
     render() {
         return (
             <div className='panel'>
-                Speeds
+                Mission Speeds
                 <table>
                     <tbody>
-                    <tr>
+                        <tr>
                             <td>
-                                Transit
+                                <Typography>
+                                    Transit
+                                </Typography>        
                             </td>
                             <td>
-                                <input type="text" defaultValue={this.speeds.transit} onChange={(evt) => { this.speeds.transit = evt.target.value; Settings.write(SPEED_SETTING_KEY, this.speeds) }} />
+                                {/****** Changing the max value may result in bot hardware failure ****** 
+                                  ****** Please do not increase the max value unless you know the   ****** 
+                                  ****** consequences                                               ******/}
+                                <Slider
+                                    sx={{ width: 175, backgroundColor: 'white', color: 'black'}}
+                                    aria-label="Transit"
+                                    defaultValue={this.speeds.transit}
+                                    valueLabelDisplay="auto"
+                                    step={0.5}
+                                    marks
+                                    min={0}
+                                    max={this.state.speed_max}
+                                    onChange={
+                                        (evt) => 
+                                        {
+                                            this.state.speeds.transit = evt.target.value; 
+
+                                            if(this.state.speed_max <= this.state.safe_speed_watch || this.state.go_over_max_value)
+                                            {
+                                                //console.log("Less than max: " + evt.target.value);
+                                            } else if(confirm("Are you sure you'd like to run above the max speed level for the bot? (This may result in hardware failure)"))
+                                            {
+                                                this.setState({ go_over_max_value: true });
+                                                //console.log("more than max: " + evt.target.value);
+                                            } else
+                                            {
+                                                this.state.speeds.transit = this.state.safe_speed_watch; 
+                                                //console.log("did not confirm: " + this.state.safe_speed_watch);
+                                            }
+
+                                            Settings.write(SPEED_SETTING_KEY, this.state.speeds)
+                                        }
+                                    }
+                                />
+                            </td>
+                            <td>
+                                <Typography sx={{ width: 50 }}>
+                                    {this.state.speeds.transit} m/s
+                                </Typography>
                             </td>
                         </tr>
                         <tr>
                             <td>
-                                Station Keep
+                                <Typography>
+                                    Station Keep
+                                </Typography>
                             </td>
                             <td>
-                                <input type="text" defaultValue={this.speeds.stationkeep_outer} onChange={(evt) => { this.speeds.stationkeep_outer = evt.target.value; Settings.write(SPEED_SETTING_KEY, this.speeds) }} />
+                                {/****** Changing the max value may result in bot hardware failure ****** 
+                                  ****** Please do not increase the max value unless you know the   ****** 
+                                  ****** consequences                                               ******/}
+                                <Slider
+                                    sx={{ width: 175, backgroundColor: 'white', color: 'black'}}
+                                    aria-label="Station Keep"
+                                    defaultValue={this.speeds.stationkeep_outer}
+                                    valueLabelDisplay="auto"
+                                    step={0.5}
+                                    marks
+                                    min={0}
+                                    max={this.state.speed_max}
+                                    onChange={
+                                        (evt) => 
+                                        { 
+                                            this.state.speeds.stationkeep_outer = evt.target.value; 
+
+                                            if(this.state.speed_max <= this.state.safe_speed_watch || this.state.go_over_max_value)
+                                            {
+                                                //console.log("Less than max: " + evt.target.value);
+                                            } else if(confirm("Are you sure you'd like to run above the max speed level for the bot? (This may result in hardware failure)"))
+                                            {
+                                                this.setState({ go_over_max_value: true });
+                                                //console.log("more than max: " + evt.target.value);
+                                            } else
+                                            {
+                                                this.state.speeds.stationkeep_outer = this.state.safe_speed_watch; 
+                                                //console.log("did not confirm: " + this.state.safe_speed_watch);
+                                            }
+
+                                            Settings.write(SPEED_SETTING_KEY, this.state.speeds)
+                                        }
+                                    }
+                                />
                             </td>
+                            <td>
+                                <Typography sx={{ width: 50 }}>
+                                    {this.state.speeds.stationkeep_outer} m/s
+                                </Typography>
+                            </td>  
                         </tr>
                     </tbody>
                 </table>
