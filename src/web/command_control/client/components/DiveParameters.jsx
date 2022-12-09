ecdd94f24eae3d86dee2889068c52b52fd19a8ba
Edward Sanville <edsanville@gmail.com>
2022-12-09 09:12:38 -0500

    Remember dive/drift parameters after editing goals (#305)
    Co-authored-by: ferrom-jaia <109534520+ferrom-jaia@users.noreply.github.com>

--- src/web/command_control/client/components/DiveParameters.jsx
+++ src/web/command_control/client/components/DiveParameters.jsx
@@ -6,26 +6,6 @@
 import React from 'react'
 import { Settings } from './Settings'
 
-export var currentDiveParameters = Settings.read('diveParameters') || {
-    maxDepth: 1,
-    depthInterval: 1,
-    holdTime: 0
-}
-
-export var currentDriftParameters = Settings.read('driftParameters') || {
-    driftTime: 0
-}
-
-function setDiveParameter(parameterName, event) {
-    currentDiveParameters[parameterName] = Number(event.target.value)
-    Settings.write('diveParameters', currentDiveParameters)
-}
-
-function setDriftParameter(parameterName, event) {
-    currentDriftParameters[parameterName] = Number(event.target.value)
-    Settings.write('driftParameters', currentDriftParameters)
-}
-
 export function panel() {
     return (
         <div className="panel">
@@ -34,19 +14,19 @@ export function panel() {
                 <tbody>
                     <tr key="max_depth">
                         <td>Max Depth</td>
-                        <td><input type="text" id="dive_max_depth" defaultValue={currentDiveParameters.maxDepth} onChange={setDiveParameter.bind(null, 'maxDepth')} /></td>
+                        <td><input type="text" id="dive_max_depth" defaultValue={Settings.diveMaxDepth.get()} onChange={(evt) => { Settings.diveMaxDepth.set(Number(evt.target.value)) }} /></td>
                     </tr>
                     <tr key="dive_depth_interval">
                         <td>Depth Interval</td>
-                        <td><input type="text" id="dive_depth_interval" defaultValue={currentDiveParameters.depthInterval} onChange={setDiveParameter.bind(null, 'depthInterval')} /></td>
+                        <td><input type="text" id="dive_depth_interval" defaultValue={Settings.diveDepthInterval.get()} onChange={(evt) => { Settings.diveDepthInterval.set(Number(evt.target.value)) }} /></td>
                     </tr>
                     <tr key="hold_time">
                         <td>Hold Time</td>
-                        <td><input type="text" id="dive_hold_time" defaultValue={currentDiveParameters.holdTime} onChange={setDiveParameter.bind(null, 'holdTime')} /></td>
+                        <td><input type="text" id="dive_hold_time" defaultValue={Settings.diveHoldTime.get()} onChange={(evt) => { Settings.diveHoldTime.set(Number(evt.target.value)) }} /></td>
                     </tr>
                     <tr key="drift_time">
                         <td>Drift Time</td>
-                        <td><input type="text" id="drift_time" defaultValue={currentDriftParameters.driftTime} onChange={setDriftParameter.bind(null, 'driftTime')} /></td>
+                        <td><input type="text" id="drift_time" defaultValue={Settings.driftTime.get()} onChange={(evt) => { Settings.driftTime.set(Number(evt.target.value)) }} /></td>
                     </tr>
                 </tbody>
             </table>
