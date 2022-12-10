db974fe4963ee79a96ecee5718fb6f89b3e81916
Ed Sanville <edsanville@gmail.com>
2022-12-10 10:35:14 -0500

    LogSelector -> TypeScript

--- src/plot/client/src/LogSelector.tsx
+++ src/plot/client/src/LogSelector.tsx
@@ -1,6 +1,6 @@
-import React from "react"
+import React, { ReactElement } from "react"
 
-function duration_string_from_seconds(duration_seconds) {
+function duration_string_from_seconds(duration_seconds: number) {
     var components = []
 
     if (duration_seconds >= 3600) {
@@ -22,14 +22,14 @@ function duration_string_from_seconds(duration_seconds) {
     return components.join(', ')
 }
 
-function date_string_from_microseconds(timestamp_microseconds) {
+function date_string_from_microseconds(timestamp_microseconds: number) {
     return new Date(timestamp_microseconds * 1e3).toLocaleString([], {
         dateStyle : 'medium',
         timeStyle : 'short',
     })
 }
 
-function save(key, value) {
+function save(key: string, value: string) {
     if (value != null) {
         localStorage.setItem(key, value)
     }
@@ -38,10 +38,37 @@ function save(key, value) {
     }
 }
 
+interface Log {
+    filename: string
+    fleet: string
+    bot: string
+    timestamp: number
+    duration: number
+}
+
+type LogDict = {[key: string]: {[key: string]: {[key: string]: Log}}}
+
+interface LogSelectorProps {
+    logs: Array<Log>
+    didSelectLogs: (logs: Array<string>) => undefined
+}
+
+interface LogSelectorState {
+    log_dict: LogDict
+    fleet: string
+    bot: string
+    fromDate: string
+    toDate: string
+    selectedLogs: Set<Log>
+}
+
 // Dropdown menu showing all of the available logs to choose from
 export default class LogSelector extends React.Component {
 
-    constructor(props) {
+    props: LogSelectorProps
+    state: LogSelectorState
+
+    constructor(props: LogSelectorProps) {
         super(props)
 
         this.state = {
@@ -136,7 +163,7 @@ export default class LogSelector extends React.Component {
         )
     }
 
-    didToggleLog(log) {
+    didToggleLog(log: Log) {
         var selectedLogs = this.state.selectedLogs
 
         if (selectedLogs.has(log)) {
@@ -155,10 +182,10 @@ export default class LogSelector extends React.Component {
         this.setState({selectedLogs})
     }
 
-    getFilteredLogs() {
+    getFilteredLogs(): Array<Log> {
         const { fromDate, toDate, log_dict } = this.state
 
-        function stringToTimestamp(str) {
+        function stringToTimestamp(str: string) {
             if (str == null) return null
             const d = new Date(str)
             const t = d.getTime()
@@ -173,7 +200,7 @@ export default class LogSelector extends React.Component {
             to_timestamp = to_timestamp + 24 * 60 * 60
         }
 
-        var log_array = []
+        var log_array: Array<Log> = []
 
         for (const fleet in log_dict) {
             if (this.state.fleet != null && this.state.fleet != fleet) continue;
@@ -201,21 +228,22 @@ export default class LogSelector extends React.Component {
         return log_array
     }
 
-    static getDerivedStateFromProps(props) {
+    static getDerivedStateFromProps(props: LogSelectorProps): object {
         let log_dict = LogSelector.log_dict(props.logs)
-        var state = {
+
+        var stateUpdate: {[key: string]: any} = {
             log_dict: log_dict
         }
 
         if (Object.keys(log_dict).length == 1) {
-            state.fleet = Object.keys(log_dict)[0]
+            stateUpdate.fleet = Object.keys(log_dict)[0]
         }
 
-        return state
+        return stateUpdate
     }
 
-    static log_dict(logs) {
-        var log_dict = {}
+    static log_dict(logs: Array<Log>) {
+        var log_dict: LogDict = {}
 
         for (let log of logs) {
             if (!(log.fleet in log_dict)) {
@@ -234,7 +262,7 @@ export default class LogSelector extends React.Component {
         return log_dict
     }
 
-    dict_options(dict) {
+    dict_options(dict: {[key: string]: any}): Array<ReactElement> {
         let names = Object.keys(dict)
         names.sort()
 
@@ -253,14 +281,15 @@ export default class LogSelector extends React.Component {
         return this.dict_options(this.state.log_dict)
     }
 
-    did_select_fleet(evt) {
+    did_select_fleet(evt: Event) {
         var fleet = this.state.fleet
+        let target = evt.target as HTMLSelectElement
 
-        if (evt.target.selectedIndex == 0) {
+        if (target.selectedIndex == 0) {
             fleet = null
         }
         else {
-            fleet = evt.target.value
+            fleet = target.value
         }
         this.setState({fleet})
         save("fleet", fleet)
@@ -278,14 +307,15 @@ export default class LogSelector extends React.Component {
         }
     }
 
-    did_select_bot(evt) {
+    did_select_bot(evt: Event) {
         var bot = this.state.bot
+        let target = evt.target as HTMLSelectElement
 
-        if (evt.target.selectedIndex == 0) {
+        if (target.selectedIndex == 0) {
             bot = null
         }
         else {
-            bot = evt.target.value
+            bot = target.value
         }
         this.setState({bot})
         save("bot", bot)
@@ -293,14 +323,18 @@ export default class LogSelector extends React.Component {
         this.clearLogs()
     }
 
-    fromDateChanged(evt) {
-        const fromDate = evt.target.value
+    fromDateChanged(evt: Event) {
+        let target = evt.target as HTMLSelectElement
+
+        const fromDate = target.value
         this.setState({fromDate})
         save("fromDate", fromDate)
     }
 
-    toDateChanged(evt) {
-        const toDate = evt.target.value
+    toDateChanged(evt: Event) {
+        let target = evt.target as HTMLSelectElement
+
+        const toDate = target.value
         this.setState({toDate})
         save("toDate", toDate)
     }
