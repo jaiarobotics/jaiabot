27623a1b8730a70246c47b38ce038d7cacf99a4f
ferrom-jaia <109534520+ferrom-jaia@users.noreply.github.com>
2023-01-25 22:56:39 -0500

    bug/build-failed-during-nvm-install (#347)

--- src/jdv/client/build.sh
+++ src/jdv/client/build.sh
@@ -8,21 +8,6 @@ pushd "$DIR"
 output_path='dist'
 [[ ! -z "$1" ]] && output_path="$1"
 
-if [ ! -d "${HOME}/.nvm" ]
-then
-	echo "nvm not installed! Installing..."; 
-  curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
-
-  export NVM_DIR="$HOME/.nvm"
-  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
-  nvm install v18.12.1
-  nvm use v18.12.1
-fi
-
-npm install i -g --no-audit webpack webpack-cli
-
 if [[ "$DIR/package.json" -nt "$DIR/node_modules" ]]
 then
 	echo "Installing dependencies"
