783bb9228d40375cd4ab215b5c03296e086d5d11
Matthew Ferro <matt.ferro@jaia.tech>
2023-02-27 13:15:53 -0500

    Changed corrected heading to corrected orientation in imu python driver and also added comments and changed config names for imu detection

--- src/python/adafruit_BNO055/jaiabot_imu.py
+++ src/python/adafruit_BNO055/jaiabot_imu.py
@@ -168,12 +168,12 @@ def do_port_loop():
         gravity = data['gravity']
         calibration_status = data['calibration_status'] # 1 is calibrated, 0 is not
         bot_rolled = int(abs(euler.roll) > 90) # Did we roll over?
-        corrected_heading = euler.heading + 90
+        corrected_orientation = euler.heading + 90 # correct orientation since we have a 90 degree offset
 
         try:
             line = '%s,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%d,%d,%d,%d,%d\n' % \
                 (now.strftime('%Y-%m-%dT%H:%M:%SZ'), 
-                corrected_heading, euler.pitch, euler.roll,
+                corrected_orientation, euler.pitch, euler.roll,
                 linear_acceleration[0], linear_acceleration[2], linear_acceleration[1],
                 gravity[0], gravity[2], gravity[1],
                 calibration_status[0], calibration_status[1], calibration_status[2], calibration_status[3],
