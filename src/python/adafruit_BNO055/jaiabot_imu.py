3a2cea0c7c1a067ff147f13ddac6e7741bdfe5e2
Edward Sanville <edsanville@gmail.com>
2023-02-27 21:52:40 -0500

    Quaternion and Vector3 classes

--- src/python/adafruit_BNO055/jaiabot_imu.py
+++ src/python/adafruit_BNO055/jaiabot_imu.py
@@ -37,28 +37,95 @@ class Orientation:
     roll: float
 
 
-def quaternion_to_euler_angles(q: tuple):
-    DEG = pi / 180
+@dataclass
+class Vector3:
+    x: float
+    y: float
+    z: float
 
-    # roll (x-axis rotation)
-    sinr_cosp = 2 * (q[0] * q[1] + q[2] * q[3])
-    cosr_cosp = 1 - 2 * (q[1] * q[1] + q[2] * q[2])
-    roll = atan2(sinr_cosp, cosr_cosp)
 
-    # pitch (y-axis rotation)
-    sinp = sqrt(1 + 2 * (q[0] * q[2] - q[1] * q[3]))
-    cosp = sqrt(1 - 2 * (q[0] * q[2] - q[1] * q[3]))
-    pitch = -2 * atan2(sinp, cosp) + pi / 2
+    def dot(self, other):
+        return self.x * other.x + self.y * other.y + self.z * other.z
 
-    # yaw (z-axis rotation)
-    siny_cosp = 2 * (q[0] * q[3] + q[1] * q[2])
-    cosy_cosp = 1 - 2 * (q[2] * q[2] + q[3] * q[3])
-    yaw = -atan2(siny_cosp, cosy_cosp)
 
-    if yaw < 0:
-        yaw += (2 * pi)
+    def cross(self, other):
+        return Vector3(
+            self.y * other.z - self.z * other.y,
+            -(self.x * other.z - self.z * other.x),
+            self.x * other.y - self.y * other.x
+        )
 
-    return Orientation(yaw / DEG, pitch / DEG, roll / DEG)
+
+    # k * v, scalar pre-multiplication
+    def __rmul__(self, other):
+        return Vector3(other * self.x, other * self.y, other * self.z)
+
+
+    # vector addition
+    def __add__(self, other):
+        return Vector3(self.x + other.x, self.y + other.y, self.z + other.z)
+
+
+@dataclass
+class Quaternion:
+    w: float
+    x: float
+    y: float
+    z: float
+
+
+    @staticmethod
+    def from_tuple(t: tuple):
+        return Quaternion(*t)
+
+
+    # Quaternion * Quaternion (multiplication)
+    def __mul__(self, other):
+        s1 = self.w
+        s2 = other.w
+        v1 = Vector3(self.x, self.y, self.z)
+        v2 = Vector3(other.x, other.y, other.z)
+        v = s1 * v2 + s2 * v1 + v1.cross(v2)
+
+        return Quaternion(
+            s1 * s2 - v1.dot(v2),
+            v.x, v.y, v.z
+        )
+
+
+    def to_euler_angles(self) -> Orientation:
+        DEG = pi / 180
+
+        # roll (x-axis rotation)
+        sinr_cosp = 2 * (self.w * self.x + self.y * self.z)
+        cosr_cosp = 1 - 2 * (self.x * self.x + self.y * self.y)
+        roll = atan2(sinr_cosp, cosr_cosp)
+
+        # pitch (y-axis rotation)
+        sinp = sqrt(1 + 2 * (self.w * self.y - self.x * self.z))
+        cosp = sqrt(1 - 2 * (self.w * self.y - self.x * self.z))
+        pitch = -2 * atan2(sinp, cosp) + pi / 2
+
+        # yaw (z-axis rotation)
+        siny_cosp = 2 * (self.w * self.z + self.x * self.y)
+        cosy_cosp = 1 - 2 * (self.y * self.y + self.z * self.z)
+        yaw = -atan2(siny_cosp, cosy_cosp)
+
+        if yaw < 0:
+            yaw += (2 * pi)
+
+        return Orientation(yaw / DEG, pitch / DEG, roll / DEG)
+
+
+    def unit_inverse(self):
+        # Assume unit quaternion!
+        return Quaternion(self.w, -self.x, -self.y, -self.z)
+
+
+    def apply(self, vector: Vector3):
+        v_quaternion = Quaternion(0, vector.x, vector.y, vector.z)
+        result_quaternion = self.unit_inverse() * v_quaternion * self
+        return Vector3(result_quaternion.x, result_quaternion.y, result_quaternion.z)
 
 
 class IMU:
@@ -85,15 +152,17 @@ class IMU:
             self.setup()
 
         try:
-            quaternion = self.sensor.quaternion
+            quaternion = Quaternion(self.sensor.quaternion)
+            linear_acceleration_world = quaternion.apply(Vector3(*self.sensor.linear_acceleration))
 
             return {
                 "euler": self.sensor.euler,
                 "linear_acceleration": self.sensor.linear_acceleration,
+                "linear_acceleration_world": linear_acceleration_world,
                 "gravity": self.sensor.gravity,
                 "calibration_status": self.sensor.calibration_status,
                 "quaternion": quaternion,
-                "calculated_euler": quaternion_to_euler_angles(quaternion),
+                "calculated_euler": quaternion.to_euler_angles(),
                 "axis_remap": self.sensor.axis_remap
             }
         except OSError as e:
@@ -112,15 +181,17 @@ class IMUSimulator:
         pass
 
     def getData(self):
-        quaternion = (1, 0, 0, 0)
+        quaternion = Quaternion(1, 0, 0, 0)
+        linear_acceleration_world = quaternion.apply(Vector3(0, 0, 0))
 
         return {
             "euler": (0.0, 0.0, 0.0),
             "linear_acceleration": (0.0, 0.0, 0.0),
+            "linear_acceleration_world": linear_acceleration_world,
             "gravity": (0.0, 0.0, 9.8),
             "calibration_status": (3, 3, 3, 3),
             "quaternion": quaternion,
-            "calculated_euler": quaternion_to_euler_angles(quaternion)
+            "calculated_euler": quaternion.to_euler_angles()
         }
 
 
