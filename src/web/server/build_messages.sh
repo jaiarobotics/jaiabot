d6f5cbdf97f812b5a04356fc838578588f01891a
Toby Schneider <toby@gobysoft.org>
2023-03-28 15:09:13 +1100

    Update build_messages.sh to track CMake found locations for Goby and DCCL headers rather than hardcoding them to /usr/include

--- src/web/server/build_messages.sh
+++ src/web/server/build_messages.sh
@@ -7,17 +7,24 @@ python_outdir='.'
 mkdir -p $python_outdir
 
 PROTO_INCLUDE=/etc/jaiabot/proto_include/
-[[ ! -z "$1" ]] && PROTO_INCLUDE="$2"
+[[ ! -z "$2" ]] && PROTO_INCLUDE="$2"
 
-if [ ! -d ${PROTO_INCLUDE} ]
-then
-    mkdir -p ${PROTO_INCLUDE}
-    
-    ln -sf /usr/include/goby ${PROTO_INCLUDE}/goby
-    ln -sf /usr/include/dccl ${PROTO_INCLUDE}/dccl
-    mkdir -p ${PROTO_INCLUDE}/jaiabot
-    ln -sf $(pwd)/../../lib/messages ${PROTO_INCLUDE}/jaiabot/messages
-fi
+GOBY_INCLUDE_DIR=/usr/include
+[[ ! -z "$3" ]] && GOBY_INCLUDE_DIR="$3"
+
+DCCL_INCLUDE_DIR=/usr/include
+[[ ! -z "$4" ]] && DCCL_INCLUDE_DIR="$4"
+
+
+mkdir -p ${PROTO_INCLUDE}    
+
+[[ -e "${PROTO_INCLUDE}/goby" ]] && rm ${PROTO_INCLUDE}/goby
+ln -sf ${GOBY_INCLUDE_DIR}/goby ${PROTO_INCLUDE}/goby
+[[ -e "${PROTO_INCLUDE}/dccl" ]] && rm ${PROTO_INCLUDE}/dccl
+ln -sf ${DCCL_INCLUDE_DIR}/dccl ${PROTO_INCLUDE}/dccl
+mkdir -p ${PROTO_INCLUDE}/jaiabot
+[[ -e "${PROTO_INCLUDE}/jaiabot/messages" ]] && rm ${PROTO_INCLUDE}/jaiabot/messages
+ln -sf $(pwd)/../../lib/messages ${PROTO_INCLUDE}/jaiabot/messages
 
 echo "🟢 Building Jaia protobuf python modules"
 
