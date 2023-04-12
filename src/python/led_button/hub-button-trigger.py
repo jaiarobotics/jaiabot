17b781d98d2f5b94ca8a30bbf89f7b4c7a3c9f13
ferrom-jaia <109534520+ferrom-jaia@users.noreply.github.com>
2023-04-12 15:08:59 -0400

    feature/write-script-for-reading-spi-python (#402)
    * adding gps firmware options
    
    * fixed typo
    
    * adding systemd configuration templates for gps
    
    * Adding option for electronics stack
    
    * add else option for electronics stack
    
    * Adding ability to switch code between electronic stacks
    
    * Updating hub led script descriptions
    
    * Fixing typo and updating how I get the eletronic stack information
    
    * Fixing bugs

--- src/python/led_button/hub-button-trigger.py
+++ src/python/led_button/hub-button-trigger.py
@@ -6,10 +6,72 @@ import requests
 import RPi.GPIO as GPIO
 import time
 import logging
+from enum import Enum
+import argparse
 
 logging.basicConfig(format='%(asctime)s %(levelname)10s %(message)s')
 log = logging.getLogger('hub-button-trigger')
 log.setLevel('DEBUG')
+
+parser = argparse.ArgumentParser(description='Turn on/off red/green light on hub led button', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('--electronics_stack', choices=['0', '1', '2'], help='If set, configure services for electronics stack')
+args=parser.parse_args()
+
+class ELECTRONICS_STACK(Enum):
+    STACK_0 = '0'
+    STACK_1 = '1'
+    STACK_2 = '2'
+
+if args.electronics_stack == '0':
+    jaia_electronics_stack=ELECTRONICS_STACK.STACK_0
+elif args.electronics_stack == '1':
+    jaia_electronics_stack=ELECTRONICS_STACK.STACK_1
+elif args.electronics_stack == '2':
+    jaia_electronics_stack=ELECTRONICS_STACK.STACK_2
+
+def led_red():
+    if jaia_electronics_stack == ELECTRONICS_STACK.STACK_1:
+        GPIO.output(19, GPIO.LOW)
+        GPIO.output(5, GPIO.LOW)
+    elif jaia_electronics_stack == ELECTRONICS_STACK.STACK_2:
+        GPIO.output(5, GPIO.LOW)
+        GPIO.output(13, GPIO.HIGH)
+        GPIO.output(11, GPIO.LOW)
+
+def led_green():
+    if jaia_electronics_stack == ELECTRONICS_STACK.STACK_1:
+        GPIO.output(5, GPIO.HIGH)
+        GPIO.output(19, GPIO.HIGH)
+    elif jaia_electronics_stack == ELECTRONICS_STACK.STACK_2:
+        GPIO.output(5, GPIO.HIGH)
+        GPIO.output(13, GPIO.LOW)
+        GPIO.output(11, GPIO.HIGH)
+
+def led_off():
+    if jaia_electronics_stack == ELECTRONICS_STACK.STACK_1:
+        GPIO.output(6, GPIO.HIGH)
+        GPIO.output(5, GPIO.HIGH)
+        GPIO.output(19, GPIO.LOW)
+        GPIO.output(13, GPIO.LOW)
+    elif jaia_electronics_stack == ELECTRONICS_STACK.STACK_2:
+        GPIO.output(5, GPIO.HIGH) # Turns off
+        GPIO.output(13, GPIO.HIGH) # Turns off
+        GPIO.output(11, GPIO.LOW) # Turns off
+
+def led_init():
+    GPIO.setmode(GPIO.BCM)
+
+    if jaia_electronics_stack == ELECTRONICS_STACK.STACK_1:
+        GPIO.setup(6, GPIO.OUT)  # RED HIGH
+        GPIO.setup(5, GPIO.OUT)  # RED LOW
+        GPIO.setup(19, GPIO.LOW) # GREEN HIGH
+        GPIO.setup(13, GPIO.LOW) # GREEN LOW
+    elif jaia_electronics_stack == ELECTRONICS_STACK.STACK_2:
+        GPIO.setup(5, GPIO.OUT)  # RED
+        GPIO.setup(11, GPIO.OUT) # GREEN HIGH
+        GPIO.setup(13, GPIO.OUT) # GREEN LOW
+
+    led_off()
 
 # defining the api-endpoint 
 API_ENDPOINT = "http://localhost/jaia/allStop"
@@ -67,30 +129,6 @@ def held(btn):
         # Set back to true to allow another stop all
         btn.is_not_stopping_bots = True
         log.debug("Held Complete")
-
-def led_red():
-    GPIO.output(5, GPIO.LOW)
-    GPIO.output(13, GPIO.HIGH)
-    GPIO.output(11, GPIO.LOW)
-
-def led_green():
-    GPIO.output(5, GPIO.HIGH)
-    GPIO.output(13, GPIO.LOW)
-    GPIO.output(11, GPIO.HIGH)
-
-def led_off():
-    GPIO.output(5, GPIO.HIGH) # Turns off
-    GPIO.output(13, GPIO.HIGH) # Turns off
-    GPIO.output(11, GPIO.LOW) # Turns off
-
-def led_init():
-    GPIO.setmode(GPIO.BCM)
-
-    GPIO.setup(5, GPIO.OUT)  # RED
-    GPIO.setup(11, GPIO.OUT) # GREEN HIGH
-    GPIO.setup(13, GPIO.OUT) # GREEN LOW
-
-    led_off()
 
 btn = Button(4)
 btn.hold_time = 0.5
