d4af6b210a4e1e3347a70da365893b4c0180590a
Michael Twomey <michael-twomey1@outlook.com>
2023-04-24 15:07:51 -0400

    Make engineering panel buttons uniform width

--- src/web/command_control/client/components/MissionSpeedSettingsPanel.tsx
+++ src/web/command_control/client/components/MissionSpeedSettingsPanel.tsx
@@ -48,7 +48,7 @@ export default class MissionSpeedSettingsPanel extends React.Component {
                     <tbody>
                         <tr>
                             <td>
-                                <Typography>
+                                <Typography className="mission-speeds-label">
                                     Transit
                                 </Typography>        
                             </td>
@@ -98,7 +98,7 @@ export default class MissionSpeedSettingsPanel extends React.Component {
                         </tr>
                         <tr>
                             <td>
-                                <Typography>
+                                <Typography className="mission-speeds-label">
                                     Station Keep
                                 </Typography>
                             </td>
--- src/web/command_control/client/components/PIDGainsPanel.tsx
+++ src/web/command_control/client/components/PIDGainsPanel.tsx
@@ -333,17 +333,17 @@ export class PIDGainsPanel extends React.Component {
                 {
                     botSelector
                 }
-                <Button className="button-jcc" type="button" id="query_engineering_status" onClick={this.queryEngineeringStatus.bind(this)}>Query Selected Status</Button>
-                <Button className="button-jcc" type="button" id="query_all_engineering_status" onClick={this.queryAllEngineeringStatus.bind(this)}>Query All Statuses</Button>
+                <Button className="button-jcc engineering-panel-btn" type="button" id="query_engineering_status" onClick={this.queryEngineeringStatus.bind(this)}>Query Selected Status</Button>
+                <Button className="button-jcc engineering-panel-btn" type="button" id="query_all_engineering_status" onClick={this.queryAllEngineeringStatus.bind(this)}>Query All Statuses</Button>
                 {
                     pidGainsTable(engineering)
                 }
-                <Button className="button-jcc" type="button" id="submit_gains" onClick={this.submitGains.bind(this)}>Change Gains</Button>
+                <Button className="button-jcc engineering-panel-btn" type="button" id="submit_gains" onClick={this.submitGains.bind(this)}>Change Gains</Button>
                 {
                     botRequirementsTable(engineering)
                 }
-                <Button className="button-jcc" type="button" id="submit_bot_requirements" onClick={this.submitBotRequirements.bind(this)}>Update Selected Bot</Button>
-                <Button className="button-jcc" type="button" id="submit_all_bot_requirements" onClick={this.submitAllBotRequirements.bind(this)}>Update All Bots</Button>
+                <Button className="button-jcc engineering-panel-btn" type="button" id="submit_bot_requirements" onClick={this.submitBotRequirements.bind(this)}>Update Selected Bot</Button>
+                <Button className="button-jcc engineering-panel-btn" type="button" id="submit_all_bot_requirements" onClick={this.submitAllBotRequirements.bind(this)}>Update All Bots</Button>
             </div>
         )
     
