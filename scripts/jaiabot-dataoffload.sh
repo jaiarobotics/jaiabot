45c9d097ec7001a5df7fc5800641ade9e528d4d6
ferrom-jaia <109534520+ferrom-jaia@users.noreply.github.com>
2023-05-05 13:24:12 -0400

    task/update-data-offload-script-to-move-files-to-archive (#437)
    * Updated the offload script so we mv the logs after each offload to an archive directory so we get an accurate rync percentage
    
    * Keeping sym links because if we remove them then we stop logging

--- scripts/jaiabot-dataoffload.sh
+++ scripts/jaiabot-dataoffload.sh
@@ -14,17 +14,65 @@ set -u
 
 nice -n 10 rsync -aP --info=progress2 --no-inc-recursive --timeout=15 --exclude='*.txt*' --exclude="*latest.goby" ${log_dir}/ jaia@hub0:/var/log/jaiabot/bot_offload
 
+# Set the path of the directory one level up
+parent_dir="${log_dir}/.."
+
+# Where the logs get moved to after rsync to hub
+archive_dir="${parent_dir}/archive"
+
+# Check if the directory exists
+if [ ! -d "${archive_dir}" ]; then
+  echo "Archive directory does not exist. Creating directory..."
+  # Create the directory
+  mkdir "${archive_dir}"
+  echo "Archive directory created successfully."
+else
+  echo "Archive directory already exists."
+fi
+
+# Where the sym links get moved to after rsync to hub
+sym_dir="${parent_dir}/sym"
+
+# Move sym links to another directory to perserve them
+# First create a directory for them 
+if [ ! -d "${sym_dir}" ]; then
+  echo "Sym directory does not exist. Creating directory..."
+  # Create the directory
+  mkdir "${sym_dir}"
+  echo "Sym directory created successfully."
+else
+  echo "Sym directory already exists."
+fi
+
+# Find all symbolic links in the source directory
+find "$log_dir" -type l | while read link; do
+    # Use readlink to find the target file of each symbolic link
+    target=$(readlink "$link")
+
+    # Move the target file to the destination directory
+    mv "$target" "$sym_dir"
+
+    mv "$link" "$sym_dir"
+done
+
+# Check if there are files available to move to archive directory
+if [ "$(ls -A "${log_dir}")" ]; then
+  echo "Moving log files to archive..."
+  mv "${log_dir}"/* "${archive_dir}"
+  echo "Files moved successfully."
+else
+  echo "No files available to move."
+fi
+
+# Then move the sym links and targets back for logging purposes
+if [ "$(ls -A "${sym_dir}")" ]; then
+  echo "Moving sym links to log dir..."
+  mv "${sym_dir}"/* "${log_dir}"
+  echo "Links moved successfully."
+else
+  echo "No links available to move."
+fi
+
 echo "Removing old log files..."
-# compress debug logs, omitting goby_intervehicle_subscriptions_bot.pb.txt
-
-############################################################
-#TODO TEST this and see why this is having permission issues
-############################################################
-#find ${log_dir} -type f -name '*[0-9].txt' -execdir xz -- {} \;
-
-# remove debug logs older than 7 days
-#find ${log_dir} -type f -mtime +7 -name '*.txt.xz' -execdir rm -v -f -- {} \;
-find ${log_dir} -type f -mtime +7 -name '*.txt' -execdir rm -v -f -- {} \;
-
-# remove all logs older than 14 days
-find ${log_dir} -type f -mtime +7 -name '*' -execdir rm -v -f -- {} \;   
+# remove all logs older than 7 days
+find "${archive_dir}" -type f -mtime +7 -name '*' -execdir rm -v -f -- {} \;   
