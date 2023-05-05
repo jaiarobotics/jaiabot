d25e8c75e58fac910ed89f05a67599da0aa6cd6e
Michael Twomey <michael-twomey1@outlook.com>
2023-05-05 17:53:26 -0400

    Remove toggling of map layers button since it's an undefined element
    An error was being thrown becasue the classlist property was being
    accessed from an undefined element.

--- src/web/command_control/client/components/CommandControl.tsx
+++ src/web/command_control/client/components/CommandControl.tsx
@@ -2557,7 +2557,6 @@ export default class CommandControl extends React.Component {
 								const mapLayers = document.getElementById('mapLayers')
 								mapLayers.style.width = '0px'
 								const mapLayersBtn = document.getElementById('mapLayersButton')
-								mapLayersBtn.classList.toggle('active')
 							}}
 						>
 							<FontAwesomeIcon icon={faLayerGroup as any} title="Map Layers" />
@@ -2571,7 +2570,6 @@ export default class CommandControl extends React.Component {
 								const mapLayers = document.getElementById('mapLayers')
 								mapLayers.style.width = '400px'
 								const mapLayersBtn = document.getElementById('mapLayersButton')
-								mapLayersBtn.classList.toggle('active')
 							}}
 						>
 							<FontAwesomeIcon icon={faLayerGroup as any} title="Map Layers" />
