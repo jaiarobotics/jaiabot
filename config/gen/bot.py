c3f100d70d90005d4dadba651d2d9b31c960d826
Edward Sanville <edsanville@gmail.com>
2023-05-26 17:48:10 -0400

    Added jaia_goby_log_level environment variable, for controlling goby_logger group filtering

--- config/gen/bot.py
+++ config/gen/bot.py
@@ -6,6 +6,7 @@
 import sys
 import os
 from common import config
+from common import logger
 from common import is_simulation, is_runtime
 import common, common.bot, common.comms, common.sim, common.udp
 from pathlib import Path
@@ -142,7 +143,8 @@ elif common.app == 'goby_logger':
     print(config.template_substitute(templates_dir+'/goby_logger.pb.cfg.in',
                                      app_block=app_common,
                                      interprocess_block = interprocess_common,
-                                     goby_logger_dir=log_file_dir))
+                                     goby_logger_dir=log_file_dir,
+                                     goby_logger_group_regex=logger.group_regex))
 elif common.app == 'goby_liaison' or common.app == 'goby_liaison_jaiabot':
     liaison_port=30000
     if is_simulation():
