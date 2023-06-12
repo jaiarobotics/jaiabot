99bc7d6b831fdc6509ef3affa20fbe2a1f1ae05b
michael-twomey <109538793+michael-twomey@users.noreply.github.com>
2023-06-12 09:08:49 -0400

    task/add-jdv-link-to-jcc (#445)
    * Add JDV link inside Engineering Panel of JCC
    
    * Make ip address for jdv incorporate fleet id and hub id
    
    * Move JDV button to HubDetailsComponent
    
    * Add link to router pg + add 'links' prop to detailsExpanded interface
    
    * Make links absolute paths to clear the url of the current tab
    
    ---------
    
    Co-authored-by: ferrom-jaia <109534520+ferrom-jaia@users.noreply.github.com>

--- src/web/command_control/client/components/CommandControl.tsx
+++ src/web/command_control/client/components/CommandControl.tsx
@@ -113,7 +113,7 @@ import {
 
 const jaiabot_icon = require('../icons/jaiabot.png')
 
-import {BotDetailsComponent, HubDetailsComponent, DetailsExpandedState} from './Details'
+import {BotDetailsComponent, HubDetailsComponent, HubDetailsProps, DetailsExpandedState} from './Details'
 import { jaiaAPI, JaiaAPI } from '../../common/JaiaAPI';
 
 import tooltips from '../libs/tooltips'
@@ -398,6 +398,7 @@ export default class CommandControl extends React.Component {
 				imu: false,
 				sensor: false,
 				power: false,
+				links: false
 			},
 			mapLayerActive: false, 
 			engineeringPanelActive: false,
@@ -2433,13 +2434,16 @@ export default class CommandControl extends React.Component {
 
 		switch (detailsBoxItem?.type) {
 			case 'hub':
-				detailsBox = HubDetailsComponent(hubs?.[this.selectedHubId()], 
-												this.api, 
-												closeDetails, 
-												this.state.detailsExpanded,
-												this.takeControl.bind(this),
-												this.detailsDefaultExpanded.bind(this));
-				
+				const hubDetailsProps: HubDetailsProps = {
+					hub: hubs?.[this.selectedHubId()],
+					api: this.api,
+					isExpanded: this.state.detailsExpanded,
+					detailsDefaultExpanded: this.detailsDefaultExpanded.bind(this),
+					getFleetId: this.getFleetId.bind(this),
+					takeControl: this.takeControl.bind(this),
+					closeWindow: closeDetails,
+				}
+				detailsBox = <HubDetailsComponent {...hubDetailsProps} />				
 				break;
 			case 'bot':
 				//**********************
@@ -2518,7 +2522,14 @@ export default class CommandControl extends React.Component {
 		return (
 			<div id="axui_container" className={containerClasses}>
 
-				<EngineeringPanel api={this.api} bots={bots} hubs={hubs} getSelectedBotId={this.selectedBotId.bind(this)} control={this.takeControl.bind(this)} />
+				<EngineeringPanel 
+					api={this.api} 
+					bots={bots} 
+					hubs={hubs} 
+					getSelectedBotId={this.selectedBotId.bind(this)}
+					getFleetId={this.getFleetId.bind(this)}
+					control={this.takeControl.bind(this)} 
+				/>
 
 				<MissionControllerPanel 
 					api={this.api} 
@@ -3490,6 +3501,10 @@ export default class CommandControl extends React.Component {
 
 	selectedHubId() {
 		return this.selectedHubIds().at(-1)
+	}
+
+	getFleetId() {
+		return this.podStatus.hubs[0].fleet_id
 	}
 
 	selectedBotIds() {
