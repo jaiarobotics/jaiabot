a4837b87da8ed725c0b1bbd92ac026b67b5ad248
Michael Twomey <michael-twomey1@outlook.com>
2023-07-05 07:18:59 -0400

    Complete first pass of speed bins for rc mode

--- src/web/command_control/client/components/RCControllerPanel.tsx
+++ src/web/command_control/client/components/RCControllerPanel.tsx
@@ -25,6 +25,8 @@ interface State {
 	isJoyStickStart: boolean,
 	throttleDirection: string,
 	rudderDirection: string,
+	throttleBinNumber: number,
+	rudderBinNumber: number,
 	controlType: string
 }
 
@@ -39,94 +41,157 @@ export default class RCControllerPanel extends React.Component {
 	// This means our max backward throttle would be 10 or 0.5 m/s.
 	limitBackwardThrottle: number = 0.1
 
+	maxForwardThrottle: number
+	maxBackwardThrottle: number
+	numberOfBins: number
+
     constructor(props: Props) {
         super(props)
         this.api = props.api
+		this.maxForwardThrottle = this.limitForwardThrottle * 100
+		this.maxBackwardThrottle = this.limitBackwardThrottle * 100
+		this.numberOfBins = 3
 
         this.state = {
 			isJoyStickStart: false,
-			throttleDirection: "",
-			rudderDirection: "",
-			controlType: "Manual Dual"
+			throttleDirection: '',
+			rudderDirection: '',
+			throttleBinNumber: 0,
+			rudderBinNumber: 0,
+			controlType: 'Manual Dual'
         }
     }
 
 	updateThrottleDirectionMove(event: IJoystickUpdateEvent) {
-		if (event.direction.toString() === "FORWARD") {
-			this.props.remoteControlValues.pid_control.throttle = (event.y * 100) * this.limitForwardThrottle
-		} else if (event.direction.toString() === "BACKWARD") {
-			this.props.remoteControlValues.pid_control.throttle = (event.y * 100) * this.limitBackwardThrottle
+		let binNumber = 0
+		if (event.direction.toString() === 'FORWARD') {
+			const throttleForwardAdjustValue = (event.y * 100) * this.limitForwardThrottle
+			binNumber = this.calcThrottleBinNum(throttleForwardAdjustValue, 'FORWARD')
+			this.props.remoteControlValues.pid_control.throttle = throttleForwardAdjustValue
+		} else if (event.direction.toString() === 'BACKWARD') {
+			const throttleBackwardAdjustValue = (event.y * 100) * this.limitBackwardThrottle
+			binNumber = this.calcThrottleBinNum(throttleBackwardAdjustValue, 'BACKWARD')
+			this.props.remoteControlValues.pid_control.throttle = throttleBackwardAdjustValue
+		}
+		this.setState({ throttleDirection: event.direction.toString(), throttleBinNumber: binNumber }, () => {})		
+	}
+
+	calcThrottleBinNum(speed: number, throttleDirection: string) {
+		// Basic error handling to protect against unexpected speed value
+		if (!speed || speed === 0) {
+			return 0
 		}
 
-		this.setState({ throttleDirection: event.direction.toString() })		
+		let binNumber = 0
+		if (throttleDirection === 'FORWARD') {
+			const binBreakpointDivisor = Math.ceil(this.maxForwardThrottle / this.numberOfBins)
+			binNumber = Math.floor(speed / binBreakpointDivisor) + 1
+		} else if (throttleDirection === 'BACKWARD') {
+			const binBreakpointDivisor = Math.ceil((this.maxBackwardThrottle) / this.numberOfBins)
+			// Need to use Math.ceil as opposed to Math.floor becasue the output is negative. We need -2.x => -2 and Math.ceil does that
+			binNumber = (Math.ceil(speed / binBreakpointDivisor)) - 1
+		}
+		return binNumber
 	}
 
 	updateThrottleDirectionStop() {
 		this.props.remoteControlValues.pid_control.throttle = 0
-		this.setState({ throttleDirection: "" })
+		this.setState({ throttleDirection: '', throttleBinNumber: 0 })
 	}
 
 	updateRudderDirectionMove(event: IJoystickUpdateEvent) {
 		const rudderAdjustValue = this.adjustThrottleResponse(event.x)
+		const binNumber = this.calcRudderBinNum(rudderAdjustValue)
 		this.props.remoteControlValues.pid_control.rudder = rudderAdjustValue
-		this.setState({ rudderDirection: event.direction.toString() })
+		this.setState({ rudderDirection: event.direction.toString(), rudderBinNumber: binNumber })
+	}
+
+	calcRudderBinNum(speed: number) {
+		// Basic error handling to protect against unexpected speed value
+		if (!speed || speed === 0) {
+			return 0
+		}
+
+		const absMaxRudderSpeed = 100
+		let binNumber = 0
+		if (speed > 0) {
+			const binBreakpointDivisor = Math.ceil(absMaxRudderSpeed / this.numberOfBins)
+			binNumber = Math.floor(speed / binBreakpointDivisor) + 1
+		} else {
+			const binBreakpointDivisor = Math.ceil((absMaxRudderSpeed) / this.numberOfBins)
+			// Need to use Math.ceil as opposed to Math.floor becasue the output is negative. We need -2.x => -2 and Math.ceil does that
+			binNumber = (Math.ceil(speed / binBreakpointDivisor)) - 1
+		}
+		return binNumber
 	}
 
 	updateRudderDirectionStop() {
 		this.props.remoteControlValues.pid_control.rudder = 0
-		this.setState({ rudderDirection: "" })
+		this.setState({ rudderDirection: '', rudderBinNumber: 0 })
 	}
 
 	moveSoloController(event: IJoystickUpdateEvent) {
 		let rudderAdjustValue = this.adjustThrottleResponse(event.x)
-		let throttleDirection: string
-		let rudderDirection: string
+		let throttleDirection = ''
+		let rudderDirection = ''
+		let throttleBinNumber = 0
+		let rudderBinNumber = 0
 
 		if (event.y >= 0) {
-			this.props.remoteControlValues.pid_control.throttle = (event.y * 100) * this.limitForwardThrottle
-			throttleDirection = "FORWARD"
+			const throttleForwardAdjustValue = (event.y * 100) * this.limitForwardThrottle
+			this.props.remoteControlValues.pid_control.throttle = throttleForwardAdjustValue
+			throttleBinNumber = this.calcThrottleBinNum(throttleForwardAdjustValue, 'FORWARD')
+			throttleDirection = 'FORWARD'
 		} else if (event.y < 0) {
-			this.props.remoteControlValues.pid_control.throttle = (event.y * 100) * this.limitBackwardThrottle
-			throttleDirection = "BACKWARD"
+			const throttleBackwardAdjustValue = (event.y * 100) * this.limitBackwardThrottle
+			this.props.remoteControlValues.pid_control.throttle = throttleBackwardAdjustValue
+			throttleBinNumber = this.calcThrottleBinNum(throttleBackwardAdjustValue, 'BACKWARD')
+			throttleDirection = 'BACKWARD'
 		}
 
 		this.props.remoteControlValues.pid_control.rudder = rudderAdjustValue
+		rudderBinNumber = this.calcRudderBinNum(rudderAdjustValue)
 
 		if (event.x >= 0) {
-			rudderDirection = "RIGHT"
+			rudderDirection = 'RIGHT'
 		} else if (event.x < 0) {
-		    rudderDirection = "LEFT"
+		    rudderDirection = 'LEFT'
 		}
 
-		this.setState({ throttleDirection, rudderDirection })
+		this.setState({ 
+			throttleDirection, 
+			rudderDirection,
+			throttleBinNumber,
+			rudderBinNumber
+		})
 	}
 
 	handleGamepadAxisChange(axisName: string, rudderAdjustValue: number, value: number) {
 		const controlType = this.state.controlType
-		let throttleDirection: string
-		let rudderDirection: string
+		let throttleDirection = ''
+		let rudderDirection = ''
 
-		if (axisName === (controlType === "Manual Single" ? "LeftStickX" : "RightStickX")) {
+		if (axisName === (controlType === 'Manual Single' ? 'LeftStickX' : 'RightStickX')) {
 			this.props.remoteControlValues.pid_control.rudder = rudderAdjustValue
 			if (value > 0) {
-				rudderDirection = "RIGHT"
+				rudderDirection = 'RIGHT'
 			} else if (value < 0) {
-				rudderDirection = "LEFT"
+				rudderDirection = 'LEFT'
 			} else if (value === 0) {
-				rudderDirection = ""
+				rudderDirection = ''
 				this.props.remoteControlValues.pid_control.rudder = 0
 			}
 		} 
 		
-		if (axisName === "LeftStickY") {	
+		if (axisName === 'LeftStickY') {	
 			if (value > 0) {
-				throttleDirection = "FORWARD"
+				throttleDirection = 'FORWARD'
 				this.props.remoteControlValues.pid_control.throttle = (value * 100) * this.limitForwardThrottle
 			} else if (value < 0) {
-				throttleDirection = "BACKWARD"
+				throttleDirection = 'BACKWARD'
 				this.props.remoteControlValues.pid_control.throttle = (value * 100) * this.limitBackwardThrottle
 			} else if(value === 0) {
-				throttleDirection = ""
+				throttleDirection = ''
 				this.props.remoteControlValues.pid_control.throttle = 0
 			}
 		}
@@ -141,14 +206,18 @@ export default class RCControllerPanel extends React.Component {
 	}
 
 	controlChange(event: SelectChangeEvent) {
-		console.log(event.target)
 		this.setState({ controlType: event.target.value })
 	}
 
 	clearRemoteControlValues() {
 		this.props.remoteControlValues.pid_control.throttle = 0
 		this.props.remoteControlValues.pid_control.rudder = 0
-		this.setState({ throttleDirection: "", rudderDirection: "" })
+		this.setState({ 
+			throttleDirection: '', 
+			rudderDirection: '',
+			throttleBinNumber: 0,
+			rudderBinNumber: 0
+	 	})
 	}
 
 	adjustThrottleResponse(value: number) {
@@ -168,16 +237,16 @@ export default class RCControllerPanel extends React.Component {
 			  MuiOutlinedInput: {
 				styleOverrides: {
 				  root: {
-					"&:hover .MuiOutlinedInput-notchedOutline": {
-					  borderColor: "white",
+					'&:hover .MuiOutlinedInput-notchedOutline': {
+					  borderColor: 'white',
 					},
-					"&.Mui-focused .MuiOutlinedInput-notchedOutline": {
-					  borderColor: "white",
+					'&.Mui-focused .MuiOutlinedInput-notchedOutline': {
+					  borderColor: 'white',
 					},
 				  },
 				  notchedOutline: {
-					borderColor: "white",
-					padding: "0px",
+					borderColor: 'white',
+					padding: '0px',
 				  },
 				},
 			  },
@@ -186,35 +255,35 @@ export default class RCControllerPanel extends React.Component {
 
 		// Create the Select Object
         let selectControlType = (
-			<div className="rc-dropdown">
+			<div className='rc-dropdown'>
 				<div>Control:</div>
 				<ThemeProvider theme={theme}>
 					<Select
-						labelId="control-type-select-label"
-						id="control-type-select"
+						labelId='control-type-select-label'
+						id='control-type-select'
 						value={this.state.controlType}
-						label="Assign"
+						label='Assign'
 						onChange={(e: SelectChangeEvent) => this.controlChange(e)}
-						sx={{ color: "white", fontSize: "1.25rem" }}
+						sx={{ color: 'white', fontSize: '1.25rem' }}
 						input={
 							<OutlinedInput
 								notched
 								sx={{
-									"&.MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline": {
-									borderColor: "red"
+									'&.MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline': {
+									borderColor: 'red'
 									},
-									"&.MuiOutlinedInput-root:hover .MuiOutlinedInput-notchedOutline": {
-									borderColor: "blue"
+									'&.MuiOutlinedInput-root:hover .MuiOutlinedInput-notchedOutline': {
+									borderColor: 'blue'
 									},
-									"&.MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline": {
-									borderColor: "green"
+									'&.MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline': {
+									borderColor: 'green'
 									}
 								}}
 							/>
 						}
 					>
-						<MenuItem key={1} value={"Manual Dual"}>Manual Dual</MenuItem>
-						<MenuItem key={2} value={"Manual Single"}>Manual Single</MenuItem>
+						<MenuItem key={1} value={'Manual Dual'}>Manual Dual</MenuItem>
+						<MenuItem key={2} value={'Manual Single'}>Manual Single</MenuItem>
 					</Select>
 				</ThemeProvider>
 			</div>
@@ -225,10 +294,10 @@ export default class RCControllerPanel extends React.Component {
 		let soleController: ReactElement
 
 		leftController = (
-			<div className="controller">
+			<div className='controller'>
 				<Joystick
-					baseColor="white" 
-					stickColor="black"
+					baseColor='white' 
+					stickColor='black'
 					controlPlaneShape={JoystickShape.AxisY}
 					size={100}
 					throttle={100}
@@ -244,10 +313,10 @@ export default class RCControllerPanel extends React.Component {
 		)
 
 		rightController = (
-			<div className={`controller ${this.state.controlType === "Manual Single" ? "hide-controller" : ""}`}>
+			<div className={`controller ${this.state.controlType === 'Manual Single' ? 'hide-controller' : ''}`}>
 				<Joystick
-					baseColor="white" 
-					stickColor="black"
+					baseColor='white' 
+					stickColor='black'
 					controlPlaneShape={JoystickShape.AxisX}
 					size={100}
 					throttle={100}
@@ -263,10 +332,10 @@ export default class RCControllerPanel extends React.Component {
 		)
 
 		soleController = (
-			<div className="controller">
+			<div className='controller'>
 				<Joystick
-					baseColor="white" 
-					stickColor="black"
+					baseColor='white' 
+					stickColor='black'
 					size={100}
 					throttle={100}
 					start={() => {
@@ -284,27 +353,27 @@ export default class RCControllerPanel extends React.Component {
 		this.props.remoteControlValues.bot_id = this.props.bot.bot_id
 
 		return (
-			<div id="remoteControlPanelContainer">
+			<div id='remoteControlPanelContainer'>
 				
-				<div className="rc-heading">Remote Control Panel: Bot {this.props.bot.bot_id}</div>
+				<div className='rc-heading'>Remote Control Panel: Bot {this.props.bot.bot_id}</div>
 
-				<div className="stick-container">
+				<div className='stick-container'>
 
-					{this.state.controlType === "Manual Dual" ? leftController : soleController}
+					{this.state.controlType === 'Manual Dual' ? leftController : soleController}
 
-					<div className="rc-labels-container">
+					<div className='rc-labels-container'>
 						{selectControlType}
-						<div className="rc-info-container" >
+						<div className='rc-info-container' >
 							<div>Throttle Direction:</div>
-							<div className="rc-data">{this.state.throttleDirection}</div>
+							<div className='rc-data'>{this.state.throttleDirection}</div>
 							<div>Throttle:</div>
-							<div className="rc-data">{this.props.remoteControlValues.pid_control.throttle.toFixed(0)}</div>
+							<div className='rc-data'>{this.state.throttleBinNumber}</div>
 						</div>
-						<div className="rc-info-container">
+						<div className='rc-info-container'>
 							<div>Rudder Direction:</div>
-							<div className="rc-data">{this.state.rudderDirection}</div>	
+							<div className='rc-data'>{this.state.rudderDirection}</div>	
 							<div>Rudder:</div>					
-							<div className="rc-data">{this.props.remoteControlValues.pid_control.rudder.toFixed(0)}</div>
+							<div className='rc-data'>{this.state.rudderBinNumber}</div>
 						</div>
 					</div>
 
@@ -313,7 +382,7 @@ export default class RCControllerPanel extends React.Component {
 					<Gamepad
 						deadZone={0.2}
 						onConnect={() => {
-							console.log("connected");
+							console.log('connected');
 						}}
 						onAxisChange={(axisName: string, value: number) => {
 							this.props.createInterval();
