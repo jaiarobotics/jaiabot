3db54161b259d15e1aa5ec5b43aa4bbd2b3a3703
Edward Sanville <edsanville@gmail.com>
2023-09-13 22:34:25 -0400

    task-packet endpoint takes startDate and endDate

--- src/web/server/app.py
+++ src/web/server/app.py
@@ -4,6 +4,21 @@ import argparse
 from flask import Flask, send_from_directory, Response, request
 import json
 import logging
+from datetime import *
+
+def parseDate(dateString: str):
+    if dateString is None:
+        return None
+    
+    try:
+        return datetime.strptime(dateString, '%Y-%m-%dT%H:%M:%S')
+    except:
+        try:
+            return datetime.strptime(dateString, '%Y-%m-%d')
+        except:
+            logging.warning(f'Could not parse dateString: {dateString}')
+            return None
+    
 
 # Internal Imports
 import jaia
@@ -53,7 +68,10 @@ def getStatus():
 
 @app.route('/jaia/task-packets', methods=['GET'])
 def getPackets():
-    return JSONResponse(jaia_interface.get_task_packets())
+    startDate = parseDate(request.args.get('startDate', None))
+    endDate = parseDate(request.args.get('endDate', None))
+
+    return JSONResponse(jaia_interface.get_task_packets(startDate=startDate, endDate=endDate))
 
 @app.route('/jaia/metadata', methods=['GET'])
 def getMetadata():
--- src/web/server/jaia.py
+++ src/web/server/jaia.py
@@ -1,15 +1,21 @@
+import glob
+import json
+import bisect
 import socket
 import threading
 
 from jaiabot.messages.portal_pb2 import ClientToPortalMessage, PortalToClientMessage
 from jaiabot.messages.engineering_pb2 import Engineering
-from jaiabot.messages.jaia_dccl_pb2 import Command, BotStatus, CommandForHub
+from jaiabot.messages.jaia_dccl_pb2 import *
 from jaiabot.messages.hub_pb2 import HubStatus
 
 import google.protobuf.json_format
 
 from time import sleep
-import datetime
+from pathlib import *
+from pprint import *
+from typing import *
+from datetime import *
 from math import *
 import contours
 
@@ -17,11 +23,15 @@ import logging
 
 
 def now():
-    return int(datetime.datetime.now().timestamp() * 1e6)
+    return int(datetime.now().timestamp() * 1e6)
 
 
 def utcnow():
-    return int(datetime.datetime.utcnow().timestamp() * 1e6)
+    return int(datetime.utcnow().timestamp() * 1e6)
+
+def utime(d: datetime):
+    '''Returns the utime for a datetime object'''
+    return int(d.timestamp() * 1e6)
 
 
 def floatFrom(obj):
@@ -46,13 +56,19 @@ class Interface:
     bots_engineering = {}
 
     # List of all TaskPackets received, with last known location of that bot
-    task_packets = []
+    received_task_packets = []
 
     # ClientId that is currently in control
     controllingClientId = None
 
     # MetaData
     metadata = {}
+
+    # Path to taskpacket files
+    taskPacketPath = '/var/log/jaiabot/bot_offload/'
+    # Data from taskpacket files
+    offloaded_task_packet_dates: List[str] = []
+    offloaded_task_packets: List[Dict] = []
 
     def __init__(self, goby_host=('optiplex', 40000), read_only=False):
         self.goby_host = goby_host
@@ -62,6 +78,8 @@ class Interface:
         self.read_only = read_only
         if read_only:
             logging.warning('This client is READ-ONLY.  You cannot send commands.')
+
+        self.load_taskpacket_files()
 
         # Messages to display on the client end
         self.messages = {}
@@ -384,7 +402,7 @@ class Interface:
 
     def process_task_packet(self, task_packet_message):
         task_packet = protobufMessageToDict(task_packet_message)
-        self.task_packets.append(task_packet)
+        self.received_task_packets.append(task_packet)
 
     def process_active_mission_plan(self, bot_id, active_mission_plan):
         try:
@@ -393,14 +411,26 @@ class Interface:
         except IndexError:
             logging.warning(f'Received active mission plan for unknown bot {active_mission_plan.bot_id}')
 
-    def get_task_packets(self):
-        return self.task_packets
+    def get_task_packets(self, startDate: datetime=None, endDate: datetime=None):
+        # Get stored task packets within date range
+        if startDate is not None:
+            start_utime = utime(startDate)
+            startIndex = bisect.bisect_left(self.offloaded_task_packet_dates, start_utime)
+        else:
+            startIndex = 0
+
+        if endDate is not None:
+            end_utime = utime(endDate)
+            endIndex = bisect.bisect_right(self.offloaded_task_packet_dates, end_utime)
+        else:
+            endIndex = len(self.offloaded_task_packets)
+
+        return self.offloaded_task_packets[startIndex: endIndex] + self.received_task_packets
 
     # Contour map
 
     def get_depth_contours(self):
-
-        return contours.taskPacketsToContours(self.task_packets)
+        return contours.taskPacketsToContours(self.get_task_packets())
 
     # Controlling clientId
 
@@ -412,3 +442,18 @@ class Interface:
     def get_Metadata(self):
         return self.metadata
 
+    def load_taskpacket_files(self):
+        taskPacketWindow = timedelta(days=1)
+
+        for filePath in glob.glob(self.taskPacketPath + '*.taskpacket'):
+            filePath = Path(filePath)
+            stem = filePath.stem
+            dateString = stem[-15:]
+
+            taskPackets: List[Dict] = [json.loads(line) for line in open(filePath)]
+            self.offloaded_task_packets.extend(taskPackets)
+
+        self.offloaded_task_packets = filter(lambda taskPacket: 'start_time' in taskPacket, self.offloaded_task_packets)
+        self.offloaded_task_packets = sorted(self.offloaded_task_packets, key=lambda taskPacket: taskPacket['start_time'])
+
+        self.offloaded_task_packet_dates = [int(taskPacket['start_time']) for taskPacket in self.offloaded_task_packets]
