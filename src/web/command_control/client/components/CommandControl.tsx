596f69ec34be069fe428deed2b37b509a37ab964
Matthew Ferro <matt.ferro@jaia.tech>
2023-09-21 16:07:25 -0400

    Added check for measure tool panel visible so we would not get warning message for adding wpt, and updated taskpacket server code

--- src/web/command_control/client/components/CommandControl.tsx
+++ src/web/command_control/client/components/CommandControl.tsx
@@ -1517,7 +1517,8 @@ export default class CommandControl extends React.Component {
 			// Allow an operator to click on certain features while edit mode is off
 			const editModeExemptions = ['dive', 'drift', 'rallyPoint', 'bot', 'wpt']
 			const isCollection = feature.get('features')
-			if (editModeExemptions.includes(feature?.get('type')) || isCollection) {
+
+			if (editModeExemptions.includes(feature?.get('type')) || isCollection || this.state.visiblePanel === 'MEASURE_TOOL') {
 				// Operator is free to click on this feature while Edit Mode is off
 			} else {
 				const runList = this.state.runList
