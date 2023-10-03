35a22c705030fc5ec4adc343896c08f3a372b90b
Michael Twomey <michael-twomey1@outlook.com>
2023-10-03 11:24:33 -0400

    Set edit mode to off as default for runs created with line tool

--- src/web/command_control/client/components/CommandControl.tsx
+++ src/web/command_control/client/components/CommandControl.tsx
@@ -2573,7 +2573,8 @@ export default class CommandControl extends React.Component {
 								Missions.addRunWithGoals(this.missionPlans[id].bot_id, this.missionPlans[id].plan.goal, runList);
 							}
 
-							this.setRunList(runList)
+							// Default to edit mode off for runs created with line tool
+							runList.runIdInEditMode = ''
 
 							// Close panel after applying
 							this.setVisiblePanel(PanelType.NONE)
