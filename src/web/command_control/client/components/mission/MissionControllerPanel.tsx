b42a0af95d57d94871f57d81023756162e4684d2
Michael Twomey <michael-twomey1@outlook.com>
2023-11-01 14:58:07 -0400

    Make MissionControllerPanel a proper TypeScript React class component

--- src/web/command_control/client/components/mission/MissionControllerPanel.tsx
+++ src/web/command_control/client/components/mission/MissionControllerPanel.tsx
@@ -1,55 +1,32 @@
 import React from 'react'
+
 import RunPanel from './RunPanel';
-import {JaiaAPI} from '../../../common/JaiaAPI'
+import { JaiaAPI } from '../../../common/JaiaAPI'
+import { Missions } from '../Missions'
 import { PortalBotStatus } from '../shared/PortalStatus';
 import { MissionInterface, RunInterface } from '../CommandControl';
-import { mdiPlus, mdiDelete, mdiFolderOpen, mdiContentSave, mdiAutoFix } from '@mdi/js';
-import { Missions } from '../Missions'
-import Button from '@mui/material/Button';
-import Icon from '@mdi/react'
 
-interface Props {
-    api: JaiaAPI,
-    bots: {[key: number]: PortalBotStatus},
-    mission: MissionInterface,
-    loadMissionClick: any,
-    saveMissionClick: any,
-    deleteAllRunsInMission: any,
-    autoAssignBotsToRuns: any,
+import Icon from '@mdi/react'
+import Button from '@mui/material/Button';
+import { mdiPlus, mdiDelete, mdiFolderOpen, mdiContentSave, mdiAutoFix } from '@mdi/js';
+
+type MissionControllerProps = {
+    api: JaiaAPI
+    bots: {[key: number]: PortalBotStatus}
+    mission: MissionInterface
+    loadMissionClick: any
+    saveMissionClick: any
+    deleteAllRunsInMission: any
+    autoAssignBotsToRuns: any
     toggleEditMode: (evt: React.ChangeEvent, run: RunInterface) => boolean
     unSelectHubOrBot: () => void
 }
 
-export default class MissionControllerPanel extends React.Component {
-    api: JaiaAPI
-    props: Props
+type MissionControllerState = {}
 
-    constructor(props: Props) {
-        super(props)
-        this.api = props.api
-    }
-
-    static getDerivedStateFromProps(props: Props) {
-        return {
-            bots: props.bots
-        }
-    }
-
+export default class MissionControllerPanel extends React.Component<MissionControllerProps, MissionControllerState> {
     render() {
-		const self = this;
-        const emptyMission = Object.keys(this.props.mission.runs).length == 0
-
-		let runPanelComponent =
-			<RunPanel
-				bots={self.props.bots} 
-				mission={self.props.mission}
-				loadMissionClick={self.props.loadMissionClick}
-				saveMissionClick={self.props.saveMissionClick}
-				deleteAllRunsInMission={self.props.deleteAllRunsInMission}
-				autoAssignBotsToRuns={self.props.autoAssignBotsToRuns}
-				toggleEditMode={self.props.toggleEditMode}
-                unSelectHubOrBot={self.props.unSelectHubOrBot}
-			/>
+        const emptyMission = Object.keys(this.props.mission.runs).length === 0
 
 		return (
 			<div id="missionPanel">
@@ -101,9 +78,17 @@ export default class MissionControllerPanel extends React.Component {
                         <Icon path={mdiAutoFix} title="Auto Assign Bots"/>
                     </Button>
                 </div>
-				{runPanelComponent}
+                <RunPanel
+                    bots={this.props.bots} 
+                    mission={this.props.mission}
+                    loadMissionClick={this.props.loadMissionClick}
+                    saveMissionClick={this.props.saveMissionClick}
+                    deleteAllRunsInMission={this.props.deleteAllRunsInMission}
+                    autoAssignBotsToRuns={this.props.autoAssignBotsToRuns}
+                    toggleEditMode={this.props.toggleEditMode}
+                    unSelectHubOrBot={this.props.unSelectHubOrBot}
+			    />
 			</div>
 		)
-
     }
 }
