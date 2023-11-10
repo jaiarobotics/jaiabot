080180546741a7b8569daa8945ecf82997961f97
Edward Sanville <edsanville@gmail.com>
2023-11-10 18:34:00 -0500

    Separate assigned run name from bot name in bot details title bar

--- src/web/command_control/client/components/Details.tsx
+++ src/web/command_control/client/components/Details.tsx
@@ -12,6 +12,7 @@ import { Command, CommandType, HubCommandType, BotStatus, MissionState } from '.
 import { formatLatitude, formatLongitude, formatAttitudeAngle, addDropdownListener } from './shared/Utilities'
 
 // Style Imports
+import '../style/components/Details.less'
 import { 
     mdiPlay,
     mdiStop,
@@ -700,21 +701,22 @@ export function BotDetailsComponent(props: BotDetailsProps) {
         clickOnMap = <h3 className='name'>Click edit toggle to create waypoints</h3>
     }
 
-    const getPanelTitle = () => {
-        let titleStr = `Bot ${bot.bot_id}`
+    function getBotString() {
+        return `Bot ${bot.bot_id}`
+    }
+
+    function getRunString() {
         const run = getBotRun(bot.bot_id, mission.runs)
-        if (run) {
-            titleStr += ` - ${run.name}`
-        }
-        return titleStr
+        return run?.name ?? 'No Run'
     }
 
     return (
         <React.Fragment>
             <div id='botDetailsBox'>
                 <div className='botDetailsHeading'>
-                    <div className='HorizontalFlexbox'>
-                        <h2 className='name'>{getPanelTitle()}</h2>
+                    <div className='titleBar'>
+                        <h2 className='botName'>{getBotString()}</h2>
+                        <h4 className='runName'>{getRunString()}</h4>
                         <div onClick={() => closeWindow()} className='closeButton'>⨯</div>
                     </div>
                     {clickOnMap}
