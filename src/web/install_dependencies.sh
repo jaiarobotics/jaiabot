980613312a3ef1915e0ad84823636fded4f64248
Matthew Ferro <matt.ferro@jaia.tech>
2023-11-17 13:48:56 -0500

    Removed undo feature and added fix for node module build failures

--- src/web/install_dependencies.sh
+++ src/web/install_dependencies.sh
@@ -9,5 +9,29 @@ fi
 
 echo 🟢 Installing npm dependencies to: $NODE_MODULES_DIR
 
+if [ -d "${NODE_MODULES_DIR}node_modules" ]; then
+    if [ "$(find "node_modules" -maxdepth 0 -printf '%u\n')" == "root" ]; then
+        echo "Running with root privileges to remove node_modules."
+        sudo rm -rf "${NODE_MODULES_DIR}node_modules"
+    fi
+
+    if [ -d "${NODE_MODULES_DIR}node_modules/ol-layerswitcher" ]; then
+        echo "Remove ol-layerswitcher before install"
+
+        if [ "$(find "node_modules/ol-layerswitcher" -maxdepth 0 -printf '%u\n')" == "root" ]; then
+            echo "Running with root privileges to remove node_modules/ol-layerswitcher."
+            sudo rm -rf "${NODE_MODULES_DIR}node_modules/ol-layerswitcher"
+        else
+            echo "Not running with root privileges to remove node_modules/ol-layerswitcher."
+            rm -rf "${NODE_MODULES_DIR}node_modules/ol-layerswitcher"
+	fi
+    fi
+fi
+
+if [ "$(find "package-lock.json" -maxdepth 0 -printf '%u\n')" == "root" ]; then
+    echo "Running with root privileges to remove package-lock.json."
+    sudo rm -rf "${NODE_MODULES_DIR}package-lock.json"
+fi
+
 cd $NODE_MODULES_DIR
 npm install --no-audit --quiet
