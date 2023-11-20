5eb6596048c5497f3a2026ef4837770939226027
Matthew Ferro <matt.ferro@jaia.tech>
2023-11-20 08:01:05 -0500

    Adding comments to install_dependencies.sh

--- src/web/install_dependencies.sh
+++ src/web/install_dependencies.sh
@@ -9,12 +9,18 @@ fi
 
 echo 🟢 Installing npm dependencies to: $NODE_MODULES_DIR
 
+# We need to check if node_modules is already create and the privileges set.
+# If the privileges are root then we should remove and install again as
+# this may cause issues with running and installing new dependencies
 if [ -d "${NODE_MODULES_DIR}node_modules" ]; then
     if [ "$(find "node_modules" -maxdepth 0 -printf '%u\n')" == "root" ]; then
         echo "Running with root privileges to remove node_modules."
         sudo rm -rf "${NODE_MODULES_DIR}node_modules"
     fi
 
+    # We need to check for ol-layerswitcher because it is causing jcc web
+    # build errors. Again, we check the privileges on the folder to determine
+    # if we need to escalate our priveleges to remove it.
     if [ -d "${NODE_MODULES_DIR}node_modules/ol-layerswitcher" ]; then
         echo "Remove ol-layerswitcher before install"
 
@@ -28,6 +34,8 @@ if [ -d "${NODE_MODULES_DIR}node_modules" ]; then
     fi
 fi
 
+# We need to check the privileges on package-lock.json because if it is root
+# then we will have jcc build errors.
 if [ "$(find "package-lock.json" -maxdepth 0 -printf '%u\n')" == "root" ]; then
     echo "Running with root privileges to remove package-lock.json."
     sudo rm -rf "${NODE_MODULES_DIR}package-lock.json"
