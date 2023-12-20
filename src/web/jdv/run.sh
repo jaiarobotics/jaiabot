3f9ed0842fafa1ebc2aef2cd302fe64544fa94cd
Edward Sanville <edsanville@gmail.com>
2023-12-20 02:20:52 -0500

    Disable flask DEBUG for jdv Use the test.webpack.config.js for local testing of jdv

--- src/web/jdv/run.sh
+++ src/web/jdv/run.sh
@@ -17,12 +17,15 @@ pushd ../../python/ > /dev/null
     ./build_venv.sh ${BUILD_DIR}/python
 popd > /dev/null
 
-# Build client
-pushd client
-    ./build.sh ${BUILD_DIR}/jdv
-popd
-
 # Start server
 pushd server
-    ./jaiabot_data_vision.py -a ${BUILD_DIR} -p 40011 -l INFO $@ &
+    ${BUILD_DIR}/python/venv/bin/python3 ./jaiabot_data_vision.py -a ${BUILD_DIR} -p 40011 -l INFO $@ &
+popd
+
+# Build client
+pushd client
+    BUILD_JDV=${BUILD_DIR}/jdv
+    echo "🟢 Building jdv into ${BUILD_JDV}"
+
+    webpack --mode production --config ./test.webpack.config.js --env TARGET_DIR=${BUILD_JDV} --progress
 popd
