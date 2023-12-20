314b3d7f8fc89f93115928dc1106b1b0422c1c4e
Edward Sanville <edsanville@gmail.com>
2023-12-19 20:56:09 -0500

    Fix install_dependencies.sh and build_messages.sh to target a specified directory

--- src/web/install_dependencies.sh
+++ src/web/install_dependencies.sh
@@ -1,45 +1,16 @@
 #!/bin/bash
 
-# Install dependencies to current directory, or command-line parameter
-if [[ -z $1 ]]; then
-    NODE_MODULES_DIR="./"
-else
-    NODE_MODULES_DIR=$1
-fi
+# This directory will contain intermediate build artifacts
+INTERMEDIATE_BUILD_DIR="../../build/intermediate/"
 
-echo 🟢 Installing npm dependencies to: $NODE_MODULES_DIR
+echo 🟢 Installing npm dependencies to: $INTERMEDIATE_BUILD_DIR
 
-# We need to check if node_modules is already created.
-# If the privileges are root then we should remove and install again as
-# this may cause issues with running and installing new dependencies.
-if [ -d "${NODE_MODULES_DIR}node_modules" ]; then
-    if [ "$(find "node_modules" -maxdepth 0 -printf '%u\n')" == "root" ]; then
-        echo "Running with root privileges to remove node_modules."
-        sudo rm -rf "${NODE_MODULES_DIR}node_modules"
-    fi
+# Create build directory, if necessary
+mkdir -p ${INTERMEDIATE_BUILD_DIR}
 
-    # We need to check for ol-layerswitcher because it is causing jcc web
-    # build errors if it already exists. We check the privileges on the folder to determine
-    # if we need to escalate our priveleges to remove it.
-    if [ -d "${NODE_MODULES_DIR}node_modules/ol-layerswitcher" ]; then
-        echo "Remove ol-layerswitcher before install"
+# Copy our package.json file there, because npm doesn't allow building to arbitrary locations
+cp -f ./package.json ${INTERMEDIATE_BUILD_DIR}/
 
-        if [ "$(find "node_modules/ol-layerswitcher" -maxdepth 0 -printf '%u\n')" == "root" ]; then
-            echo "Running with root privileges to remove node_modules/ol-layerswitcher."
-            sudo rm -rf "${NODE_MODULES_DIR}node_modules/ol-layerswitcher"
-        else
-            echo "Not running with root privileges to remove node_modules/ol-layerswitcher."
-            rm -rf "${NODE_MODULES_DIR}node_modules/ol-layerswitcher"
-	fi
-    fi
-fi
-
-# We need to check the privileges on package-lock.json because if it is root
-# then we will have jcc build errors.
-if [ "$(find "package-lock.json" -maxdepth 0 -printf '%u\n')" == "root" ]; then
-    echo "Running with root privileges to remove package-lock.json."
-    sudo rm -rf "${NODE_MODULES_DIR}package-lock.json"
-fi
-
-cd $NODE_MODULES_DIR
-npm install --no-audit --quiet
+pushd ${INTERMEDIATE_BUILD_DIR} > /dev/null
+    npm install --no-audit --silent
+popd > /dev/null
--- src/web/run.sh
+++ src/web/run.sh
@@ -5,18 +5,32 @@ set -e
 # Kill all descendants if we exit or are killed
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 
-# Install pre-requisites
-./install_dependencies.sh
+JAIA_DIR="../../"
+BUILD_DIR="${JAIA_DIR}/build/intermediate"
 
-# Build messages and install pyjaia
-pushd ../python/pyjaia
-    ./build_messages.sh
-    python3 -m pip install ./
+# Install pre-requisites
+./install_dependencies.sh ${BUILD_DIR}
+
+# Build the venv
+
+## Make target python directory
+mkdir -p ${BUILD_DIR}/python/
+
+## install the pyjaia directory into the intermediate build products directory
+### rsync the requirements.txt and pyjaia directory
+rsync -a ${JAIA_DIR}/src/python/requirements.txt ${JAIA_DIR}/src/python/pyjaia ${BUILD_DIR}/python/
+### build the messages to the intermediate build product directory
+./build_messages.sh ${JAIA_DIR} ${BUILD_DIR}/python/pyjaia
+
+## Create the venv
+pushd ${BUILD_DIR}/python/
+    python3 -m venv venv
+    ./venv/bin/pip install -r requirements.txt
 popd
 
 # Start server
 pushd server
-    ./app.py $1 &
+    ${BUILD_DIR}/python/venv/bin/python3 ./app.py $1 &
 popd
 
 # Build Command Control
