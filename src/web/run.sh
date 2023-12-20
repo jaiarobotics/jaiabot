4e103a19e5936dc2a3dccdf6d27993105af63aa2
Edward Sanville <edsanville@gmail.com>
2023-12-20 01:32:41 -0500

    Add build_venv.sh script

--- src/web/run.sh
+++ src/web/run.sh
@@ -9,21 +9,9 @@ JAIA_DIR="$(pwd)/../../"
 BUILD_DIR="${JAIA_DIR}/build/web_dev/"
 
 # Build the venv
-## Make target python directory
-mkdir -p ${BUILD_DIR}/python/
-
-## install the pyjaia directory into the intermediate build products directory
-### rsync the requirements.txt and pyjaia directory
-rsync -a ${JAIA_DIR}/src/python/requirements.txt ${JAIA_DIR}/src/python/pyjaia ${BUILD_DIR}/python/
-### build the messages to the intermediate build product directory
-${JAIA_DIR}/src/python/pyjaia/build_messages.sh ${JAIA_DIR} ${BUILD_DIR}/python/pyjaia
-
-## Create the venv
-pushd ${BUILD_DIR}/python/ > /dev/null
-    python3 -m venv venv
-    ./venv/bin/pip install -q wheel
-    ./venv/bin/pip install -q -r requirements.txt
-popd > /dev/null
+pushd ../python > /dev/null
+    ./build_venv.sh ${BUILD_DIR}/python
+popd
 
 # Start server
 pushd server > /dev/null
