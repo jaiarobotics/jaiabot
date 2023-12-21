037d93a41d8fb1256473b2f54d6443a8c448a448
Ed Sanville <edsanville@gmail.com>
2023-12-21 17:32:55 -0500

    Fix setup-toold-build script

--- build.sh
+++ build.sh
@@ -17,27 +17,14 @@ script_dir=$(dirname $0)
 
 ARCH=$(dpkg --print-architecture)
 
+# Make sure we're using the nvm versions of npm and webpack
+source $HOME/.nvm/nvm.sh
+
 set -e -u
 mkdir -p ${script_dir}/build/${ARCH}
 
 # install clang-format hook if not installed
 [ ! -e ${script_dir}/.git/hooks/pre-commit ] && ${script_dir}/scripts/clang-format-hooks/git-pre-commit-format install
-
-# install nvm if not installed
-if [ ! -d "${HOME}/.nvm" ]
-then
-        echo "nvm not installed! Installing...";
-  curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
-
-  export NVM_DIR="$HOME/.nvm"
-  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
-  npm install -g npm@9.6.4
-  nvm install v18.12.1
-  nvm use v18.12.1
-  npm install i -g --no-audit webpack webpack-cli
-fi
 
 echo "Configuring..."
 cd ${script_dir}/build/${ARCH}
---
+++ scripts/setup-tools-build-docker.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Add Docker’s official GPG key:
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+# Use the following command to set up the stable repository.
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+# Update the apt package index, and install the latest version of Docker Engine and containerd
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+# Create the docker group and add your user.
+sudo usermod -aG docker $USER
+newgrp docker
+# Verify that you can run docker commands without sudo.
+docker run hello-world
+# Configure Docker to start on boot
+sudo systemctl enable docker.service
+sudo systemctl enable containerd.service
+# Build the container
+cd ../.docker/focal/arm64
+docker build -t gobysoft/jaiabot-ubuntu-arm64:20.04.1 .
+# Optionally, push to docker hub
+docker push gobysoft/jaiabot-ubuntu-arm64:20.04.1
