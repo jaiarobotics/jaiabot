0bcf599922d65a7542cb1507c99298c33bddea30
Toby Schneider <toby@gobysoft.org>
2023-12-21 14:46:05 +1100

    remove chmod

--- scripts/jaiahub-dataoffload-cloudhub.sh
+++ scripts/jaiahub-dataoffload-cloudhub.sh
@@ -10,8 +10,5 @@ IP_PY="/etc/jaiabot/ip.py"
 CLOUDHUB_ID=30
 CLOUDHUB_IP=$(${IP_PY} addr --node hub --net cloudhub_vpn --fleet_id ${jaia_fleet_index} --node_id ${CLOUDHUB_ID} --ipv6)
 
-# ensure we use consistent read-only perms
-chmod -R 440 /var/log/jaiabot/bot_offload/*
-
 # use non-s3fs temporary directory
 rsync -r -c --temp-dir=/var/log/jaiabot --info=progress2 --no-inc-recursive --timeout=15 /var/log/jaiabot/bot_offload/ jaia@[${CLOUDHUB_IP}]:/var/log/jaiabot/bot_offload/
