f775cfde263afb3c19465a41cae15f81fdffd81d
Edward Sanville <edsanville@gmail.com>
2023-12-27 18:16:32 -0500

    Add jcc and jdv dependencies on their source files Add jcc and jfv dependencies on the npm_dependencies target Fixed npm_dependencies typo Removed deletion of ol-layerswitcher (it was only causing build issues because of a race condition in the parallel build process, due to missing jcc dependence upon the npm_dependencies target)

--- src/web/install_dependencies.sh
+++ src/web/install_dependencies.sh
@@ -17,21 +17,6 @@ if [ -d "${NODE_MODULES_DIR}node_modules" ]; then
         echo "Running with root privileges to remove node_modules."
         sudo rm -rf "${NODE_MODULES_DIR}node_modules"
     fi
-
-    # We need to check for ol-layerswitcher because it is causing jcc web
-    # build errors if it already exists. We check the privileges on the folder to determine
-    # if we need to escalate our priveleges to remove it.
-    if [ -d "${NODE_MODULES_DIR}node_modules/ol-layerswitcher" ]; then
-        echo "Remove ol-layerswitcher before install"
-
-        if [ "$(find "node_modules/ol-layerswitcher" -maxdepth 0 -printf '%u\n')" == "root" ]; then
-            echo "Running with root privileges to remove node_modules/ol-layerswitcher."
-            sudo rm -rf "${NODE_MODULES_DIR}node_modules/ol-layerswitcher"
-        else
-            echo "Not running with root privileges to remove node_modules/ol-layerswitcher."
-            rm -rf "${NODE_MODULES_DIR}node_modules/ol-layerswitcher"
-	fi
-    fi
 fi
 
 # We need to check the privileges on package-lock.json because if it is root
