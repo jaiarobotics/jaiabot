72e3ddb572e080a09bf96b97243877c51c238c93
Edward Sanville <edsanville@gmail.com>
2023-12-28 15:05:56 -0500

    Added CSV download button to JCC (#727)
    * Added CSV download button to JCC
    
    * Cleanup from Michael Twomey github comments
    
    * Remove @async
    
    * Adjusting the kmz and csv download buttons so they are consistent
    
    ---------
    
    Co-authored-by: Matthew Ferro <matt.ferro@jaia.tech>

---
+++ src/web/shared/CSVExport.ts
@@ -0,0 +1,73 @@
+import {TaskPacket} from './JAIAProtobuf'
+import {LogTaskPacket} from './LogMessages'
+
+/**
+ * Converts a Unix timestamp (microseconds) to an ISO date string.
+ *
+ * @param {(number | undefined)} tMicroseconds Unix timestamp in microseconds
+ * since Unix epoch.
+ * @returns {string} Date string in ISO date format.
+ */
+function isoString(tMicroseconds: number | undefined): string {
+    if (tMicroseconds == null) {
+        return ''
+    }
+
+    return new Date(tMicroseconds / 1000).toISOString()
+}
+
+/**
+ * Converts an array of task packets into the CSV file format as a string.
+ *
+ * @param {(TaskPacket | LogTaskPacket)[]} taskPackets The task packets to
+ * include in the CSV file.
+ * @returns {string} The CSV file contents as a string.
+ */
+export async function getCSV(taskPackets: (TaskPacket | LogTaskPacket)[]) {
+    let csvText = 'id,bot,task,lat,lon,time,bottom dive,depth achieved (m),current strength (m/s),current heading (deg)\n'
+    let id = 0
+
+    for (const taskPacket of taskPackets) {
+        if ('_scheme_' in taskPacket) {
+            if (taskPacket._scheme_ !== 1) {
+                // Skip DCCL messages
+                continue
+            }
+        }
+
+        const startLocation = taskPacket.dive?.start_location ?? taskPacket.drift?.start_location
+
+        const rowData: string[] = [
+            id.toString(),
+            taskPacket.bot_id?.toString() ?? '',
+            taskPacket.type.toString() ?? '',
+            startLocation.lat?.toString() ?? '',
+            startLocation.lon?.toString() ?? '',
+            isoString(taskPacket.start_time),
+            String(taskPacket.dive?.bottom_dive ?? false),
+            taskPacket.dive?.depth_achieved?.toFixed(3) ?? '',
+            taskPacket.drift?.estimated_drift?.speed?.toFixed(3) ?? '',
+            taskPacket.drift?.estimated_drift?.heading?.toFixed(3) ?? ''
+        ]
+
+        csvText += rowData.join(',') + '\n'
+
+        id += 1
+    }
+
+    return csvText
+}
+
+/**
+ * Returns a filename to use for the task packet CSV file.  The filename includes
+ * the date of the first task packet's start time.
+ *
+ * @param {TaskPacket[]} taskPackets The task packets to
+ * include in the CSV file.
+ * @returns {string} A filename in the format `taskPackets-[date].csv`.
+ */
+export function getCSVFilename(taskPackets: TaskPacket[]) {
+    const fileDate = taskPackets[0]?.start_time
+    const fileDateString = fileDate ? isoString(fileDate) : new Date().toISOString()
+    return `taskPackets-${fileDateString}.csv`
+}
