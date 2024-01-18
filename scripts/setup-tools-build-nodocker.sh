6346ae0fb13e1281c25a5bd725fff489779d8183
JRoybot <jeff@roy.net>
2024-01-18 15:14:25 -0500

    Updated setup scripts to address issues found in testing

--- scripts/setup-tools-build-nodocker.sh
+++ scripts/setup-tools-build-nodocker.sh
@@ -4,13 +4,13 @@
 apt-get -y update && apt-get -y install sudo
 
 echo -e "deb http://packages.jaia.tech/ubuntu/release/1.y/ focal/\ndeb http://packages.jaia.tech/ubuntu/gobysoft/release/1.y/ focal/" >> /etc/apt/sources.list.d/jaiabot_release_1.y.list && \
-  apt-key adv --recv-key --keyserver keyserver.ubuntu.com 954A004CD5D8CF32 && \
-  apt-key adv --recv-key --keyserver keyserver.ubuntu.com 19478082E2F8D3FE
+  apt-key adv --recv-key --keyserver hkp://keyserver.ubuntu.com:80 954A004CD5D8CF32 && \
+  apt-key adv --recv-key --keyserver hkp://keyserver.ubuntu.com:80 19478082E2F8D3FE
 
 # Add packages.gobysoft.org mirror to your apt sources
 echo "deb http://packages.jaia.tech/ubuntu/gobysoft/1.y/ `lsb_release -c -s`/" | sudo tee /etc/apt/sources.list.d/gobysoft_release.list
 # Install the public key for packages.gobysoft.org
-sudo apt-key -y adv --recv-key --keyserver keyserver.ubuntu.com 19478082E2F8D3FE
+sudo apt-key adv --recv-key --keyserver hkp://keyserver.ubuntu.com:80 19478082E2F8D3FE
 # Update apt
 sudo apt-get -y update
 # Install the required dependencies
