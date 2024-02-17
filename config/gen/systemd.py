124614329fb262187950bb7f42e0860b4c4189bb
Matthew Ferro <matt.ferro@jaia.tech>
2024-02-17 11:12:57 -0500

    Adding bot type configuration

--- config/gen/systemd.py
+++ config/gen/systemd.py
@@ -56,6 +56,7 @@ parser.add_argument('--user_role', choices=['user', 'advanced', 'developer'], he
 parser.add_argument('--electronics_stack', choices=['0', '1', '2'], help='If set, configure services for electronics stack')
 parser.add_argument('--imu_type', choices=['bno055', 'bno085', 'none'], help='If set, configure services for imu type')
 parser.add_argument('--arduino_type', choices=['spi', 'usb', 'none'], help='If set, configure services for arduino type')
+parser.add_argument('--bot_type', choices=['hydro', 'echo', 'none'], help='If set, configure services for bot type')
 
 args=parser.parse_args()
 
@@ -83,6 +84,11 @@ class ELECTRONICS_STACK(Enum):
     STACK_1 = '1'
     STACK_2 = '2'
     STACK_3 = '2'
+
+class BOT_TYPE(Enum):
+    HYDRO = 'hydro'
+    ECHO = 'echo'
+    NONE = 'none'
 
 # Set the arduino type based on the argument
 # Used to set the serial port device
@@ -119,6 +125,13 @@ elif args.electronics_stack == '2':
 else:
     jaia_electronics_stack = ELECTRONICS_STACK.STACK_0
     jaia_gps_type = GPS_TYPE.I2C
+
+if args.bot_type == 'hydro':
+    jaia_bot_type = BOT_TYPE.HYDRO
+elif args.bot_type == 'echo':
+    jaia_bot_type = BOT_TYPE.ECHO
+else:
+    jaia_bot_type = BOT_TYPE.NONE
 
 # make the output directories, if they don't exist
 os.makedirs(os.path.dirname(args.env_file), exist_ok=True)
@@ -161,6 +174,7 @@ subprocess.run('bash -ic "' +
                'export jaia_electronics_stack=' + str(jaia_electronics_stack.value) + '; ' +
                'export jaia_imu_type=' + str(jaia_imu_type.value) + '; ' +
                'export jaia_arduino_type=' + str(jaia_arduino_type.value) + '; ' +
+               'export jaia_bot_type=' + str(jaia_bot_type.value) + '; ' +
                'source ' + args.gen_dir + '/../preseed.goby; env | egrep \'^jaia|^LD_LIBRARY_PATH\' > /tmp/runtime.env; cp --backup=numbered /tmp/runtime.env ' + args.env_file + '; rm /tmp/runtime.env"',
                check=True, shell=True)
 
@@ -314,12 +328,6 @@ jaiabot_apps = [
      'error_on_fail': 'ERROR__FAILED__JAIABOT_ATLAS_SCIENTIFIC_EZO_EC_DRIVER',
      'runs_on': Type.BOT,
      'wanted_by': 'jaiabot_health.service'},
-    {'exe': 'jaiabot_echo_driver',
-     'description': 'JaiaBot Echo Driver',
-     'template': 'goby-app.service.in',
-     'error_on_fail': 'ERROR__FAILED__JAIABOT_ECHO_DRIVER',
-     'runs_on': Type.BOT,
-     'wanted_by': 'jaiabot_health.service'},
     {'exe': 'jaiabot_driver_arduino',
      'description': 'JaiaBot Driver Arduino',
      'template': 'goby-app.service.in',
@@ -349,16 +357,6 @@ jaiabot_apps = [
      'subdir': 'atlas_scientific_ezo_ec',
      'args': '20002',
      'error_on_fail': 'ERROR__FAILED__PYTHON_JAIABOT_AS_EZO_EC',
-     'runs_on': Type.BOT,
-     'runs_when': Mode.RUNTIME,
-     'wanted_by': 'jaiabot_health.service',
-     'restart': 'on-failure'},
-    {'exe': 'jaiabot_echo.py',
-     'description': 'JaiaBot MAI Echo Python Driver',
-     'template': 'py-app.service.in',
-     'subdir': 'echo',
-     'args': '20003',
-     'error_on_fail': 'ERROR__FAILED__PYTHON_JAIABOT_ECHO',
      'runs_on': Type.BOT,
      'runs_when': Mode.RUNTIME,
      'wanted_by': 'jaiabot_health.service',
@@ -449,6 +447,27 @@ else:
         'restart': 'on-failure'},
     ]
     jaiabot_apps.extend(jaiabot_apps_imu)
+
+if jaia_bot_type.value == 'echo':
+    jaiabot_apps_echo = [
+        {'exe': 'jaiabot_echo_driver',
+        'description': 'JaiaBot Echo Driver',
+        'template': 'goby-app.service.in',
+        'error_on_fail': 'ERROR__FAILED__JAIABOT_ECHO_DRIVER',
+        'runs_on': Type.BOT,
+        'wanted_by': 'jaiabot_health.service'},
+        {'exe': 'jaiabot_echo.py',
+        'description': 'JaiaBot MAI Echo Python Driver',
+        'template': 'py-app.service.in',
+        'subdir': 'echo',
+        'args': '20003',
+        'error_on_fail': 'ERROR__FAILED__PYTHON_JAIABOT_ECHO',
+        'runs_on': Type.BOT,
+        'runs_when': Mode.RUNTIME,
+        'wanted_by': 'jaiabot_health.service',
+        'restart': 'on-failure'},
+    ] 
+    jaiabot_apps.extend(jaiabot_apps_echo)
 
 jaia_firmware = [
     {'exe': 'hub-button-led-poweroff.py',
