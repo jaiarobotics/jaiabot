596457a43b3cea4510be14b0ae16aeb6a76b7c1f
Matthew Ferro <matt.ferro@jaia.tech>
2024-02-20 17:39:03 -0500

    Adding configuration for imu installation type

--- src/python/adafruit_BNO085/jaia_firm_bno085_reset_gpio_pin.py
+++ src/python/adafruit_BNO085/jaia_firm_bno085_reset_gpio_pin.py
@@ -1,8 +1,19 @@
 import RPi.GPIO as GPIO
 import time
+from enum import Enum
+import argparse
 
-# BNO085 IMU reset pin
-bno085_pin = 10
+parser = argparse.ArgumentParser(description='Turn on/off bno085 imu', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('--imu_install_type', choices=['embedded', 'retrofit', 'none'], help='If set, configure services for imu install type')
+args=parser.parse_args()
+
+# Set BNO085 IMU reset pin
+if args.imu_install_type == 'embedded':
+    bno085_pin = 10
+elif args.imu_install_type == 'retrofit':
+    bno085_pin = 23
+else:
+    bno085_pin = 10
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(bno085_pin, GPIO.OUT)  
