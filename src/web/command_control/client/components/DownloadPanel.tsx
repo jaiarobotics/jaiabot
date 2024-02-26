fe375ebdc9fe342cb3eeb70816a0d1b1fa6e561b
Eric Paul <ericpaul105@gmail.com>
2024-02-26 14:21:11 -0500

    Made the changes as requested in pull request. Added a msg Queue is Empty in the accordion when there is no ongoing download

--- src/web/command_control/client/components/DownloadPanel.tsx
+++ src/web/command_control/client/components/DownloadPanel.tsx
@@ -38,10 +38,24 @@ enum AccordionTabs {
 export default function DownloadPanel(props: Props) {
     const [openAccordionTabs, setOpenAccordionTabs] = useState([])
 
+    /**
+     * Checks if the specified accordion tab is currently open.
+     * 
+     * @param {AccordionTabs} accordionTab - The accordion tab to check.
+     * @returns {boolean} - Returns true if the accordion tab is open, false otherwise.
+     */
     const isOpenAccordionTab = (accordionTab: AccordionTabs) => {
         return openAccordionTabs.includes(accordionTab)
     }
 
+
+    /**
+     * Toggles the state of the specified accordion tab.
+     * If the accordion tab is open, it will be closed, and vice versa.
+     * 
+     * @param {AccordionTabs} accordionTab - The accordion tab to toggle.
+     * @returns {void}
+     */
     const handleAccordionTabClick = (accordionTab: AccordionTabs) => {
         let updatedOpenAccordionTabs = [...openAccordionTabs]
         
@@ -58,8 +72,16 @@ export default function DownloadPanel(props: Props) {
         }
     }
 
+
+    /**
+     * Calls processDownloadAllBots function in CommandControl  when the user clicks on "Download All" button
+     * 
+     * @returns {void}
+     */
     const handleDownloadAll = async () => {
+        
         await props.processDownloadAllBots();
+
     };
 
     
@@ -85,10 +107,11 @@ export default function DownloadPanel(props: Props) {
     
     
     /**
-     * Event handler for the CSV dowload button.  Creates the CSV file and initiates
-     * the download.
+     * Event handler for the CSV dowload button.
+     * Creates the CSV file and initiates the download.
      *
-    **/
+     * @returns {void}
+     */
     const handleDownloadCSV = async (event: React.MouseEvent<HTMLButtonElement>) => {
         const csvFilename = getCSVFilename(taskData.taskPackets)
         downloadToFile(await getCSV(taskData.taskPackets), 'text/csv', csvFilename)
@@ -122,7 +145,6 @@ export default function DownloadPanel(props: Props) {
                         <Button onClick={handleClickedDownloadKMZ} className='button-jcc'>Download KMZ</Button>
                     </AccordionDetails>
             </Accordion>
-            {/* <br /> */}
             <Accordion 
                     expanded={isOpenAccordionTab(AccordionTabs.DownloadQueue)}
                     onChange={() => handleAccordionTabClick(AccordionTabs.DownloadQueue)}
@@ -136,21 +158,30 @@ export default function DownloadPanel(props: Props) {
                         <Typography>Download Queue</Typography>
                     </AccordionSummary>
                     <AccordionDetails className="download-panel-accordion-inner-container">
-                    {props.downloadableBots.map(bot => {
-                    return (
-                        <div className="download-queue-card">
-                            <div className="download-queue-bot-number">Bot: {bot.bot_id}</div>
-                            <CircularProgress determinate value={props.getBotDownloadPercent(bot.bot_id)}/>
-                            <div className='download-queue-clos-btn-container' onClick={() => props.removeBotFromQueue(bot)}>
-                                <Icon path={mdiClose} size={1} className='download-queue-close-btn'/>
-                            </div>
-                        </div>
-                    )
-                    })}
-                    </AccordionDetails>
-                </Accordion>
-            </div>
 
+                     {
+                        props.downloadableBots.length == 0 ? 
+                        <div className="download-queue-empty">
+                            Queue is Empty
+                        </div> 
+                        :
+                        <div className="download-queue-inner-container">
+                            {props.downloadableBots.map(bot => {
+                            return (
+                                <div className="download-queue-card">
+                                    <div className="download-queue-bot-number">Bot: {bot.bot_id}</div>
+                                    <CircularProgress determinate value={props.getBotDownloadPercent(bot.bot_id)}/>
+                                    <div className='download-queue-clos-btn-container' onClick={() => props.removeBotFromQueue(bot)}>
+                                        <Icon path={mdiClose} size={1} className='download-queue-close-btn'/>
+                                    </div>
+                                </div>
+                            )
+                            })}
+                        </div>
+                     }  
+                    </AccordionDetails>
+            </Accordion>
+            </div>
         </div>
     )
 }
