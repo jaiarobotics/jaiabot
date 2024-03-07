428a54b86f3b2d6e198681a1357ad6e263a8969f
Nick Marshall <nick.marshall@jaia.tech>
2024-03-07 10:27:03 -0500

    Updated algorithm to locate false dives. Also split up the workload into multiple functions.

--- scripts/log-analysis/false_dive.py
+++ scripts/log-analysis/false_dive.py
@@ -3,12 +3,49 @@
 import h5py
 import numpy as np
 import os
+import sys
+import pytz
+from datetime import datetime
 
 # Directions: 
 #
-# Navigate to the directory containing this file, run ./false_dive.py, and enter the file path
-# to your .h5 files in the terminal.
+# Navigate to the directory containing this file, run ./false_dive.py followed by the file path
+# to your .h5 files in the terminal (e.g. './false_dive.py ~/jaia/dive_data/').
 #
+
+# Prints the data. Takes utime list, bot_id, dive_indices list, and false_indices list
+def print_data(utimes, dives, false_dives, bot_id):
+    num_dives = len(dives)
+    num_false_dives = len(false_dives)
+
+    print(f'\nBOT {bot_id}:')
+    if len(false_dives) > 0:
+        print(f'False Dives: {num_false_dives}')
+        print(f'Total Dives: {num_dives}')
+        print(f'Failure Rate: {(num_false_dives / num_dives) * 100:.2f}%\n')
+        print("False dive(s) occured at: ")
+        
+        for time in false_dives:
+            print(utime_to_realtime(round(utimes[time] / 1_000_000)))
+
+    else:
+        print("NO FALSE DIVES.")
+        print(f'COMPLETED {num_dives} DIVES.\n')
+
+
+# Test to make sure that one dive isn't being counted twice. Returns true if the utime of both indices is equal.
+def test_time_stamps(utimes, idx_1, idx_2):
+    return utime_to_realtime(round(utimes[idx_1] / 1_000_000)) == utime_to_realtime(round(utimes[idx_2] / 1_000_000))
+
+
+# Converts utime into EST
+def utime_to_realtime(utime):
+    dt = datetime.utcfromtimestamp(utime).replace(tzinfo=pytz.UTC)  
+    desired_tz = pytz.timezone('America/New_York')
+    dt = dt.astimezone(desired_tz)
+
+    return dt    
+
 
 def get_dive_data(directory_path):
     if not os.path.isabs(directory_path):
@@ -22,67 +59,43 @@ def get_dive_data(directory_path):
     for filename in os.listdir(directory_path):
         if filename.endswith('.h5'):
             file_path = os.path.join(directory_path, filename)
-            #
-            # Edit this file path to match your system
-            #
+
             with h5py.File(file_path, 'r') as file:
 
                 # Groups we're interested in
                 taskPacket = file['jaiabot::task_packet;0/jaiabot.protobuf.TaskPacket']
-                dives = file['jaiabot::task_packet;0/jaiabot.protobuf.TaskPacket/dive']
-                temperature = file['jaiabot::bot_status;0/jaiabot.protobuf.BotStatus']
-                battery_percentages = file['jaiabot::bot_status;0/jaiabot.protobuf.BotStatus']
+                dives = file['jaiabot::task_packet;0/jaiabot.protobuf.TaskPacket/dive']                
                 bot_status = file['jaiabot::bot_status;0/jaiabot.protobuf.BotStatus']
 
                 # Specific data lists of each group
-                bot_depths = np.array(dives['depth_achieved'])
-                temps = np.array(temperature['temperature'])
-                battery = np.array(battery_percentages['battery_percent'])
+                states = np.array(bot_status['mission_state'])
+                depth_achieved = np.array(taskPacket['dive/depth_achieved'])
+                temps = np.array(bot_status['temperature'])
+                battery = np.array(bot_status['battery_percent'])
                 utime = np.array(taskPacket['_utime_'])
                 bot_status_time = np.array(bot_status['_utime_'])
                 bot_id = np.array(bot_status['bot_id'])
 
                 dive_indices = []
                 false_indices = []
-                failed_times = []
-                time_indices = []
-
-                fail_temps = []
-                fail_battery_percents = []        
-
-                # Get indices of all dives and false dives as an array of ints
-                dive_indices = np.concatenate([dive_indices, np.where(bot_depths)[0]]).astype(int)
-                false_indices = np.concatenate([false_indices, np.where(bot_depths < 1)[0]]).astype(int)
-
-                for idx in false_indices:
-                    failed_times.append(round(utime[idx] / 1_000_000))
 
                 i = 0
-                for status_time in bot_status_time:
+                while i < len(depth_achieved):
+                    dive_indices.append(i)
+                    if depth_achieved[i] < 1 and not test_time_stamps(utime, i, i-1):
+                        false_indices.append(i)
                     i += 1
-                    for time in failed_times:
-                        if round(status_time / 1_000_000) == time:
-                            time_indices.append(i)
 
-                for idx in time_indices:
-                    fail_temps.append(temps[idx])
-                    fail_battery_percents.append(battery[idx])
+                file.close()
 
-                print(f'\nBOT ID: {bot_id[0]}')
-                if len(fail_temps) > 0:
-                    avg_fail_temp = sum(fail_temps) / len(fail_temps)
-                    avg_fail_batt = sum(fail_battery_percents) / len(fail_battery_percents)
-                    failure_rate = len(false_indices) / len(dive_indices) * 100
-                
-                    total_dives = len(dive_indices)
-                    total_fails = len(false_indices)
-                    print(f'Total Dives: {total_dives/2}')
-                    print(f'Total Fails: {total_fails/2}')
-                    print(f'\nFailure rate: {failure_rate:.2f}%\nAverage Failure Water Temp: {avg_fail_temp:.2f}\nAverage Failure Battery Percentage: {avg_fail_batt:.2f}\n')
-                else: 
-                    print("NO FAILED DIVES\n")
+                print_data(utime, dive_indices, false_indices, bot_id[0])
+
+
+def main():
+    get_dive_data()
+
 
 if __name__ == '__main__':
 
-    directory_path = input('Enter the path to your log files (.h5): ')
+    directory_path = sys.argv[1]
     get_dive_data(directory_path)
