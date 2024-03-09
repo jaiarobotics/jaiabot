dc0ed5a74a5029e1aa10b137dbc13326da81eecd
Edward Sanville <edsanville@gmail.com>
2024-03-09 18:33:56 -0500

    Refactor TimeRange into time_range.py

--- src/python/adafruit_BNO085/series.py
+++ src/python/adafruit_BNO085/series.py
@@ -6,24 +6,7 @@ import bisect
 from typing import *
 from datetime import *
 import statistics
-
-
-UnixTimeMicroseconds = int
-
-@dataclass
-class TimeRange:
-    start: UnixTimeMicroseconds
-    end: UnixTimeMicroseconds
-
-    def duration(self):
-        return timedelta(microseconds=(self.end - self.start))
-    
-    @staticmethod
-    def fromDatetimes(start: datetime, end: datetime):
-        return TimeRange(start.timestamp() * 1e6, end.timestamp() * 1e6)
-
-    def __contains__(self, time: UnixTimeMicroseconds):
-        return time > self.start and time < self.end
+from time_range import *
 
 
 def floatRange(start: float, end: float, delta: float):
