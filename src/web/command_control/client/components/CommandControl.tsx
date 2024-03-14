f02fc9b9336fa09a43d65cb54c304e1cd9247b83
Matthew Ferro <matt.ferro@jaia.tech>
2024-03-14 15:22:51 -0400

    Fixing comment spacing

--- src/web/command_control/client/components/CommandControl.tsx
+++ src/web/command_control/client/components/CommandControl.tsx
@@ -1154,13 +1154,13 @@ export default class CommandControl extends React.Component {
 		return null
 	}
 
-	/**
-	 * Toggle the visibility of the waypoints table for a specific run.
-	 * 
-	 * @param {string} runId The unique identifier of the run.
-	 * 
-	 * @returns {void}
-	 */
+    /**
+     * Toggle the visibility of the waypoints table for a specific run.
+     * 
+     * @param {string} runId The unique identifier of the run.
+     * 
+     * @returns {void}
+     */
     toggleShowTableOfWaypoints = (runId: string) => {
 		// Get the current run list
 		const runList = this.getRunList();
