ba5868dbccbcbdf9060f18d6e71b596c74c17965
Ed Sanville <edsanville@gmail.com>
2024-04-11 13:56:41 -0400

    Fix pyjaia installation (delete pyproject.toml)

--- src/python/pyjaia/setup.py
+++ src/python/pyjaia/setup.py
@@ -2,28 +2,26 @@
 
 from setuptools import setup, find_packages
 
-print(find_packages())
-
 setup(name='pyjaia',
-      version='1.0',
-      description='Jaia python utilities',
-      author='Jaia Robotics',
-      author_email='edsanville@gmail.com',
-      url='https://www.jaia.tech',
-      packages=[
-          'pyjaia', 
-          'dccl', 
-          'goby.middleware.protobuf', 
-          'jaiabot.messages'],
-      install_requires=[
-          'wheel', 
-          'protobuf==3.20.0', 
-          'scipy', 
-          'numpy', 
-          'cmocean',
-          'turfpy'
-          ],
+        version='1.0',
+        description='Jaia python utilities',
+        author='Jaia Robotics',
+        author_email='edsanville@gmail.com',
+        url='https://www.jaia.tech',
+        packages=[
+            'pyjaia',
+            'dccl', 
+            'goby.middleware.protobuf', 
+            'jaiabot.messages'],
+        install_requires=[
+            'wheel', 
+            'protobuf==3.20.0', 
+            'scipy', 
+            'numpy', 
+            'cmocean',
+            'turfpy'
+            ],
         scripts=[
             'pyjaia/waves/jaia-analyze-waves.py'
         ]
-     )
+    )
