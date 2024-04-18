2637143e7266cf18d04f498e2a33009b2649cf1a
Edward Sanville <edsanville@gmail.com>
2024-04-18 09:59:40 -0400

    The pyjaia setup.py file needs to manually specify the protobuf python modules, as they don't contain __init__.py files

--- src/python/pyjaia/setup.py
+++ src/python/pyjaia/setup.py
@@ -2,13 +2,18 @@
 
 from setuptools import setup, find_packages
 
+# Because these directories don't have __init__.py files, we need to specify these modules explicitly
+protobuf_packages = [
+    'dccl', 'goby.middleware.protobuf', 'jaiabot.messages'
+]
+
 setup(name='pyjaia',
         version='1.0',
         description='Jaia python utilities',
         author='Jaia Robotics',
         author_email='edsanville@gmail.com',
         url='https://www.jaia.tech',
-        packages=find_packages(),
+        packages=find_packages() + protobuf_packages,
         install_requires=[
             'wheel', 
             'protobuf==3.20.0', 
