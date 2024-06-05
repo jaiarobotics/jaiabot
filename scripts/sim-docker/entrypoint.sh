438219c2c5d3e429108aef542bc0e4f2cdbf88b7
JRoybot <jeff@roy.net>
2024-06-05 11:28:18 -0400

    Debugging env variables

--- scripts/sim-docker/entrypoint.sh
+++ scripts/sim-docker/entrypoint.sh
@@ -2,5 +2,5 @@
 cd /jaiabot/src/web
 ./run.sh &
 cd /jaiabot/config/launch/simulation/
-./generate_all_launch.sh $NUM_BOTS $WARP_SPEED
+./generate_all_launch.sh $JAIA_SIM_BOTS $JAIA_SIM_WARP
 ./all.launch
