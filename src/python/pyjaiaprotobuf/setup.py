aa076bbd03f5c261d602ea48e335f7846f7371df
Ed Sanville <edsanville@gmail.com>
2024-07-24 19:58:38 -0400

    Spin the Jaia protobuf messages off into their own python package called "pyjaiaprotobuf"

--- src/python/pyjaiaprotobuf/setup.py
+++ src/python/pyjaiaprotobuf/setup.py
@@ -1,24 +1,18 @@
 #!/usr/bin/env python3
 from setuptools import setup
 
-setup(name='pyjaia',
+setup(name='pyjaiaprotobuf',
       version='1.0',
-      description='Jaia python utilities',
+      description='Jaia python protobuf messages',
       author='Jaia Robotics',
       author_email='edsanville@gmail.com',
       url='https://www.jaia.tech',
       packages=[
-          'pyjaia',
-          'pyjaia.waves'],
+          'dccl', 
+          'goby.middleware.protobuf', 
+          'jaiabot.messages'],
       install_requires=[
           'wheel', 
           'protobuf==3.20.0', 
-          'scipy', 
-          'numpy', 
-          'cmocean',
-          'turfpy'
-          ],
-        scripts=[
-            'pyjaia/waves/jaia-analyze-waves.py'
-        ]
+          ]
     )
