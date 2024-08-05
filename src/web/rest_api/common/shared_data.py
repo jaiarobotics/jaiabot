c93e51b726bdc2020cfa501fc8ca6d72d8f45b7f
Edward Sanville <edsanville@gmail.com>
2024-08-05 14:01:27 -0400

    task/add-task-packets-to-rest-api-jaia-1517 (#939)
    * Streaming client stores TaskPackets in a list.
    task_packets endpoint gets the TaskPackets that occurred during a specified time interval.
    
    * Let's use Unix microsecond timestamps for all API calls, to avoid date string formatting and timezone issues.
    
    * Remove unused date string to timestamp conversion function from the common.time module.
    
    * REST API task_packets endpoint obeys the target field in the request, and filters as requested.
    
    * REST API task_packets endpoint uses start_time and end_time instead of start_date and end_date, signifying that they're Unix timestamps and not date strings.
    
    ---------
    
    Co-authored-by: Toby Schneider <toby@gobysoft.org>

--- src/web/rest_api/common/shared_data.py
+++ src/web/rest_api/common/shared_data.py
@@ -3,8 +3,10 @@
 ##
 
 from queue import Queue
+from typing import *
 import threading
 import logging
+import bisect
 
 
 from common.time import utc_now_microseconds
@@ -22,6 +24,44 @@ class Data:
     # MetaData
     metadata = {}
 
+    # Task Packets
+    task_packets = []
+
+    def get_task_packets(self, bot_ids: Union[Iterable[int], None], start_time_microseconds: Union[int, None], end_time_microseconds: Union[int, None]):
+        """Gets a list of task packets occurring during a timespan.
+
+        Args:
+            start_time_microseconds (Union[int, None]): The start of the timespan, as a Unix microsecond timestamp.  None means open-ended start time.
+            end_time_microseconds (Union[int, None]): The end of the timespan, as a Unix microsecond timestamp.  None means open-ended end time.
+
+        Returns:
+            List[TaskPacket]: A list of the task packets, sorted ascending by start_time.
+        """
+        # Sort the task packets by dates
+        self.task_packets.sort(key=lambda task_packet: task_packet.start_time)
+
+        # Filter by bot_id if we got a set of bot_ids
+        if bot_ids is not None:
+            bot_ids_set = set(bot_ids)
+            filtered_task_packets = list(filter(lambda task_packet: task_packet.bot_id in bot_ids_set, self.task_packets))
+        else:
+            filtered_task_packets = self.task_packets
+
+        # bisect module doesn't have key functions till python 3.10!
+        start_times = [task_packet.start_time for task_packet in filtered_task_packets]
+
+        if start_time_microseconds is not None:
+            start_index = bisect.bisect_left(start_times, start_time_microseconds)
+        else:
+            start_index = 0
+
+        if end_time_microseconds is not None:
+            end_index = bisect.bisect_right(start_times, end_time_microseconds)
+        else:
+            end_index = len(filtered_task_packets)
+        
+        return filtered_task_packets[start_index:end_index]
+
     def process_portal_to_client_message(self, msg):
         if msg.HasField('bot_status'):
             msg.bot_status.received_time = utc_now_microseconds()
@@ -37,7 +77,7 @@ class Data:
         if msg.HasField('task_packet'):
             logging.info('Task packet received')
             packet = msg.task_packet
-            #self.process_task_packet(packet)
+            self.task_packets.append(packet)
 
         if msg.HasField('device_metadata'):
             self.metadata = msg.device_metadata
--- src/web/rest_api/test/short_api_test.py
+++ src/web/rest_api/test/short_api_test.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python3
 
 import requests
+import os
+import datetime
+from pprint import pprint
 
-res = requests.post('http://127.0.0.1:9092/jaia/v1', json={"target": {"all": True}, "status": True, "api_key": "4vS6s2jnulxVjrKSB-__tQ"})
+try: 
+    api_key=os.environ['JAIA_REST_API_PRIVATE_KEY']
+except KeyError:
+    api_key=""
+
+now_micros = int(datetime.datetime.now().timestamp() * 1e6)
+DAY = 1e6 * 60 * 60 * 24 # microseconds in a day
+
+res = requests.post('http://127.0.0.1:9092/jaia/v1', json={"target": {"all": False, "bots": [1]}, "task_packets": {"start_time": now_micros - DAY, "end_time": now_micros + DAY}, "api_key": api_key})
 assert(res.ok)
-print(res.json())
+pprint(res.json())
