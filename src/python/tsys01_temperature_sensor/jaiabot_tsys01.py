fa6499620ddb96f5631b3c66e7ff46273317a9d9
michael-twomey <michael-twomey1@outlook.com>
2024-08-06 16:03:24 -0400

    Configure python driver to send data to cpp app over UDP

--- src/python/tsys01_temperature_sensor/jaiabot_tsys01.py
+++ src/python/tsys01_temperature_sensor/jaiabot_tsys01.py
@@ -4,6 +4,7 @@ import tsys01
 import argparse
 import socket
 from time import sleep
+from jaiabot.messages.tsys01_pb2 import TSYS01Data
 
 parser = argparse.ArgumentParser(description='Read temperature from TSYS01 temperature sensor and publish it over UDP')
 parser.add_argument('-p', '--port', dest='port', default=20005, help='Port to access temperature readings from TSYS01')
@@ -11,6 +12,7 @@ args = parser.parse_args()
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind(('', args.port))
+buffer_size = 1024
 
 sensor = tsys01.TSYS01()
 
@@ -19,11 +21,12 @@ if not sensor.init():
     exit(1)
 
 while True:
+    data, addr = sock.recvfrom(buffer_size)
+    tsys01_data = TSYS01Data()
+
     if not sensor.read():
         print("Error reading sensor")
         exit(1)
 
-    temperature = sensor.temperature()
-    print(temperature)
-
-    sleep(0.1)
+    tsys01_data.temperature = sensor.temperature()
+    sock.sendto(tsys01_data.SerializeToString(), addr)
