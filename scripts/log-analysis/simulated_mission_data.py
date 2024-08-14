66cb4239a5f6fdb1867324b6dde4d26453995fc1
Joey Mussalli <106332621+OverArchMC@users.noreply.github.com>
2024-08-14 11:29:44 -0400

    Update simulated_mission_data.py

--- scripts/log-analysis/simulated_mission_data.py
+++ scripts/log-analysis/simulated_mission_data.py
@@ -39,9 +39,6 @@ def get_data(file_list):
             task_pattern = re.compile(r'jaiabot::task_packet;.')
             status_pattern = re.compile(r'jaiabot::bot_status;.')
 
-            # str_filename = str(filename).split('_')
-            # instance_fleet_number = str_filename[-2][5:]
-
             try:
                 for group in groups:
                     if task_pattern.search(group):
@@ -60,7 +57,7 @@ def get_data(file_list):
 
                 status_pitch_unfiltered = pd.Series(bot_status['jaiabot.protobuf.BotStatus/attitude/pitch'], name='status_pitch')
                 status_yaw_unfiltered = pd.Series(bot_status['jaiabot.protobuf.BotStatus/attitude/heading'], name='status_yaw')
-                status_roll_unfiltered = pd.Series(bot_status['jaiabot.protobuf.BotStatus/attitude/roll'], name='status_roll')
+                # status_roll_unfiltered = pd.Series(bot_status['jaiabot.protobuf.BotStatus/attitude/roll'], name='status_roll')
 
                 status_depth_unfiltered = pd.Series(bot_status['jaiabot.protobuf.BotStatus/depth'], name='status_depth')
 
@@ -75,7 +72,7 @@ def get_data(file_list):
                 mission_state = mission_state_unfiltered[status_scheme != 1]
                 status_pitch = status_pitch_unfiltered[status_scheme != 1]
                 status_yaw = status_yaw_unfiltered[status_scheme != 1]
-                status_roll = status_roll_unfiltered[status_scheme != 1]
+                # status_roll = status_roll_unfiltered[status_scheme != 1]
                 status_depth = status_depth_unfiltered[status_scheme != 1]
 
                 status_utime.reset_index(drop=True, inplace=True)
@@ -83,18 +80,13 @@ def get_data(file_list):
                 mission_state.reset_index(drop=True, inplace=True)
                 status_pitch.reset_index(drop=True, inplace=True)
                 status_yaw.reset_index(drop=True, inplace=True)
-                status_roll.reset_index(drop=True, inplace=True)
+                # status_roll.reset_index(drop=True, inplace=True)
                 status_depth.reset_index(drop=True, inplace=True)
                 
-                # first_transit_index = np.where(mission_state == 110)[0][0]
-                # last_stop_index = np.where(mission_state == 142)[-1][-1]
                 start_idx = np.where(mission_state == 110)[0][0]
                 end_idx = np.where(mission_state == 142)[-1][-1]
 
                 status_utime = status_utime - status_utime[start_idx]
-                # status_datetime = pd.to_datetime(status_utime, unit='us', origin='unix', utc=True).dt.tz_convert('America/New_York')
-                # status_datetime = pd.to_datetime(status_utime, unit='us', origin='unix', utc=True)
-
                 status_datetime = pd.to_datetime(status_utime, unit='us', origin='unix', utc=True)
                 status_datetime.name = 'Date/Time'
 
@@ -102,14 +94,12 @@ def get_data(file_list):
 
                 for i in range(len(status_datetime)):
                     status_datetime[i] = status_datetime[i][11:19]
-                # print(status_datetime)
                 
-                instances = pd.concat([bot_id[start_idx:end_idx], status_roll[start_idx:end_idx], status_pitch[start_idx:end_idx], status_yaw[start_idx:end_idx], bot_lat[start_idx:end_idx], bot_long[start_idx:end_idx], status_depth[start_idx:end_idx], mission_state[start_idx:end_idx], status_datetime[start_idx:end_idx]], axis=1)
-                
-                bot_data[max(bot_id)] = instances
+                instances = pd.concat([bot_id[start_idx:end_idx], status_pitch[start_idx:end_idx], status_yaw[start_idx:end_idx], bot_lat[start_idx:end_idx], bot_long[start_idx:end_idx], status_depth[start_idx:end_idx], mission_state[start_idx:end_idx], status_datetime[start_idx:end_idx]], axis=1)
+                instances.reset_index(drop=True, inplace=True)
+                instances.index += 1
 
-                # instance_start_time = str(status_datetime[0])[0:-16]
-                # instance_end_time = str(status_datetime[len(status_datetime)-1])[0:-16]
+                bot_data[max(bot_id)] = instances
 
                 
             except IOError as e:
@@ -130,7 +120,7 @@ def get_files(path, recursive):
     else:
         print("File or Directory does not exist or File is not an h5. Try again.")
 
-def export_data(bot_data, out_path="..\\..\\..\\..\\test.xlsx"):    
+def export_data(bot_data, out_path="..\\..\\..\\..\\Simulated_Mission_Data.xlsx"):    
 
     # with pd.ExcelWriter(out_path, mode='a', if_sheet_exists='overlay') as writer: 
     with pd.ExcelWriter(out_path, mode='w') as writer: 
