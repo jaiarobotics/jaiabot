7fc5e8d950d279ecb04ba73c1d524466c114c2f2
Joey Mussalli <106332621+OverArchMC@users.noreply.github.com>
2024-08-19 11:20:08 -0400

    Update pass_checkout_tests.py

--- scripts/log-analysis/pass_checkout_tests.py
+++ scripts/log-analysis/pass_checkout_tests.py
@@ -15,10 +15,9 @@ from sortedcontainers import SortedDict
 
 Usage:
 - Navigate to the directory this script is saved in.
-- In a terminal, run python3 calibration_and_imu_issue_counter_to_excel.py <path/to/h5>.
+- In a terminal, run python3 pass_checkout_tests.py <path/to/h5>.
     - If given a .h5 it will run one file, if given a directory it will run through all files in the directory. 
 - Add <-r> as a flag to run a recursive search through subdirectories. 
-- Add <-c> as a flag to combine files from the same day of testing. 
 
 
 """
@@ -30,8 +29,9 @@ Usage:
 maximum_false_dive_rate = 0.10 # Out of 10 dives there can be 1 false dive
 maximum_imu_issues_per_hour = 3
 maximum_false_transits_per_hour = 3
-maximum_distance_off_desired_dive_hold_depth = 1 # In meters
-maximum_time_at_incorrect_dive_hold_depth = 3 # In seconds
+maximum_distance_off_desired_dive_hold_depth = 2 # In meters
+maximum_time_at_incorrect_dive_hold_depth = 15 # In seconds
+maximum_threshold_to_reenter_dive_depth = 1 # In meters
 
 
 def get_data(file_list):
@@ -94,9 +94,13 @@ def get_data(file_list):
                 status_calibration_state_unfiltered = pd.Series(bot_status['jaiabot.protobuf.BotStatus/calibration_status'], name='calibration_status')
                 status_depth_unfiltered = pd.Series(bot_status['jaiabot.protobuf.BotStatus/depth'], name='status_depth')
                 
-                desired_depth = pd.Series(file['jaiabot::desired_setpoints/jaiabot.protobuf.DesiredSetpoints/helm_course/depth'], name='desired_depth')
+                # desired_depth = pd.Series(file['jaiabot::desired_setpoints/jaiabot.protobuf.DesiredSetpoints/helm_course/depth'], name='desired_depth')
                 # desired_depth = pd.Series(file['goby::middleware::frontseat::desired_course/goby.middleware.frontseat.protobuf.DesiredCourse/depth'], name='desired_depth')
-                # desired_depth = pd.Series(file['jaiabot::mission_dive/jaiabot.protobuf.DivePowerDescentDebug/goal_depth'], name='desired_depth')
+                try:
+                    desired_depth = pd.Series(file['jaiabot::mission_dive/jaiabot.protobuf.DivePowerDescentDebug/goal_depth'], name='desired_depth')
+                except:
+                    print("Continue")
+                    
                 # desired_depth.reset_index(drop=True, inplace=True)
 
                 status_utime = status_utime_unfiltered[status_scheme != 1]
@@ -113,11 +117,18 @@ def get_data(file_list):
 
                 instance_bot_number = max(bot_id)
 
-                desired_depth = desired_depth.reindex(range(len(status_depth)))
+                # print("Desired depth length before: ", len(desired_depth))
+
+                desired_depth = desired_depth.reindex(range(len(status_depth)), method='ffill')
                 desired_depth.reset_index(drop=True, inplace=True)
                 
-                # print("Desired depth length: ", len(desired_depth))
+                # print("Desired depth length after: ", len(desired_depth))
+                # print(desired_depth)
+                # print("Desired depth drop na", len(desired_depth.dropna()))
+                # desired_depth = desired_depth.dropna()
+                # desired_depth = desired_depth[desired_depth!=0]
                 # print("Status depth length: ", len(status_depth))
+                # print(desired_depth)
                 # length_diff = abs(len(desired_depth) - len(status_depth))
                 # print("Length difference: ", length_diff)
                 
@@ -193,7 +204,6 @@ def get_data(file_list):
                         reaquire_gps_states += 1
 
                         inst_start_time = str(status_datetime[i])[0:-13]
-                        # print(inst_start_time)
                         inst_start_time = inst_start_time[-8:]
 
                         while mission_state[i] == 161:
@@ -207,10 +217,8 @@ def get_data(file_list):
 
                         time_diff = datetime.strptime(inst_end_time, "%H:%M:%S") - datetime.strptime(inst_start_time, "%H:%M:%S")
 
-                        # print(time_diff)
-
                         reaquire_gps_lengths[len(reaquire_gps_lengths)-1] = time_diff.total_seconds()
-                        # print(time_diff.total_seconds())
+
                     else:
                         i += 1
 
@@ -222,7 +230,6 @@ def get_data(file_list):
                         dive_reaquire_gps_states += 1
 
                         inst_start_time = str(status_datetime[i])[0:-13]
-                        # print(inst_start_time)
                         inst_start_time = inst_start_time[-8:]
 
                         while mission_state[i] == 128:
@@ -236,10 +243,8 @@ def get_data(file_list):
 
                         time_diff = datetime.strptime(inst_end_time, "%H:%M:%S") - datetime.strptime(inst_start_time, "%H:%M:%S")
 
-                        # print(time_diff)
-
                         dive_reaquire_gps_lengths[len(dive_reaquire_gps_lengths)-1] = time_diff.total_seconds()
-                        # print(time_diff.total_seconds())
+
                     else:
                         i += 1
                 
@@ -265,22 +270,35 @@ def get_data(file_list):
                 while i < last_stop_index:
                     if mission_state[i] == 125:
                         time_at_incorrect_depth = 0
+
+                        max_depth_achieved = 0
+
+                        reentered_dive_threshold_count = 0
+                        while reentered_dive_threshold_count < 2 and mission_state[i] == 125:
+                                # print("Pass")
+                                max_depth_achieved = max(max_depth_achieved, status_depth[i])
+
+                                if status_depth[i] - desired_depth[i] < maximum_threshold_to_reenter_dive_depth:
+                                    reentered_dive_threshold_count += 1
+                                i += 1
+
                         while mission_state[i] == 125:
                             if desired_depth[i] != 0 and not np.isnan(desired_depth[i]):
-                                print("Status depth: ", status_depth[i])
-                                print("Desired depth: ", desired_depth[i])
+                                # print("Status depth: ", status_depth[i])
+                                # print("Desired depth: ", desired_depth[i])
                                 if abs(status_depth[i] - desired_depth[i]) > maximum_distance_off_desired_dive_hold_depth:
                                     time_at_incorrect_depth += 1
-                                    if time_at_incorrect_depth > maximum_time_at_incorrect_dive_hold_depth:
+                                    if time_at_incorrect_depth > maximum_time_at_incorrect_dive_hold_depth  and max_depth_achieved > desired_depth[i] - maximum_distance_off_desired_dive_hold_depth:
                                         holds_dive += 1
                                         time_at_incorrect_depth = 0
+                                        # print("Status depth: ", status_depth[i-15:i])
+                                        # print("Desired depth: ", desired_depth[i-15:i])
                                 else:
                                     time_at_incorrect_depth = 0
                             i += 1
                     else:
                         i += 1
                                     
-                # print(reaquire_gps_lengths)
 
                 instance_start_time = str(status_datetime[0])[0:-16]
                 instance_end_time = str(status_datetime[len(status_datetime)-1])[0:-16]
@@ -311,7 +329,6 @@ def get_data(file_list):
                     total_false_dives[instance_bot_id] += false_dives
                     dive_reaquire_gps_total[instance_bot_id] += dive_reaquire_gps_states
                     reaquire_gps_total[instance_bot_id] += reaquire_gps_states
-                    # print(reaquire_gps_lengths.mean())
                     reaquire_gps_time[instance_bot_id] += reaquire_gps_lengths.sum()
                     dive_reaquire_gps_time[instance_bot_id] += dive_reaquire_gps_lengths.sum()
                     bot_false_transits[instance_bot_id] += false_transits
