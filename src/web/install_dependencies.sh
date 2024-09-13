663950e531507402649f3877fb5b73d20e81a5ce
JRoybot <jeff@roy.net>
2024-09-13 16:08:53 -0400

    Restore emotion packages

--- src/web/install_dependencies.sh
+++ src/web/install_dependencies.sh
@@ -7,5 +7,7 @@ echo 🟢 Installing npm dependencies in ${PACKAGE_JSON_DIR}
 
 pushd ${PACKAGE_JSON_DIR} > /dev/null
     # Shut up unless there's an error!
-    npm install --no-audit --no-progress --silent || npm install --no-audit --no-progress --quiet
+    #     npm install --no-audit --no-progress --silent || npm install --no-audit --no-progress --quiet
+    # TODO Restore after debugging!
+    npm install 
 popd > /dev/null
