3f3d2de7f10fade32bc2515875313ef41bccb5ae
Toby Schneider <toby@gobysoft.org>
2024-09-25 18:09:29 -0400

    Drop ubuntu packaged pip dependencies

--- src/python/update_requirements.sh
+++ src/python/update_requirements.sh
@@ -12,6 +12,12 @@ sed -i 's/[~=]=/>=/' requirements.txt
 docker run -w $(realpath ${script_dir}) -v ${jaia_root}:${jaia_root} -t gobysoft/jaiabot-ubuntu-amd64:24.04.1 \
        /bin/bash -c "apt update && apt install -y rsync python3-dev python3-venv; ./build_venv.sh /tmp/jaia; source /tmp/jaia/venv/bin/activate; pip3 freeze --all  > requirements.txt"
 
+
+# from Ubuntu
+sed -i '/=.*ubuntu.*/d' requirements.txt
+sed -i '/gpg/d' requirements.txt
+
+# local
 sed -i '/pyjaia/d' requirements.txt
 sed -i '/adafruit-circuitpython-bno08x/d' requirements.txt
 
