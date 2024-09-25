1d59c463b8d46f8fa03ad317a2b7354c3ee9f040
Toby Schneider <toby@gobysoft.org>
2024-09-25 18:03:50 -0400

    Remove deb packaged dependencies

--- src/python/sync_requirements.sh
+++ src/python/sync_requirements.sh
@@ -10,6 +10,8 @@ jaia_root=$(realpath ${script_dir}/../..)
 docker run -w $(realpath ${script_dir}) -v ${jaia_root}:${jaia_root} -t gobysoft/jaiabot-ubuntu-amd64:20.04.1 \
        /bin/bash -c "apt update && apt install -y rsync python3-dev python3-venv; ./build_venv.sh /tmp/jaia; source /tmp/jaia/venv/bin/activate; pip3 freeze --all > requirements.txt"
 
+sed -i '/=.*ubuntu.*/d' requirements.txt
+
 sed -i '/pyjaia/d' requirements.txt
 sed -i '/pyjaiaprotobuf/d' requirements.txt
 sed -i '/adafruit-circuitpython-bno08x/d' requirements.txt
