9f4eb4547a975c9b0d7003834cdf362ef8137e54
Toby Schneider <toby@gobysoft.org>
2025-02-12 17:51:24 -0500

    Add major upgrade to JCU

--- config/gen/hub.py
+++ config/gen/hub.py
@@ -182,6 +182,7 @@ elif common.app == 'goby_liaison_prelaunch':
                                      user_role=user_role,
                                      inventory=inventory,
                                      vfleet_playbooks=vfleet_playbooks,
+                                     this_hub_index=hub_index,
                                      ansible_playbook_full_path=ansible_playbook_full_path))
 elif common.app == 'goby_gps':
     print(config.template_substitute(templates_dir+'/goby_gps.pb.cfg.in',
