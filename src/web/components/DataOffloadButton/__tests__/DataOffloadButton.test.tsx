f0e746d322759ffe9dcc174d83395fa7eb6196a9
Michael Twomey <109538793+michael-jaia@users.noreply.github.com>
2025-02-14 13:16:25 -0500

    task/refactor-data-offload-button/JAIA-1853 (#1075)
    * Set base structure for button managing dialog box
    
    * Make Message component + remove extra confirm handler
    
    * Add isCommandAvailable func + make map of available states
    
    * Use Props for Bot data rather than Context
    
    * Add functionality to getDisabledCode
    
    * Move DataOffloadDialog into same dir as DataOffloadButton
    
    * Style dialog box + add blocking overlay
    
    * Prepare for review + add docstrings
    
    * Make changes based on code review
    
    * Add docstrings to component level functions
    
    * Add unit tests for data offload button and dialog box
    
    * Update path to stylesheet
    
    * Update test setup

---
+++ src/web/components/DataOffloadButton/__tests__/DataOffloadButton.test.tsx
@@ -0,0 +1,57 @@
+import { render, screen } from "@testing-library/react";
+import { userEvent } from "@testing-library/user-event";
+
+import DataOffloadButton from "../DataOffloadButton";
+import { DisabledCodes, messages } from "../data-offload-messages";
+
+import { bots } from "../../../data/bots/bots";
+import { PortalBotStatus } from "../../../shared/PortalStatus";
+import { MissionState } from "../../../shared/JAIAProtobuf";
+
+const botStatusMock1: PortalBotStatus = {
+    bot_id: 1,
+    mission_state: MissionState.IN_MISSION__UNDERWAY__MOVEMENT__TRANSIT,
+};
+
+const botStatusMock2: PortalBotStatus = {
+    bot_id: 2,
+    mission_state: MissionState.IN_MISSION__UNDERWAY__RECOVERY__STOPPED,
+    wifi_link_quality_percentage: 0,
+};
+
+const botStatusMock3: PortalBotStatus = {
+    bot_id: 3,
+    mission_state: MissionState.IN_MISSION__UNDERWAY__RECOVERY__STOPPED,
+    wifi_link_quality_percentage: 100,
+};
+
+bots.setBot(botStatusMock1);
+bots.setBot(botStatusMock2);
+bots.setBot(botStatusMock3);
+
+test("Click data offload button in disabled state due to mission state", async () => {
+    render(<DataOffloadButton bot={bots.getBot(1)} />);
+    const button = screen.getByRole("button", { name: "data-offload-individual" });
+    await userEvent.click(button);
+    expect(screen.getByText("Alert")).toBeInTheDocument();
+    expect(screen.getByText(messages.get(DisabledCodes.MISSION_STATE))).toBeInTheDocument();
+    expect(screen.getByText("Close")).toBeInTheDocument();
+});
+
+test("Click data offload button in disabled state due to no Wi-Fi connection", async () => {
+    render(<DataOffloadButton bot={bots.getBot(2)} />);
+    const button = screen.getByRole("button", { name: "data-offload-individual" });
+    await userEvent.click(button);
+    expect(screen.getByText("Alert")).toBeInTheDocument();
+    expect(screen.getByText(messages.get(DisabledCodes.WIFI_QUALITY))).toBeInTheDocument();
+    expect(screen.getByText("Close")).toBeInTheDocument();
+});
+
+test("Click data offload button in enabled state", async () => {
+    render(<DataOffloadButton bot={bots.getBot(3)} />);
+    const button = screen.getByRole("button", { name: "data-offload-individual" });
+    await userEvent.click(button);
+    expect(screen.getByText("Confirm")).toBeInTheDocument();
+    expect(screen.getByText("Cancel")).toBeInTheDocument();
+    expect(screen.getByText("Start Data Offload")).toBeInTheDocument();
+});
