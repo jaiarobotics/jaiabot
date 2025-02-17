47e0b4d966160a45ad4bf65fddb40b0567f95ad1
michael-twomey <michael-twomey1@outlook.com>
2025-02-17 14:12:52 -0500

    Add number to waypoint icon

--- src/web/openlayers/features/waypoint-feature.ts
+++ src/web/openlayers/features/waypoint-feature.ts
@@ -2,7 +2,7 @@ import { Point } from "ol/geom";
 import { Feature } from "ol";
 import { fromLonLat } from "ol/proj";
 import { Coordinate } from "ol/coordinate";
-import { Fill, Icon, Style, Text } from "ol/style";
+import { Fill, Icon, Style, Stroke, Text } from "ol/style";
 
 import { view } from "../views/view";
 import { MapFeatureTypes } from "../../types/openlayers-types";
@@ -30,6 +30,18 @@ function generateWaypointStyle(index: number) {
         image: new Icon({
             src: waypointIcon,
             anchor: [0.5, 1],
+        }),
+        stroke: new Stroke({
+            color: "black",
+            width: 50,
+        }),
+        text: new Text({
+            text: String(index + 1),
+            font: "12pt sans-serif",
+            fill: new Fill({
+                color: "black",
+            }),
+            offsetY: -15,
         }),
     });
 }
