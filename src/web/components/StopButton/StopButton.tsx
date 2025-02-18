eef2fb1e988d3ef70ec5ef9835c3ec3df5cc7abe
JRoybot <jeff@roy.net>
2025-02-18 14:13:53 -0500

    Created Stop Button, copied from Data Offload Button

---
+++ src/web/components/StopButton/StopButton.tsx
@@ -0,0 +1,85 @@
+import { useState } from "react";
+import { StopDialog, DialogActions } from "./StopDialog";
+import { DisabledCodes } from "./stop-messages";
+import Bot from "../../data/bots/bot";
+import { CommandType } from "../../utils/protobuf-types";
+import { isCommandAvailable } from "../../utils/command";
+
+// MDI and MUI
+import { Icon } from "@mdi/react";
+import { Button } from "@mui/material";
+import { mdiStop } from "@mdi/js";
+import "../../style/stylesheets/util.less";
+
+interface Props {
+    bot: Bot;
+}
+
+/**
+ * Produces the Stop button for an individual Bot.
+ * It manages the alert/confirm dialog that appears when clicking on the button.
+ */
+export default function StopButton(props: Props) {
+    const [isDialogVisible, setIsDialogVisible] = useState(false);
+
+    /**
+     * Forms the style of the button (light if enabled, dark if disabled)
+     *
+     * @returns {string} General class name jaia-button plus enable/disable factor
+     */
+    const getClassName = () => {
+        let className = "jaia-button";
+
+        if (getDisabledCode() !== DisabledCodes.NONE) {
+            className += " disabled";
+        }
+
+        return className;
+    };
+
+    /**
+     * Checks the Bot's state and decides what disabled code (if any) applies based on the button conditions
+     *
+     * @returns {DisabledCodes} The applicable disabled code based on the Bot and button conditions
+     *
+     */
+    const getDisabledCode = () => {
+        if (!isCommandAvailable(CommandType.STOP, props.bot.getMissionStatus().missionState)) {
+            return DisabledCodes.MISSION_STATE;
+        }
+        return DisabledCodes.NONE;
+    };
+
+    /**
+     * Closes the dialog box then acts based on the type of button clicked
+     *
+     * @param {DialogActions} dialogAction Indicates which button was clicked
+     * @returns {void}
+     * @notes
+     * After refactoring the commands in BotDetails will issue the stop command
+     */
+    const onDialogClose = (dialogAction: DialogActions) => {
+        setIsDialogVisible(false);
+
+        if (dialogAction === DialogActions.CONFIRMED) {
+            // Send stop command
+        }
+    };
+
+    return (
+        <div>
+            <Button
+                className={getClassName()}
+                aria-label={"stop-individual-bot"}
+                onClick={() => setIsDialogVisible(true)}
+            >
+                <Icon path={mdiStop} title="Data Offload" />
+            </Button>
+            <StopDialog
+                isVisible={isDialogVisible}
+                disabledCode={getDisabledCode()}
+                onClose={onDialogClose}
+            />
+        </div>
+    );
+}
---
+++ src/web/components/StopButton/StopDialog.tsx
@@ -0,0 +1,94 @@
+import { DisabledCodes, messages } from "./stop-messages";
+
+export enum DialogActions {
+    NONE = 0,
+    CONFIRMED = 1,
+}
+
+interface TitleProps {
+    disabledCode: DisabledCodes;
+}
+
+interface ButtonRowProps {
+    disabledCode: DisabledCodes;
+    onClose: (dialogAction: DialogActions) => void;
+}
+
+interface DialogProps {
+    isVisible: boolean;
+    disabledCode: DisabledCodes;
+    onClose: (dialogAction: DialogActions) => void;
+}
+
+/**
+ * Produces the dialog box that appears when clicking on the Stop Button
+ * This dialog will be an alert if the command cannot be
+ * sent or a confirmation prior to sending the command.
+ */
+export function StopDialog(props: DialogProps) {
+    /**
+     * Forms the class name with a base of "jaia-dialog" and adds
+     * "alert" when the disabled code does not equal NONE.
+     *
+     * @returns {string} General class name jaia-dialog plus confirm/alert type
+     */
+    const getClassName = () => {
+        return `jaia-dialog ${props.disabledCode !== DisabledCodes.NONE ? "alert" : ""}`;
+    };
+
+    if (!props.isVisible) {
+        return <div></div>;
+    }
+
+    return (
+        <div>
+            <div className="blocking-overlay" onClick={() => {}}></div>
+            <div className={getClassName()}>
+                <Title disabledCode={props.disabledCode} />
+                <p>{messages.get(props.disabledCode)}</p>
+                <ButtonRow disabledCode={props.disabledCode} onClose={props.onClose} />
+            </div>
+        </div>
+    );
+}
+
+/**
+ * Produces the title for the dialog box. If there is nothing blocking the command from
+ * being sent the title will be Confirm, otherwise it will be Alert.
+ */
+function Title(props: TitleProps) {
+    if (props.disabledCode === DisabledCodes.NONE) {
+        return <h1>Confirm</h1>;
+    }
+
+    return <h1>Alert</h1>;
+}
+
+/**
+ * Produces the buttons for the dialox box.
+ * For a confirmation dialog, the buttons will be Cancel and Confirm.
+ * For an alert, the button will be Stop.
+ */
+function ButtonRow(props: ButtonRowProps) {
+    if (props.disabledCode === DisabledCodes.NONE) {
+        return (
+            <div className="dialog-button-row">
+                <button className="dialog-button" onClick={() => props.onClose(DialogActions.NONE)}>
+                    Cancel
+                </button>
+                <button
+                    className="dialog-button"
+                    onClick={() => props.onClose(DialogActions.CONFIRMED)}
+                >
+                    Stop
+                </button>
+            </div>
+        );
+    }
+
+    return (
+        <button className="dialog-button" onClick={() => props.onClose(DialogActions.NONE)}>
+            Close
+        </button>
+    );
+}
