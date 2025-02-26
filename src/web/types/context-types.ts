9697370bc20a89b52998d87f5bbda78350262b17
michael-twomey <michael-twomey1@outlook.com>
2025-02-26 16:22:28 -0500

    Pull GlobalContext into JaiaContext

---
+++ src/web/types/context-types.ts
@@ -0,0 +1,33 @@
+export const enum HubAccordionNames {
+    QUICKLOOK = "quickLook",
+    COMMANDS = "commands",
+    LINKS = "links",
+}
+
+export interface HubAccordionStates {
+    quickLook: boolean;
+    commands: boolean;
+    links: boolean;
+}
+
+export const enum BotAccordionNames {
+    QUICKLOOK = "quickLook",
+    COMMANDS = "commands",
+    ADVANCEDCOMMANDS = "advancedCommands",
+    HEALTH = "health",
+    DATA = "data",
+    GPS = "gps",
+    IMU = "imu",
+    SENSOR = "sensor",
+}
+
+export interface BotAccordionStates {
+    quickLook: boolean;
+    commands: boolean;
+    advancedCommands: boolean;
+    health: boolean;
+    data: boolean;
+    gps: boolean;
+    imu: boolean;
+    sensor: boolean;
+}
