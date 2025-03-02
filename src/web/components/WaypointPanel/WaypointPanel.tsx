30a15eafdd0818e7d82344347bc5a68153571f0c
michael-twomey <michael-twomey1@outlook.com>
2025-03-02 15:38:58 -0500

    Add basic content to waypoint panel

--- src/web/components/WaypointPanel/WaypointPanel.tsx
+++ src/web/components/WaypointPanel/WaypointPanel.tsx
@@ -1,11 +1,14 @@
 import { useContext, useEffect } from "react";
 
-import { JaiaDispatchContext } from "../../context/Jaia/JaiaContext";
+import { JaiaContext, JaiaDispatchContext } from "../../context/Jaia/JaiaContext";
 import { JaiaActions } from "../../context/Jaia/jaia-actions";
+import { missionsManager } from "../../data/missions_manager/missions-manager";
 
 import "./WaypointPanel.less";
+import { UNASSIGNED_ID, LAT_LON_DECIMALS } from "../../utils/constants";
 
 export default function WaypointPanel() {
+    const jaiaContext = useContext(JaiaContext);
     const jaiaDispatchContext = useContext(JaiaDispatchContext);
 
     useEffect(() => {
@@ -14,5 +17,40 @@ export default function WaypointPanel() {
         };
     }, []);
 
-    return <div className="waypoint-panel"></div>;
+    const getWaypoint = () => {
+        const mission = jaiaContext.missions.get(jaiaContext.selectedWaypoint.missionID);
+        return mission.getWaypoint(jaiaContext.selectedWaypoint.waypointNum);
+    };
+
+    const formatBotID = () => {
+        const botID = missionsManager.getBotID(jaiaContext.selectedWaypoint.missionID);
+
+        if (botID === UNASSIGNED_ID) {
+            return "";
+        }
+
+        return botID;
+    };
+
+    return (
+        <div className="waypoint-panel-container">
+            <div className="waypoint-panel">
+                <div className="label">Wpt:</div>
+                <div>{jaiaContext.selectedWaypoint.waypointNum}</div>
+
+                <div className="line-break"></div>
+
+                <div className="label">Bot:</div>
+                <div>{formatBotID()}</div>
+
+                <div className="line-break"></div>
+
+                <div className="label">Lat:</div>
+                <div>{getWaypoint().getLocation().lat.toFixed(LAT_LON_DECIMALS)}</div>
+
+                <div className="label">Lon:</div>
+                <div>{getWaypoint().getLocation().lon.toFixed(LAT_LON_DECIMALS)}</div>
+            </div>
+        </div>
+    );
 }
