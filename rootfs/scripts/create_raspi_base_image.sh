037adcdf81c9aa6df31a742e5c9c12bffc098045
Toby Schneider <tsaubergine@users.noreply.github.com>
2025-03-14 17:37:04 -0400

    Update create_raspi_base_image.sh
    Bump raspberry pi firmware version

--- rootfs/scripts/create_raspi_base_image.sh
+++ rootfs/scripts/create_raspi_base_image.sh
@@ -74,7 +74,7 @@ ROOTFS_BUILD_TAG="$(cd "$(dirname "$0")"; git describe --tags HEAD | sed 's/_/~/
 DATE="$(date +%Y%m%d)"
 WORKDIR="$(mktemp -d)"
 STARTDIR="$(pwd)"
-RASPI_FIRMWARE_VERSION=1.20240926
+RASPI_FIRMWARE_VERSION=1.20250305
 
 # Default options that might be overridden
 ROOTFS_BUILD_PATH="$TOPLEVEL/rootfs"
