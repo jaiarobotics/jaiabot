719a13399c9e1f84c19aa2bfba536b8723934fec
michael-twomey <michael-twomey1@outlook.com>
2025-04-01 10:55:44 -0400

    Point to first character versus location of string

--- src/bin/sensors/app.cpp
+++ src/bin/sensors/app.cpp
@@ -120,7 +120,7 @@ void jaiabot::apps::Sensors::send_to_mcu(sensor::protobuf::SensorRequest request
     std::string* encoded = io_msg->mutable_data();
     request.SerializeToString(encoded);
 
-    uint32_t crc32_value = crc::calculate_crc32(encoded, encoded->size());
+    uint32_t crc32_value = crc::calculate_crc32(encoded->data(), encoded->size());
 
     constexpr int bits_in_byte = 8;
     constexpr int bytes_in_crc32 = 4;
