8b05d1a54afadd95f18904afe7882ed6a4a13827
JRoybot <jeff@roy.net>
2025-04-07 12:48:50 -0400

    fix entrypoint again

--- scripts/sim-docker/entrypoint.sh
+++ scripts/sim-docker/entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 export jaia_fleet_index=$JAIA_SIM_FLEET
 export JAIA_REST_API_PRIVATE_KEY=simulation
-cd /usr/share/jaiabot/web/server
 source /usr/share/jaiabot/python/venv/bin/activate
+cd /usr/share/jaiabot/web/server
 ./app.py &
 cd /usr/share/jaiabot/web/rest_api/
-./app.py localhost &
+./app.py &
 cd /usr/share/jaiabot/config/launch/simulation
 ./generate_all_launch.sh $JAIA_SIM_BOTS $JAIA_SIM_WARP
 ./all.launch 
