5e865b312605434831a0f0162e6846d0c0161602
JRoybot <jeff@roy.net>
2025-04-08 09:35:25 -0400

    launch JDV from entrypoint

--- scripts/sim-docker/entrypoint.sh
+++ scripts/sim-docker/entrypoint.sh
@@ -6,6 +6,8 @@ cd /usr/share/jaiabot/web/server
 ./app.py &
 cd /usr/share/jaiabot/web/rest_api/
 ./app.py &
+cd /usr/share/jaiabot/web/jdv/server
+python3 jaiabot_data_vision.py -d /var/log/jaiabot/bot_offload -a /usr/share/jaiat/web/jdv/client/dist/ -p 40011
 cd /usr/share/jaiabot/config/launch/simulation
 ./generate_all_launch.sh $JAIA_SIM_BOTS $JAIA_SIM_WARP
 ./all.launch 
