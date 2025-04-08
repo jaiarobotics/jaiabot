440675e51ac49478ffb779aaaa071622927890d9
JRoybot <jeff@roy.net>
2025-04-08 10:58:50 -0400

    debugging entrypoint

--- scripts/sim-docker/entrypoint.sh
+++ scripts/sim-docker/entrypoint.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 export jaia_fleet_index=$JAIA_SIM_FLEET
 export JAIA_REST_API_PRIVATE_KEY=simulation
+cd /usr/share/jaiabot/config/launch/simulation
+./generate_all_launch.sh $JAIA_SIM_BOTS $JAIA_SIM_WARP
+./all.launch &
 source /usr/share/jaiabot/python/venv/bin/activate
 cd /usr/share/jaiabot/web/server
 ./app.py &
 cd /usr/share/jaiabot/web/rest_api/
 ./app.py &
 cd /usr/share/jaiabot/web/jdv/server
-python3 jaiabot_data_vision.py -d /var/log/jaiabot/bot_offload -a /usr/share/jaiat/web/jdv/client/dist/ -p 40011
-cd /usr/share/jaiabot/config/launch/simulation
-./generate_all_launch.sh $JAIA_SIM_BOTS $JAIA_SIM_WARP
-./all.launch 
+python3 jaiabot_data_vision.py -d /var/log/jaiabot/bot_offload -a /usr/share/jaiat/web/jdv/client/dist/ -p 40011 &
+
