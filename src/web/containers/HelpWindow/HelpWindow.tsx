a5779472a4f3270791f20300c3a555e03be28cb6
Kait211 <126176210+Kait211@users.noreply.github.com>
2025-04-08 17:04:08 -0700

    Update HelpWindow.tsx
    changes made to the help panel still a working progress.

--- src/web/containers/HelpWindow/HelpWindow.tsx
+++ src/web/containers/HelpWindow/HelpWindow.tsx
@@ -12,9 +12,21 @@ import {
     mdiDownloadMultiple,
     mdiProgressDownload,
     mdiCog,
-    mdiWrench,
     mdiSquareEditOutline,
     mdiRuler,
+    mdiRotate3dVariant,
+    mdiMagnifyPlusOutline,
+    mdiMagnifyMinusOutline,
+    mdiPower,
+    mdiRestartAlert,
+    mdiRestart,
+    mdiChartLine,
+    mdiWifiCog,
+    mdiWrenchCog,
+    mdiController,
+    mdiSkipNext,
+    mdiDownload,
+    mdiDelete,
 } from "@mdi/js";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { IconDefinition } from "@fortawesome/fontawesome-common-types";
@@ -56,16 +68,12 @@ function getButtonRow(descriptor: ButtonDescriptor) {
 }
 
 const buttons: ButtonDescriptor[] = [
+    //here
     {
         iconPath: mdiCheckboxMarkedCirclePlusOutline,
         name: "System Check All Bots",
         description:
-            "Test !Run a system check on all bots in the pod.  Missions can only be run after the system check completes successfully.",
-    },
-    {
-        imgSrc: rallyIcon,
-        name: "Add Rally Point",
-        description: "Add a rally point that can be used at the start or end of a survey mission.",
+            "Run a system check on all bots in the pod.  Missions can only be run after the system check completes successfully.",
     },
     {
         iconPath: mdiStop,
@@ -79,6 +87,11 @@ const buttons: ButtonDescriptor[] = [
         description: "Run the mission as currently edited on the map.",
     },
     {
+        iconPath: mdiDownloadMultiple,
+        name: "Download All",
+        description: "Start downloading log and sensor data from all of the bots to the hub.",
+    },
+    {
         iconPath: mdiArrowULeftTop,
         name: "Undo",
         description: "Undo the last 10 mission planning actions (excluding Task modifications).",
@@ -87,7 +100,12 @@ const buttons: ButtonDescriptor[] = [
         iconPath: mdiViewList,
         name: "Mission Panel",
         description:
-            "Open the Mission Panel, which shows each run in the current mission, and which bots are assigned to which run.  Missions can also be saved, loaded, and cleared from this panel.",
+            "Open the Mission Panel, which shows each run in the current mission, along with the bots assigned to each run. Bots can be auto-assigned to runs automatically. Missions can also be saved, loaded, and cleared from this panel",
+    },
+    {
+        imgSrc: rallyIcon,
+        name: "Add Rally Point",
+        description: "Add a rally point that can be used at the start or end of a survey mission.",
     },
     {
         iconPath: mdiSquareEditOutline,
@@ -96,15 +114,16 @@ const buttons: ButtonDescriptor[] = [
             "Open the Optimized Mission Survey, which can be used to configure a survey mission, where the pod of Jaiabots will coordinate to survey an area of the map.  The tasks performed at each waypoint in the survey mission can be customized.",
     },
     {
-        iconPath: mdiDownloadMultiple,
-        name: "Download All",
-        description: "Start downloading log and sensor data from all of the bots to the hub.",
-    },
-    {
         iconPath: mdiProgressDownload,
         name: "Download Queue",
         description:
             "Open the Download Queue panel, which shows the currently queued data downloads from the bots to the hub.",
+    },
+    {
+        iconPath: mdiRuler,
+        name: "Measure Distance",
+        description:
+            "Click two or more points to measure the total distance along a set of line segments.",
     },
     {
         iconPath: mdiCog,
@@ -112,10 +131,82 @@ const buttons: ButtonDescriptor[] = [
         description: "Open the Settings panel for Jaia Command & Control.",
     },
     {
-        iconPath: mdiRuler,
-        name: "Measure Distance",
-        description:
-            "Click two or more points to measure the total distance along a set of line segments.",
+        iconPath: mdiRotate3dVariant,
+        name: "Reset rotation",
+        description: "When using tablet, click to reset rotation of the map.",
+    },
+    {
+        iconPath: mdiMagnifyPlusOutline,
+        name: "Zoom in",
+        description: "Increase the map's magnification.",
+    },
+    {
+        iconPath: mdiMagnifyMinusOutline,
+        name: "Zoom out",
+        description: "Decrease the map's magnification.",
+    },
+    //here
+    {
+        iconPath: mdiPower,
+        name: "Shutdown",
+        description: "Shutdowns the system for the Hub.",
+    },
+    {
+        iconPath: mdiRestartAlert,
+        name: "Reboot",
+        description: "Reboots the Hub.",
+    },
+    {
+        iconPath: mdiRestart,
+        name: "Restart",
+        description: "Restart the Hub.",
+    },
+    {
+        iconPath: mdiChartLine,
+        name: "test",
+        description: "test",
+    },
+    {
+        iconPath: mdiWifiCog,
+        name: "test",
+        description: "test",
+    },
+    {
+        iconPath: mdiWrenchCog,
+        name: "Upgrade",
+        description: "test",
+    },
+    //here
+    {
+        iconPath: mdiStop,
+        name: "Stop",
+        description: "test",
+    },
+    {
+        iconPath: mdiPlay,
+        name: "RC mode",
+        description: "test",
+    },
+    {
+        iconPath: mdiDelete,
+        name: "Clear Mission",
+        description: "test",
+    },
+    //here
+    {
+        iconPath: mdiController,
+        name: "RC mode",
+        description: "test",
+    },
+    {
+        iconPath: mdiSkipNext,
+        name: "test",
+        description: "test",
+    },
+    {
+        iconPath: mdiDownload,
+        name: "test",
+        description: "test",
     },
 ];
 
@@ -137,12 +228,47 @@ export function HelpWindow(props: Props) {
             <div className="help-titlebar">
                 <div className="help-title">Jaia Command & Control Help</div>
                 <Button onClick={props.onClose}>
-                    <Icon path={mdiWindowClose} title="Close Window"></Icon>
+                    <Icon path={mdiWindowClose} title="Close Window" />
                 </Button>
             </div>
             <table className="help-button-table">
-                <tbody>{buttons.map(getButtonRow)}</tbody>
+                <tbody>
+                    {buttons.map((button, index) => (
+                        <>
+                            {index === 0 && (
+                                <tr className="instruction-note-row">
+                                    <td colSpan={3} className="instruction-note">
+                                        Commands in the commandsDrawer.
+                                    </td>
+                                </tr>
+                            )}
+                            {index === 14 && (
+                                <tr className="instruction-note-row">
+                                    <td colSpan={3} className="instruction-note">
+                                        Commands under the Hub.
+                                    </td>
+                                </tr>
+                            )}
+                            {index === 17 && (
+                                <tr className="instruction-note-row">
+                                    <td colSpan={3} className="instruction-note">
+                                        Links under the Hub.
+                                    </td>
+                                </tr>
+                            )}
+                            {index === 20 && (
+                                <tr className="instruction-note-row">
+                                    <td colSpan={3} className="instruction-note">
+                                        Test.
+                                    </td>
+                                </tr>
+                            )}
+                            {getButtonRow(button)}
+                        </>
+                    ))}
+                </tbody>
             </table>
+            <div className="help-footer-text">For more help</div>
         </div>
     );
 }
