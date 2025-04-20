063aaf008285371518abb28284e2cc27cb2afa0c
Ed Sanville <edsanville@gmail.com>
2025-04-19 20:58:13 -0400

    Added raw_salinity field to the BotStatus, and store the raw salinity from the chip there.  Another `salinity` field will be used for the temperature and pressure compensated version of salinity.

--- src/bin/fusion/fusion.cpp
+++ src/bin/fusion/fusion.cpp
@@ -496,7 +496,7 @@ jaiabot::apps::Fusion::Fusion() : ApplicationBase(5 * si::hertz)
     interprocess().subscribe<jaiabot::groups::salinity>(
         [this](const jaiabot::protobuf::SalinityData& salinityData) {
             glog.is_debug1() && glog << "=> " << salinityData.ShortDebugString() << std::endl;
-            latest_bot_status_.set_salinity(salinityData.salinity());
+            latest_bot_status_.set_raw_salinity(salinityData.salinity());
         });
 
     interprocess().subscribe<goby::middleware::groups::health_report>(
