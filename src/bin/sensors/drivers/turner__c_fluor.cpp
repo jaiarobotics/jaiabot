4b272a205f61a64f066872dea5286a5c3108265c
Nick Marshall <nickjrmarshall@gmail.com>
2025-04-28 15:22:41 -0400

    Adding fluorometer field to CalibrationCoefficients message

--- src/bin/sensors/drivers/turner__c_fluor.cpp
+++ src/bin/sensors/drivers/turner__c_fluor.cpp
@@ -107,15 +107,15 @@ void jaiabot::apps::TurnerCFluorDriver::send_cfg()
 
     auto* cal_offset = sensor_cfg.add_cfg();
     cal_offset->set_key("offset");
-    cal_offset->set_value(std::to_string(existing_calibration.fluorometer_offset()));
+    cal_offset->set_value(std::to_string(existing_calibration.fluorometer().fluorometer_offset()));
 
     auto* cal_coefficient = sensor_cfg.add_cfg();
     cal_coefficient->set_key("coefficient");
-    cal_coefficient->set_value(existing_calibration.fluorometer_calibration_coefficient());
+    cal_coefficient->set_value(existing_calibration.fluorometer().fluorometer_calibration_coefficient());
 
     auto* fluorometer_sn = sensor_cfg.add_cfg();
     fluorometer_sn->set_key("serial_number");
-    fluorometer_sn->set_value(existing_calibration.fluorometer_serial_number());
+    fluorometer_sn->set_value(std::to_string(existing_calibration.fluorometer().fluorometer_serial_number()));
 
     interprocess().publish<jaiabot::groups::mcu_pb_data_out>(request);
 }
