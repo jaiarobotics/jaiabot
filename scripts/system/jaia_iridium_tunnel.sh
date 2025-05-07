785a02de1859087c307f6834f8aac530999819c3
Toby Schneider <toby@gobysoft.org>
2025-05-07 16:58:55 +0100

    Switch to 19200 baud, remove IPR command that doesn't want to be handled on this modem, and fix IPv6 framing in socat

--- scripts/system/jaia_iridium_tunnel.sh
+++ scripts/system/jaia_iridium_tunnel.sh
@@ -12,7 +12,7 @@ JAIA_IRIDIUM_MO_PORT_BASE=$((IRIDIUM_MT_PORT+1000))  # Add FLEET to get port for
 CLOUDHUB_IRIDIUM_MO_PORT=${JAIA_IRIDIUM_MO_PORT_BASE}
 
 # MO Tunnel
-(set -x; /usr/bin/socat TCP-LISTEN:$((FLEET+${JAIA_IRIDIUM_MO_PORT_BASE})),reuseaddr,fork TCP:$(jaia ip chf${FLEET}):${CLOUDHUB_IRIDIUM_MO_PORT})&
+(set -x; /usr/bin/socat TCP-LISTEN:$((FLEET+${JAIA_IRIDIUM_MO_PORT_BASE})),reuseaddr,fork TCP:[$(jaia ip chf${FLEET})]:${CLOUDHUB_IRIDIUM_MO_PORT})&
 
 # MT Tunnel
 (set -x; /usr/bin/socat TCP-LISTEN:$((FLEET+${JAIA_IRIDIUM_MT_PORT_BASE})),reuseaddr,fork TCP:directip.sbd.iridium.com:${IRIDIUM_MT_PORT})&
