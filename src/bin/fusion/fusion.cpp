fa9e211bf5a66b58e8050a25a5f6bfe9df54d098
Nick Marshall <98423695+marshalllele@users.noreply.github.com>
2025-05-27 16:09:36 -0400

    feature/report-specific-conductivity (#1165)
    * Adding conductivity temperature compensation to the base code for Hydro/PAm vehicles
    
    * Pushing to test branch
    
    * Tested and working in sim. Applied a value of 45,000 to the conductivity and am seeing ~55,600 for specific conductivity in liaison
    
    * Adding in unnit testing for conductivity compensation based on temperature
    
    * o run unit tests for specific conductivity, rebuild using './build.sh --enable-testing', then run 'ctest --test-dir build/arm64/ --output-on-failure --verbose' from within the jaiabot directory.
    
    * Moving STM32 side of calculations to a seperate branch
    
    * Updated the specific conductivity function to use the viscosity method, it was found to closely represent all the test cases under 2%
    
    * Updating hdf5 documentation
    
    * Adding script to get Polynomial values for the tuning of the b factor in the specific conductivity calculation
    
    * Updated stm32 code to reflect the proto changes
    
    * Add missing python helper script for testing specific conductivity
    
    * Add references and update function comments
    
    * Add salinity and specific conductivity calculations in the oem ec class (BIO and future Hydros)
    
    * Rename python script for specific conductivity to better reflect what it is actually doing
    
    * Update specific conductivity calculation to use linear equation for conductivity values under 8000 uS/cm
    
    * Change variable naming for conductivity, conductivity raw is not temperature compensated, conductivity is temperature compensated
    
    * For now use conductivity raw in the salinity calculation
    
    * Updated function comment
    
    * Update build.sh
    
    * Changing salinity field names from salinity_chip to salinity_raw, and salinity_calculated to salinity
    
    ---------
    
    Co-authored-by: Matthew Ferro <matt.ferro@jaia.tech>

--- src/bin/fusion/fusion.cpp
+++ src/bin/fusion/fusion.cpp
@@ -426,8 +426,7 @@ jaiabot::apps::Fusion::Fusion() : ApplicationBase(5 * si::hertz)
 
     // subscribe for pressure adjusted measurements (pressure -> depth)
     interprocess().subscribe<jaiabot::groups::pressure_adjusted>(
-        [this](const jaiabot::protobuf::PressureAdjustedData& pa)
-        {
+        [this](const jaiabot::protobuf::PressureAdjustedData& pa) {
             if (pa.has_calculated_depth())
             {
                 latest_node_status_.mutable_global_fix()->set_depth_with_units(
@@ -496,11 +495,10 @@ jaiabot::apps::Fusion::Fusion() : ApplicationBase(5 * si::hertz)
         });
 
     interprocess().subscribe<jaiabot::groups::salinity>(
-        [this](const jaiabot::protobuf::SalinityData& salinity_data)
-        {
-            if (salinity_data.has_salinity_calculated())
+        [this](const jaiabot::protobuf::SalinityData& salinity_data) {
+            if (salinity_data.has_salinity())
             {
-                latest_bot_status_.set_salinity(salinity_data.salinity_calculated());
+                latest_bot_status_.set_salinity(salinity_data.salinity());
             }
         });
 
