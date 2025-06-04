b52314f991f270bd4e27085e3cf1baf631b3f680
Michael Twomey <109538793+michael-jaia@users.noreply.github.com>
2025-06-04 11:10:56 -0400

    task/add-back-jaia-button (#1202)
    * Integrate JaiaAbout window into panel structure
    
    * Add content to JaiaAbout window
    
    * Add jaia-button to function output

--- src/web/types/context-types.ts
+++ src/web/types/context-types.ts
@@ -13,7 +13,7 @@ export interface HubAccordionStates {
 export const enum BotAccordionNames {
     QUICKLOOK = "quickLook",
     COMMANDS = "commands",
-    ADVANCEDCOMMANDS = "advancedCommands",
+    ADVANCED_COMMANDS = "advanced_commands",
     HEALTH = "health",
     DATA = "data",
     GPS = "gps",
@@ -50,4 +50,5 @@ export const enum PanelNames {
     NONE = "none",
     MISSIONS = "missions",
     WAYPOINT = "waypoint",
+    JAIA_ABOUT = "jaia_about",
 }
