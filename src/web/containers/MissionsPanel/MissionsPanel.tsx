e3983906a724a9e0746a6dab3d08845dbc31f8d0
Michael Twomey <109538793+michael-jaia@users.noreply.github.com>
2025-06-06 15:42:51 -0400

    task/auto-scroll-accordions (#1217)
    * Move accordion themes outside of components
    
    * Scroll into view Bot details accordions
    
    * Hook up auto scroll accordions in HubDetails
    
    * Implement autoscroll into missions panel
    
    * Scroll duplicate missions into view
    
    * Tidy up test output

--- src/web/containers/MissionsPanel/MissionsPanel.tsx
+++ src/web/containers/MissionsPanel/MissionsPanel.tsx
@@ -5,13 +5,14 @@ import { useContext } from "react";
 import MissionsList from "./MissionsList/MissionsList";
 import DeleteMissionButton from "../../components/DeleteMissionButton/DeleteMissionButton";
 import MissionSpeedSliders from "../../components/MissionSpeedSliders/MissionSpeedSliders";
-import { JaiaContext, JaiaDispatchContext } from "../../context/JaiaContext";
+import { JaiaDispatchContext } from "../../context/JaiaContext";
 import { JaiaActions } from "../../context/jaia-actions";
+import { scrollMissionsList } from "../../utils/style";
 
 // MUI | MDI
 import Button from "@mui/material/Button";
 import Icon from "@mdi/react";
-import { mdiAutoFix, mdiContentSave, mdiDelete, mdiFolderOpen, mdiPlus } from "@mdi/js";
+import { mdiAutoFix, mdiContentSave, mdiFolderOpen, mdiPlus } from "@mdi/js";
 
 import "./MissionsPanel.less";
 
@@ -19,7 +20,6 @@ import "./MissionsPanel.less";
  * Renders a panel for operators to manage missions
  */
 export default function MissionsPanel() {
-    const jaiaContext = useContext(JaiaContext);
     const jaiaDispatch = useContext(JaiaDispatchContext);
 
     /**
@@ -29,6 +29,7 @@ export default function MissionsPanel() {
      */
     const handleAddMissionClick = () => {
         jaiaDispatch({ type: JaiaActions.ADD_MISSION });
+        scrollMissionsList();
     };
 
     /**
