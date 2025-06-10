dafc118176e32d286febf4fc9b9af4c5c3910dbd
Grace Infante <graziella_infante@brown.edu>
2025-06-10 19:10:49 +0000

    Added a check so docker doesn't run when code isn't deployed.

--- scripts/setup-tools-build.sh
+++ scripts/setup-tools-build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
-
-script_dir=$(dirname $0)
-
-${script_dir}/setup-tools-build-nodocker.sh
-${script_dir}/setup-tools-build-docker.sh
+script_dir=$(dirname "$0")
+# Always run the non-docker setup
+"${script_dir}/setup-tools-build-nodocker.sh"
+# Only run docker setup if DEPLOY env var is set
+if [[ "$DEPLOY" == "true" ]]; then
+    "${script_dir}/setup-tools-build-docker.sh"
+fi
