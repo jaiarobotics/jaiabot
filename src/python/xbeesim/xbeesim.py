a55daea7405eb6251e12643c353010e01a48ff9c
Sean Fish <seantfish@gmail.com>
2025-06-26 15:54:30 -0400

    Virtual Serial Device

--- src/python/xbeesim/xbeesim.py
+++ src/python/xbeesim/xbeesim.py
@@ -3,25 +3,15 @@
 import os
 import pty
 import tty
+import vserial
 
 class SimXBee():
     def __init__(self, name='pxbee'):
-        self.device_name = name
-        self.device_path = '/tmp/' + name
+        self.name = name
+        self.port = '/tmp/' + name
+        self.mode = 'transparent'
 
-    def cleanup(self):
-        if os.path.exists(self.device_path):
-            os.remove(self.device_path)
-
-    def __enter__(self):
-        master_fd, slave_fd = pty.openpty()
-        slave_name = os.ttyname(slave_fd)
-        self.cleanup()
-        os.symlink(slave_name, self.device_path)
-        print(f"Fake tty at {self.device_path}")
-
-    def __exit__(self, exc_type, exc_value, traceback):
-        self.cleanup()
+        self.ser = vserial.VirtualSerialDevice(self.port)
 
 def main():
     with SimXBee() as xbee:
