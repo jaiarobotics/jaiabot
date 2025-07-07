383a254a9a2e299f5cb68ea028fdc7e9beb02a9c
Kaitlyn Habib <kaitlyn.habib@jaia.tech>
2025-07-07 13:10:19 +0200

    backup commit

--- src/bin/production/app.cpp
+++ src/bin/production/app.cpp
@@ -250,9 +250,9 @@ void jaiabot::apps::JaiabotProduction::imu_sensor_data_timeCheck()
         //take line below away later just for testing
         glog.is_debug1() && glog << "⏳ Still in IMU reset window, ⏱️ time since last imu message" << since_last_imu << "s)" << std::endl;
         //response.set_imu_reset_response("sent reset request waiting 2 seconds"); //make sure this prints and show seconds that we did not get imu data maybe make own message yeah
-        std::ostringstream oss;
-        oss << "finished reset, received no IMU data for " << since_last_imu << "s";
-        response.set_imu_reset_response(oss.str());
+        std::ostringstream reset_oss;
+        reset_oss << "finished reset, received no IMU data for " << since_last_imu << "s";
+        response.set_imu_reset_response(reset_oss.str());
         return;
     }
 
@@ -267,9 +267,9 @@ void jaiabot::apps::JaiabotProduction::imu_sensor_data_timeCheck()
     {
         glog.is_debug1() && glog << "✅ IMU Test PASS: IMU data received in " 
                                  << since_last_imu << "s" << std::endl;
-        std::ostringstream oss_1;
-        oss_1 << "pass_imu_data_received, last imu message sent " << since_last_imu << "s";
-        response.set_imu_response(oss_1.str());
+        std::ostringstream IMU_oss;
+        IMU_oss << "pass_imu_data_received, last imu message sent " << since_last_imu << "s";
+        response.set_imu_response(IMU_oss.str());
     }
 
     // Timestamp it
@@ -475,11 +475,8 @@ void jaiabot::apps::JaiabotProduction::loop()
     {
         imu_sensor_data_timeCheck();
         imu_sensor_reset_check();
-
-        if (imu_reset_complete_)
-        {
-            interprocess().publish<jaiabot::groups::production>(response);
-        }
+        interprocess().publish<jaiabot::groups::production>(response);
+    
     }
     //check_imu();
 
