a8182bc9ffa028d074cbb9910b7f52b55ce03384
Sean Fish <seantfish@gmail.com>
2025-07-16 17:28:24 -0400

    Moved preseed to be generated

--- config/launch/simulation/generate_all_launch.sh
+++ config/launch/simulation/generate_all_launch.sh
@@ -5,11 +5,12 @@ warp=5
 comms_mode="w"
 
 script_dir=$(dirname $0)
+preseedfile=${script_dir}/preseed.goby
 launchfile=${script_dir}/all.launch
 warpfile=${script_dir}/../../gen/common/sim.py
 
 if [[ "$1" == "-h" || "$1" == "--help" ]]; then
-    echo "Usage: generate_all_launch.sh [n_bots, default ${n_bots}] [warp, default ${warp}]"
+    echo "Usage: generate_all_launch.sh [n_bots, default ${n_bots}] [warp, default ${warp}] [(x)bee or (w)ifi comms, default ${comms_mode}]"
     exit;
 fi
 
@@ -20,6 +21,10 @@ fi
 if [ ! -z "$2" ]; then
     warp="$2"
 fi
+if [ ! -z "$3" ]; then
+    comms_mode="$3"
+fi
+
 if [[ "$comms_mode" == *x* ]]; then
     jaia_comms_mode="xbee"
 fi
@@ -33,6 +38,13 @@ fi
 ${script_dir}/../../../rootfs/customization/includes.chroot/etc/jaiabot/init/jaia-create-ansible-inventory.sh -b $(seq -s , 1 ${n_bots}) -h 1 -f 0 > /etc/jaiabot/inventory.yml
 
 launchdelay=100
+
+cat <<EOF > ${preseedfile}
+export jaia_mode=simulation
+export jaia_comms_mode=${jaia_comms_mode}
+source "$(dirname ${BASH_SOURCE:-${(%):-%x}})/../../preseed.goby"
+EOF
+
 
 cat <<EOF > ${launchfile}
 #!/usr/bin/env -S goby_launch -s -P -k30 -pall -d500 -L
