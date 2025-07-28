4a821c9e28afe9fc95e25e03fbee78af27c38ae6
Kaitlyn Habib <kaitlyn.habib@jaia.tech>
2025-07-28 14:20:49 +0200

    set speed to 2 to get rpm 3600 or more

--- src/bin/production/app.cpp
+++ src/bin/production/app.cpp
@@ -502,7 +502,7 @@ void jaiabot::apps::JaiabotProduction::motor_harness()
 
             auto* rc = setpoint.mutable_remote_control();
             rc->set_duration(2); // 2 seconds
-            rc->set_speed(1.0);   // Speed in m/s
+            rc->set_speed(2.0);   // Speed in m/s
             rc->set_heading(0);   // Straight forward
 
             interprocess().publish<jaiabot::groups::desired_setpoints>(setpoint);
