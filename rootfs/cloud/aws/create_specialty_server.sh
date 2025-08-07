f31eb94d811bdfad63a714a885c0fcba306acb8d
Toby Schneider <toby@gobysoft.org>
2025-08-07 11:22:03 -0400

    Add IPv6 addresses for servers that have them - Update docs.jaia.tech Apache config

--- rootfs/cloud/aws/create_specialty_server.sh
+++ rootfs/cloud/aws/create_specialty_server.sh
@@ -16,6 +16,7 @@ REGION=us-west-2
 SUBNET_ID=subnet-f5f17b8d
 DISK_SIZE_GB=8
 ELASTIC_IP_ALLOCATION_ID=
+IPV6_ADDRESS=
 
 set -a; source $1; set +a;
 
@@ -53,6 +54,10 @@ network_interfaces_json=$(jq -n -c \
                      }
                    ]')
 
+
+if [[ ! -z "${IPV6_ADDRESS}" ]]; then
+    network_interfaces_json=$(echo $network_interfaces_json | jq -c --arg ipv6 "${IPV6_ADDRESS}" '.[0] += {"Ipv6Addresses": [{"Ipv6Address": $ipv6}]}')
+fi
 
 USER_DATA_SCRIPT_IN="${SCRIPT_PATH}/${SERVER_TYPE}/user-data.sh.in"
 USER_DATA_SCRIPT="${TMPDIR}/user-data.sh"
