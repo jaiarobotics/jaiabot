2ee7a8c8d26caa88a6843b4b6f0b7d3a4ac30ca7
michael-twomey <michael-twomey1@outlook.com>
2025-08-19 17:10:30 -0400

    Create base structure for query Bot status panel

--- src/web/containers/__engineering__/QueryBotStatusPanel/QueryBotStatusPanel.tsx
+++ src/web/containers/__engineering__/QueryBotStatusPanel/QueryBotStatusPanel.tsx
@@ -1,81 +1,27 @@
-/* eslint-disable jsx-a11y/label-has-for */
-/* eslint-disable jsx-a11y/label-has-associated-control */
-/* eslint-disable react/sort-comp */
-/* eslint-disable no-unused-vars */
-
-import React from "react";
-import { error, success, warning, info, debug } from "../../../utils/notifications";
 import Button from "@mui/material/Button";
-import {
-    BotStatus,
-    Engineering,
-    BotStatusRate,
-    PIDControl,
-    RFDisableOptions,
-} from "../../../types/protobuf-types";
-import { JaiaAPI } from "../../../utils/jaia-api";
-import { getElementById } from "../../../shared/Utilities";
+import { useState } from "react";
+import { jaiaAPI } from "../../../utils/jaia-api";
+import { Engineering } from "../../../types/protobuf-types";
 
-interface Props {
-    api: JaiaAPI;
-    control: (onSuccess: () => void) => void;
-}
+export default function QueryBotStatusPanel() {
+    const [botID, setBotID] = useState(0);
+    const submitQueryBotStatus = () => {
+        const engineeringCommand: Engineering = {
+            bot_id: botID,
+            query_bot_status: true,
+        };
 
-export default class QueryBotStatusPanel extends React.Component {
-    props: Props;
+        jaiaAPI.postEngineeringPanel(engineeringCommand);
+    };
 
-    constructor(props: Props) {
-        super(props);
-    }
-
-    render() {
-        return (
-            <div className="panel">
-                <label>Query Bot Status</label>
-                <table>
-                    <tbody>
-                        <tr key="bot_to_query">
-                            <td>Bot-ID</td>
-                            <td>
-                                <input
-                                    style={{ maxWidth: "80px" }}
-                                    type="number"
-                                    id="query_bot_status_input"
-                                    name="query_bot_status_input"
-                                    defaultValue="0"
-                                    min="0"
-                                    max="30"
-                                    step="1"
-                                />
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-                <Button
-                    className="button-jcc engineering-panel-btn"
-                    type="button"
-                    id="submit_query_bot_status"
-                    onClick={this.submitQueryBotStatus.bind(this)}
-                >
-                    Query Bot Status
-                </Button>
+    return (
+        <div>
+            <label>Query Bot Status</label>
+            <div>
+                <div>Bot ID</div>
+                <input />
             </div>
-        );
-    }
-
-    submitQueryBotStatus() {
-        this.props.control(() => {
-            let botId = Number(getElementById<HTMLInputElement>("query_bot_status_input").value);
-            info("Query Bot Status for botId: " + botId);
-
-            let engineeringCommand: Engineering = {
-                bot_id: botId,
-                query_bot_status: true,
-            };
-
-            debug(JSON.stringify(engineeringCommand));
-
-            this.props.api.postEngineeringPanel(engineeringCommand);
-        });
-    }
+            <button onClick={submitQueryBotStatus}>Query Bot Status</button>
+        </div>
+    );
 }
