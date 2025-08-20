0286f7ef2b774b035d379fc0014a6ae345802f04
Jeff Roy <153303638+JRoybot@users.noreply.github.com>
2025-08-20 15:26:34 -0400

    task/2.y/dev-friendly-jcc/save-mission-panel-2/jaia-1865 (#1302)
    * initial SaveMissionPanel
    
    * Save Panel HTML closer to original
    
    * WIP Save Mission Panel
    
    * update to new file structure of mission_sets
    
    * Initial set up of load/save mission set button and dialog
    
    * implement cancel
    
    * Basic load, save and delete mission set implementation
    
    * close dialog on save and load
    
    * Functional still working on style
    
    * fix import
    
    * remove unused imports
    
    * framework for button dialogs
    
    * initial save functionality
    
    * load mission set fiunctionality
    
    * delete functionality
    
    * clean up
    
    * add file name to dialog messages
    
    * close parent dialog on some actions
    
    * Use jaiaAction for load mission set to keep context in sync with the data model.
    
    * remove unneeded CONFIRM disable codes
    
    * cleanup
    
    * resolve review comments
    
    * remove incorrect mod of prop variable
    
    * Style mission set storage dialog
    
    * Remove unused imports
    
    * Format delete mission set dialog + actions
    
    * Format load mission set dialog + actions
    
    * Format save mission set dialog + actions
    
    * rename LoadSaveMissionSet to MissionSetStorage
    
    * alternative handling of save name on delete mission set
    
    * Rename less file
    
    * Reset mission set name on deletion in data model
    
    * Add docstring + newline breaks
    
    * Move mission set row into its own component + add docstrings
    
    * Modify dialog closing behavior
    
    * Change behavior of save name on delete actions, other review comment clean up
    
    * change resetSaveName back to clearSaveName, remove unused import
    
    * Prevent close of dialog on save + set min height
    
    ---------
    
    Co-authored-by: Michael Twomey <109538793+michael-jaia@users.noreply.github.com>
    Co-authored-by: michael-twomey <michael-twomey1@outlook.com>

---
+++ src/web/components/MissionSetStorage/SaveMissionSetButton/SaveMissionSetButton.tsx
@@ -0,0 +1,71 @@
+import { useState } from "react";
+import { missionSet } from "../../../data/mission_set/mission-set";
+import { DialogActions } from "../../../types/context-types";
+import { listSavedMissionSets } from "../../../utils/local-storage";
+import { DisabledCodes } from "./save-messages";
+import { SaveMissionSetDialog } from "./SaveMissionSetDialog";
+
+interface Props {
+    saveName: string;
+}
+
+/**
+ * Produces the save mission set button.
+ * It manages the alert/confirm dialog that appears when clicking on the button.
+ */
+export default function SaveMissionSetButton(props: Props) {
+    const [isDialogVisible, setIsDialogVisible] = useState(false);
+
+    /**
+     * Checks the mission set and applies the appropriate disable code
+     *
+     * @returns {DisabledCodes} The applicable disabled code based on the Mission Set conditions
+     */
+    const getDisabledCode = () => {
+        if (missionSet.getMissions().size == 0) return DisabledCodes.NO_MISSIONS;
+        if (props.saveName == "") return DisabledCodes.NO_NAME;
+        if (listSavedMissionSets().includes(props.saveName)) return DisabledCodes.OVERWRITE;
+        return DisabledCodes.NONE;
+    };
+
+    /**
+     * Displays dialog if a warning condition exist or saves the mission set
+     *
+     * @returns {void}
+     */
+    const onButtonClick = () => {
+        if (getDisabledCode() !== DisabledCodes.NONE) {
+            setIsDialogVisible(true);
+        } else {
+            onDialogClose(DialogActions.CONFIRMED);
+        }
+    };
+
+    /**
+     * Closes the dialog box then acts based on the button clicked
+     *
+     * @param {DialogActions} dialogAction Indicates which button was clicked
+     * @returns {void}
+     *
+     */
+    const onDialogClose = (dialogAction: DialogActions) => {
+        setIsDialogVisible(false);
+        if (dialogAction === DialogActions.CONFIRMED) {
+            missionSet.saveToLocalStorage(props.saveName);
+        }
+    };
+
+    return (
+        <div>
+            <button aria-label={"save-mission-set"} onClick={() => onButtonClick()}>
+                Save
+            </button>
+            <SaveMissionSetDialog
+                isVisible={isDialogVisible}
+                disabledCode={getDisabledCode()}
+                saveName={props.saveName}
+                onClose={onDialogClose}
+            />
+        </div>
+    );
+}
