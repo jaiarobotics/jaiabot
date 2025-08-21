f41c569b668f46f9b25926a72edaac360bd26b02
michael-twomey <michael-twomey1@outlook.com>
2025-08-21 14:53:00 -0400

    Display track icons on map

--- src/web/openlayers/features/sentinel/track-feature.ts
+++ src/web/openlayers/features/sentinel/track-feature.ts
@@ -1,10 +1,24 @@
 import { Feature } from "ol";
-import { Style } from "ol/style";
+import { Point } from "ol/geom";
+import { Coordinate } from "ol/coordinate";
+import { fromLonLat } from "ol/proj";
+import { Icon, Style } from "ol/style";
+
+import { view } from "../../views/view";
 import { Track } from "../../../types/protobuf-types";
 import { MapFeatureTypes } from "../../../types/openlayers-types";
 
+import trackIcon from "../../../style/icons/sentinel/track-icon.svg";
+
 export function generateTrackFeature(track: Track) {
-    const feature = new Feature();
+    if (!track.location) {
+        return new Feature();
+    }
+
+    const coordinate: Coordinate = [track.location.lon, track.location.lat];
+    const feature = new Feature({
+        geometry: new Point(fromLonLat(coordinate, view.getProjection())),
+    });
     feature.set("type", MapFeatureTypes.SENTINAL_TRACK);
     feature.set("id", track.id);
     feature.setStyle(generateTrackStyle());
@@ -12,5 +26,10 @@ export function generateTrackFeature(track: Track) {
 }
 
 function generateTrackStyle() {
-    return new Style();
+    return new Style({
+        image: new Icon({
+            src: trackIcon,
+            scale: 0.45,
+        }),
+    });
 }
--- src/web/openlayers/layers/vector/sentinel-layer.ts
+++ src/web/openlayers/layers/vector/sentinel-layer.ts
@@ -2,7 +2,6 @@ import JaiaVectorLayer from "./jaia-vector-layer";
 import { LayerTitles } from "../../../types/openlayers-types";
 import { layersZIndexes } from "../zindex";
 import { generateTrackFeature } from "../../features/sentinel/track-feature";
-import { generateInterceptFeature } from "../../features/sentinel/intercept-feature";
 import { sentinel } from "../../../data/sentinel/sentinel";
 
 class SentinelLayer extends JaiaVectorLayer {
@@ -14,14 +13,9 @@ class SentinelLayer extends JaiaVectorLayer {
         let source = this.getVectorLayer().getSource();
         source.clear();
 
-        for (let track of sentinel.getTracks()) {
+        for (let [trackID, track] of sentinel.getTracks()) {
             const trackFeature = generateTrackFeature(track);
             source.addFeature(trackFeature);
-        }
-
-        for (let intercept of sentinel.getIntercepts()) {
-            const interceptFeature = generateInterceptFeature(intercept);
-            source.addFeature(interceptFeature);
         }
     }
 }
