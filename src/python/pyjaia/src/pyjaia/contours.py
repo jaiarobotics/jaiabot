e9d4dfc31ce6e1f26a1e39881e33e947370bb9a9
michael-twomey <michael-twomey1@outlook.com>
2025-09-15 10:30:07 -0400

    Modify check in getContourValues func

--- src/python/pyjaia/src/pyjaia/contours.py
+++ src/python/pyjaia/src/pyjaia/contours.py
@@ -161,13 +161,14 @@ def getContourValues(bottomDives: List[BottomDive], contourCount = 10):
     Returns:
         NDArray[float]: An array of equally-spaced contour values spanning the rand of depths for the `bottomDive` objects.
     """
-    if len(bottomDives) < 1:
+    if len(bottomDives) < 3:
         return [] # No bottom dives, so no contours.
 
     # Get contour values
     values = [b.depth for b in bottomDives]
     minValue = min(values)
     maxValue = max(values)
+    print(f"minValue: {minValue}    maxValue: {maxValue}")
 
     if minValue == maxValue:
         logging.warning('No contours to display, because all dives reach the same depth')
