57b1bcb7fbe9005811d9fb105c1d35805cfdbc78
Jeff Roy <153303638+JRoybot@users.noreply.github.com>
2025-09-15 13:47:15 -0400

    task/2.y/refactor-move-waypoint-structure/JAIA-2076/ (#1325)
    * Move isMoveable property from waypoionts to jaiaGlobal
    
    * remove dead code
    
    * resolve review comments
    
    * Add strict check to selectedWaypoint conditional
    
    ---------
    
    Co-authored-by: michael-twomey <michael-twomey1@outlook.com>
    Co-authored-by: Michael Twomey <109538793+michael-jaia@users.noreply.github.com>

--- src/web/components/Map/Map.tsx
+++ src/web/components/Map/Map.tsx
@@ -3,7 +3,6 @@ import { JaiaDispatchContext } from "../../context/JaiaContext";
 import { JaiaActions } from "../../context/jaia-actions";
 
 import { jaiaGlobal } from "../../data/jaia_global/jaia-global";
-import { missionSet } from "../../data/mission_set/mission-set";
 
 import { Feature, MapBrowserEvent } from "ol";
 import { Coordinate } from "ol/coordinate";
@@ -15,7 +14,6 @@ import { view } from "../../openlayers/views/view";
 
 import { NodeTypes } from "../../types/jaia-system-types";
 import { MapFeatureTypes, MapModes } from "../../types/openlayers-types";
-import { UNASSIGNED_ID } from "../../utils/constants";
 
 import "./Map.less";
 
@@ -71,7 +69,7 @@ export default function Map() {
             }
         }
 
-        if (isWaypointMovable()) {
+        if (jaiaGlobal.getSelectedWaypoint().isMoveable) {
             handleMoveWaypointClick(event.coordinate);
             return;
         }
@@ -118,13 +116,20 @@ export default function Map() {
      * @returns {void}
      */
     const handleWaypointClick = (feature: Feature<Geometry>) => {
-        jaiaDispatch({
-            type: JaiaActions.CLICKED_WAYPOINT,
-            clickedWaypoint: {
-                waypointNum: feature.get("waypointNum"),
-                missionID: feature.get("missionID"),
-            },
-        });
+        const selectedWaypoint = jaiaGlobal.getSelectedWaypoint();
+        if (
+            feature.get("missionID") !== selectedWaypoint.missionID ||
+            feature.get("waypointNum") !== selectedWaypoint.waypointNum
+        ) {
+            jaiaDispatch({
+                type: JaiaActions.CLICKED_WAYPOINT,
+                clickedWaypoint: {
+                    waypointNum: feature.get("waypointNum"),
+                    missionID: feature.get("missionID"),
+                    isMoveable: false,
+                },
+            });
+        }
     };
 
     /**
@@ -191,23 +196,6 @@ export default function Map() {
             type: JaiaActions.ADD_WAYPOINT,
             location: { lon: lonLat[0], lat: lonLat[1] },
         });
-    };
-
-    /**
-     * Checks to see if the selected waypoint is movable
-     *
-     * @returns {boolean} True if the waypoint is movable, false if not
-     */
-    const isWaypointMovable = () => {
-        if (jaiaGlobal.getSelectedWaypoint().waypointNum !== UNASSIGNED_ID) {
-            const mission = missionSet.getMission(jaiaGlobal.getSelectedWaypoint().missionID);
-            const waypoint = mission.getWaypoint(jaiaGlobal.getSelectedWaypoint().waypointNum);
-
-            if (waypoint.getIsMovable()) {
-                return true;
-            }
-        }
-        return false;
     };
 
     return <div id="map" data-testid="map"></div>;
