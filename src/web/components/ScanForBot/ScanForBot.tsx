7c4453c1bd0875b2c30ef51853c8de08a5ba7262
michael-twomey <michael-twomey1@outlook.com>
2025-10-07 13:47:12 -0400

    Fill select dropdown with Bot IDs

--- src/web/components/ScanForBot/ScanForBot.tsx
+++ src/web/components/ScanForBot/ScanForBot.tsx
@@ -1,10 +1,21 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { FormControl, MenuItem, Select, SelectChangeEvent } from "@mui/material";
+import { JaiaContext } from "../../context/JaiaContext";
 import "./ScanForBot.less";
 
+/**
+ * Allows operators to manually send a subscription command to Bots
+ */
 export default function ScanForBot() {
+    const jaiaContext = useContext(JaiaContext);
     const [selectedBotID, setSelectedBotID] = useState("");
 
+    /**
+     * Updates state with the selected Bot ID
+     *
+     * @param {SelectChangeEvent} evt Contains the selected Bot ID
+     * @returns {void}
+     */
     const handleMenuSelection = (evt: SelectChangeEvent) => {
         setSelectedBotID(evt.target.value);
     };
@@ -19,7 +30,14 @@ export default function ScanForBot() {
                         onChange={(evt: SelectChangeEvent) => handleMenuSelection(evt)}
                         value={selectedBotID}
                     >
-                        <MenuItem value={"1"}>1</MenuItem>
+                        {Array.from(jaiaContext.bots.values()).map((bot) => {
+                            const botID = bot.getBotID();
+                            return (
+                                <MenuItem key={botID} value={botID}>
+                                    {botID}
+                                </MenuItem>
+                            );
+                        })}
                     </Select>
                 </FormControl>
             </div>
