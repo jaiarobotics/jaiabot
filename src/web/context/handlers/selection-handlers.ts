feceee6634bfcf829e69a421b933e0120409c9d5
Jeff Roy <153303638+JRoybot@users.noreply.github.com>
2025-10-07 11:09:11 -0400

    task/2.y/add-back-undo-button-JR/JAIA-2050 (#1333)
    * circular history buffer for undo and redo
    
    * explicitly test indicies
    
    * replace mission history with generic history buffer manager
    
    * dir structure change
    
    * fix unit tests
    
    * refactor jaiaReducer using Maps to define handlers and integrate with history buffer
    
    * condense action map
    
    * Unify action handler profiles to simplify mapping of functions
    
    * Simplify action configuration to use single map and simplify reducer
    
    * Add Undo and Redo buttons, and actions to support them
    
    * initial cut at undo and redo handlers, still buggy
    
    * debugging undo/redo mostly working, still a few quirks
    
    * store undoable state instead of just missions to keep the state consistent when undoing and redoing
    
    * sync data model items from recovered state
    
    * update comments
    
    * WIP
    
    * fix bug in push after undo, save missionSet data in snapshots
    
    * update comments, add test of redo after undo and push
    
    * prevent Map from sending invalid ADD_WAYPOINT actions
    
    * capture mission/bot assignments in snapshot, enable undo for assignments and mission speeds
    
    * remove redundant cloning
    
    * Style undo & redo buttons when disabled
    
    * fix bug with bad initial state leaving missionIDInEditMode undefined
    
    * define selectedRallyPoint in init snapshot
    
    * Move types from JaiaContext to types/context-types
    
    * Break up JaiaContext
    
    * clean up formatting after refactoreing JaiaContext
    
    * cleanup unneeded exports
    
    * move default constants out of types file
    
    * Move JaiaHistoryType
    
    * add comment
    
    * Tweak imports + docstrings
    
    * move action configs to separate file
    
    * Add docstrings
    
    * Consolidate imports
    
    * Reorder imports
    
    * Remove id from undo + redo buttons
    
    * Modify docstrings
    
    * Address review comments to simplify history buffer
    
    * Add missing docstrings
    
    * remove a redundant cloning
    
    * move syncOpenLayers to handler-utils
    
    * minor cleanup
    
    * Remove blank line
    
    ---------
    
    Co-authored-by: michael-twomey <michael-twomey1@outlook.com>

---
+++ src/web/context/handlers/selection-handlers.ts
@@ -0,0 +1,181 @@
+import { jaiaGlobal } from "../../data/jaia_global/jaia-global";
+import { missionSet } from "../../data/mission_set/mission-set";
+import { diveLayer } from "../../openlayers/layers/vector/dive-layer";
+import { driftLayer } from "../../openlayers/layers/vector/drift-layer";
+import { missionLayer } from "../../openlayers/layers/vector/mission-layer";
+import { rallyLayer } from "../../openlayers/layers/vector/rally-layer";
+import { handleMapModeChange } from "../../openlayers/maps/map";
+import { MapModes } from "../../types/openlayers-types";
+import { JaiaContextType, JaiaAction, ButtonNames, ButtonTypes } from "../../types/context-types";
+import { UNASSIGNED_ID } from "../../utils/constants";
+import { syncOpenLayers } from "./handler-utils";
+import { resetSelectedWaypoint } from "./waypoint-handlers";
+
+/**
+ * Handles click events for the Bot and Hub icons on the map and in the NodeList component
+ *
+ * @param {JaiaContextType} mutableState State object ref for making modifications
+ * @param {JaiaAction} action including clickedNode
+ * @returns {JaiaContextType} Updated mutable state object
+ *
+ * @notes
+ * This function calls jaiaGlobal.setSelectedNode to make sure the
+ * data used by OpenLayers is in sync with JaiaContext
+ */
+export function handleClickedNode(mutableState: JaiaContextType, action: JaiaAction) {
+    jaiaGlobal.setSelectedNode(action.clickedNode);
+    const selectedNode = jaiaGlobal.getSelectedNode();
+
+    mutableState.selectedNode = selectedNode;
+    mutableState.visibleDetails = selectedNode.type;
+
+    syncOpenLayers();
+
+    return mutableState;
+}
+
+/**
+ * Handles a click to the tap to move toggle
+ *
+ * @param {JaiaContextType} mutableState State object ref for making modifications
+ * @returns {JaiaContextType} Updated mutable state object
+ */
+export function handleClickedTapToMove(mutableState: JaiaContextType) {
+    mutableState.selectedWaypoint.isMoveable = !mutableState.selectedWaypoint.isMoveable;
+    jaiaGlobal.setSelectedWaypoint(mutableState.selectedWaypoint);
+    return mutableState;
+}
+
+/**
+ * Sets the map mode and visible panel based on the button clicked and the state
+ * of the application
+ *
+ * @param {JaiaContextType} mutableState State object ref for making modifications
+ * @param {JaiaAction} action including buttonType and buttonName of panel the button
+ * @returns {JaiaContextType} Updated mutable state object
+ */
+export function handleClickedButton(mutableState: JaiaContextType, action: JaiaAction) {
+    let mapMode = MapModes.DEFAULT;
+    let visiblePanel = ButtonNames.NONE;
+
+    switch (action.buttonType) {
+        case ButtonTypes.MAP_MODE:
+            if (
+                action.buttonName === ButtonNames.ADD_RALLY &&
+                jaiaGlobal.getMapMode() !== MapModes.RALLY
+            ) {
+                mapMode = MapModes.RALLY;
+            }
+
+            if (
+                action.buttonName === ButtonNames.MEASURE_TOOL &&
+                mutableState.visiblePanel !== action.buttonName
+            ) {
+                mapMode = MapModes.MEASURE;
+                visiblePanel = action.buttonName;
+            }
+            break;
+        case ButtonTypes.PANEL:
+            if (mutableState.visiblePanel !== action.buttonName) {
+                visiblePanel = action.buttonName;
+            }
+            break;
+        case ButtonTypes.COMMAND:
+            if (action.buttonName === ButtonNames.GO_TO_RALLY) {
+                visiblePanel = ButtonNames.RALLY_PANEL;
+            }
+            break;
+    }
+
+    // Resets
+    if (mutableState.selectedWaypoint.waypointNum !== UNASSIGNED_ID) {
+        resetSelectedWaypoint(mutableState);
+    }
+
+    handleMapModeChange(mapMode);
+    mutableState.mapMode = mapMode;
+    mutableState.visiblePanel = visiblePanel;
+    return mutableState;
+}
+
+/**
+ * Opens panel for the selected waypoint
+ *
+ * @param {JaiaContextType} mutableState State object ref for making modifications
+ * @param {JaiaAction} action including clickedWaypoint
+ * @returns {JaiaContextType} Updated mutable state object
+ */
+export function handleClickedWaypoint(mutableState: JaiaContextType, action: JaiaAction) {
+    jaiaGlobal.setSelectedWaypoint(action.clickedWaypoint);
+
+    mutableState.selectedWaypoint = jaiaGlobal.getSelectedWaypoint();
+    mutableState.visiblePanel = ButtonNames.WAYPOINT_PANEL;
+
+    missionLayer.updateFeatures();
+
+    return mutableState;
+}
+
+/**
+ * Handles a click on a mission edit mode toggle
+ *
+ * @param {JaiaContextType} mutableState State object ref for making modifications
+ * @param {JaiaAction} action including missionID of the mission associated with the toggle
+ * @returns {JaiaContextType} Updated mutable state object
+ */
+export function handleClickedEditMission(mutableState: JaiaContextType, action: JaiaAction) {
+    if (action.missionID !== missionSet.getMissionIDInEditMode()) {
+        missionSet.setMissionIDInEditMode(action.missionID);
+    } else {
+        missionSet.setMissionIDInEditMode(UNASSIGNED_ID);
+        mutableState.selectedWaypoint.isMoveable = false;
+        jaiaGlobal.setSelectedWaypoint(mutableState.selectedWaypoint);
+    }
+
+    mutableState.missionIDInEditMode = missionSet.getMissionIDInEditMode();
+
+    missionLayer.updateFeatures();
+
+    return mutableState;
+}
+
+/**
+ * Opens panel for the selected rally point
+ *
+ * @param {JaiaContextType} mutableState State object ref for making modifications
+ * @param {JaiaAction} action including rallyID Identifies which rally point was clicked by operator
+ * @returns {JaiaContextType} Updated mutable state object
+ */
+export function handleClickedRallyPoint(mutableState: JaiaContextType, action: JaiaAction) {
+    mutableState.selectedRallyPoint = {
+        id: action.rallyID,
+        location: rallyLayer.getRallyLocation(action.rallyID),
+    };
+    mutableState.visiblePanel = ButtonNames.RALLY_PANEL;
+    return mutableState;
+}
+
+/** Opens panel for the selected task packet
+ *
+ * @param {JaiaContextType} mutableState State object ref for making modifications
+ * @param {JaiaAction} action including clickedTaskPacket
+ * @returns {JaiaContextType} Updated mutable state object
+ */
+export function handleClickedTaskPacket(mutableState: JaiaContextType, action: JaiaAction) {
+    const selectedTaskPacket = jaiaGlobal.getSelectedTaskPacket();
+
+    if (
+        selectedTaskPacket.botID === action.clickedTaskPacket.botID &&
+        selectedTaskPacket.startTime === action.clickedTaskPacket.startTime &&
+        selectedTaskPacket.type === action.clickedTaskPacket.type
+    ) {
+        return mutableState;
+    }
+
+    jaiaGlobal.setSelectedTaskPacket(action.clickedTaskPacket);
+    mutableState.selectedTaskPacket = jaiaGlobal.getSelectedTaskPacket();
+    mutableState.visiblePanel = ButtonNames.TASK_PACKET_PANEL;
+    diveLayer.updateFeatures();
+    driftLayer.updateFeatures();
+    return mutableState;
+}
