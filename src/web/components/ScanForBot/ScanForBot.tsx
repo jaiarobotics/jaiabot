29c856718178cb481db14dc772cde38dccf00f70
michael-twomey <michael-twomey1@outlook.com>
2025-10-07 12:38:45 -0400

    Add barebones MUI dropdown for Bot selection

--- src/web/components/ScanForBot/ScanForBot.tsx
+++ src/web/components/ScanForBot/ScanForBot.tsx
@@ -1,12 +1,27 @@
+import { useState } from "react";
+import { FormControl, MenuItem, Select, SelectChangeEvent } from "@mui/material";
 import "./ScanForBot.less";
 
 export default function ScanForBot() {
+    const [selectedBotID, setSelectedBotID] = useState("");
+
+    const handleMenuSelection = (evt: SelectChangeEvent) => {
+        setSelectedBotID(evt.target.value);
+    };
+
     return (
         <div className="scan-for-bot-container">
             <div className="heading">Scan For Bot</div>
             <div className="bot-select-container">
                 <div>Bot:</div>
-                <div>1</div>
+                <FormControl size="small">
+                    <Select
+                        onChange={(evt: SelectChangeEvent) => handleMenuSelection(evt)}
+                        value={selectedBotID}
+                    >
+                        <MenuItem value={"1"}>1</MenuItem>
+                    </Select>
+                </FormControl>
             </div>
             <button>Scan For Bot</button>
             <button>Scan For All Bots</button>
