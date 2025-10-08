79f682e6f6d750b2c191f964c4ec0a3d23945f63
Matthew Ferro <109534520+ferrom-jaia@users.noreply.github.com>
2025-10-08 09:17:23 -0400

    Update Plots.tsx
    Fix spacing

--- src/web/jdv/client/src/ui/Plots.tsx
+++ src/web/jdv/client/src/ui/Plots.tsx
@@ -186,11 +186,11 @@ export function Plots(props: PlotsProps) {
             });
 
 
-	    plot_div_element.on("plotly_click", function (data) {
-  		let pointIndex = data.points[0].pointIndex;
-  		let timestamp_utime = Number(data.points[0].data.customdata[pointIndex]);
-  		props.delegate.setTime(timestamp_utime);
-	    });
+	    	plot_div_element.on("plotly_click", function (data) {
+  				let pointIndex = data.points[0].pointIndex;
+  				let timestamp_utime = Number(data.points[0].data.customdata[pointIndex]);
+  				props.delegate.setTime(timestamp_utime);
+	    	});
 
             // Zooming into plots
             plot_div_element.on("plotly_relayout", function (eventdata: Plotly.PlotRelayoutEvent) {
