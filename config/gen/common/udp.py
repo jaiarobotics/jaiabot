a7ffb056a0b66d6d1fc83bb5a8afbcd05a31f1f9
Adebowale <adadelek11@gmail.com>
2025-10-23 16:21:13 -0400

    Updated dictionaries to be grouped into simulation base ports and runtime static ports

--- config/gen/common/udp.py
+++ config/gen/common/udp.py
@@ -2,68 +2,72 @@ from common import is_simulation
 from common import comms
 
 # Dictionary of base ports for each type
-base_ports = {
+simulation_base_ports = {
+    "wifi_udp": 31000, #we add the hub_id if it's a hub, else we add number_of_hubs_max + node_id
+    "hub2hub_udp": 32000,#we add the hub_id
+    "bar30_cpp": 20100,#we add the node_id
+    "bar30_py": 20000,#we add the node_id
+    "atlas_ezo_cpp": 20200,#we add the node_id
+    "atlas_ezo_py": 20300,#we add the node_id
+    "imu": 20400,#we add the node_id
+    "contact_gpsd": 33000,#we add the contact_id
+}
+
+runtime_static_ports = {
     "wifi_udp": 31000,
     "hub2hub_udp": 32000,
     "bar30_cpp": 20100,
     "bar30_py": 20000,
-    "atlas_ezo_cpp": 20200,
-    "atlas_ezo_py": 20300,
-    "imu": 20400,
-    "contact_gpsd": 33000,
-}
-
-static_ports = {
-    "bar30_py_runtime": 20001,
-    "atlas_ezo_cpp_runtime": 0,
-    "atlas_ezo_py_runtime": 20002,
+    "atlas_ezo_cpp": 0,
+    "atlas_ezo_py": 20002,
     "tsys01_cpp": 0,
     "tsys01_py": 20006,
     "motor_cpp": 0,
     "motor_py": 20005,
-    "imu_runtime": 20000,
+    "imu": 20000,
 }
+
 
 def wifi_udp_port(node_id, hub_id=-1):
     if is_simulation():
         if node_id == comms.hub_node_id:
             if hub_id == -1:
                 raise RuntimeError("Must define hub_id when node_id is hub_node_id")
-            return base_ports["wifi_udp"] + hub_id
+            return simulation_base_ports["wifi_udp"] + hub_id
         else:
-            return base_ports["wifi_udp"] + comms.number_of_hubs_max + node_id
+            return simulation_base_ports["wifi_udp"] + comms.number_of_hubs_max + node_id
     else:
-        return base_ports["wifi_udp"]
+        return runtime_static_ports["wifi_udp"]
 
 def hub2hub_udp_port(hub_id):
-    return base_ports["hub2hub_udp"] + (hub_id if is_simulation() else 0)
+    return (simulation_base_ports["hub2hub_udp"] + hub_id) if is_simulation() else runtime_static_ports["hub2hub_udp"]
 
 def bar30_cpp_udp_port(node_id):
-    return base_ports["bar30_cpp"] + (node_id if is_simulation() else 0)
+    return (simulation_base_ports["bar30_cpp"] + node_id) if is_simulation() else runtime_static_ports["bar30_cpp"]
 
 def bar30_py_udp_port(node_id):
-    return base_ports["bar30_py"] + node_id if is_simulation() else static_ports["bar30_py_runtime"]
+    return (simulation_base_ports["bar30_py"] + node_id) if is_simulation() else runtime_static_ports["bar30_py"]
 
 def tsys01_cpp_udp_port():
-    return static_ports["tsys01_cpp"]
+    return runtime_static_ports["tsys01_cpp"]
 
 def tsys01_py_udp_port():
-    return static_ports["tsys01_py"]
+    return runtime_static_ports["tsys01_py"]
 
 def atlas_ezo_cpp_udp_port(node_id):
-    return base_ports["atlas_ezo_cpp"] + node_id if is_simulation() else static_ports["atlas_ezo_cpp_runtime"]
+    return (simulation_base_ports["atlas_ezo_cpp"] + node_id) if is_simulation() else runtime_static_ports["atlas_ezo_cpp"]
 
 def atlas_ezo_py_udp_port(node_id):
-    return base_ports["atlas_ezo_py"] + node_id if is_simulation() else static_ports["atlas_ezo_py_runtime"]
+    return (simulation_base_ports["atlas_ezo_py"] + node_id) if is_simulation() else runtime_static_ports["atlas_ezo_py"]
 
 def imu_port(node_id):
-    return base_ports["imu"] + node_id if is_simulation() else static_ports["imu_runtime"]
+    return (simulation_base_ports["imu"] + node_id) if is_simulation() else runtime_static_ports["imu"]
 
 def contact_gpsd_port(contact_id):
-    return base_ports["contact_gpsd"] + contact_id
+    return simulation_base_ports["contact_gpsd"] + contact_id
 
 def motor_cpp_udp_port():
-    return static_ports["motor_cpp"]
+    return runtime_static_ports["motor_cpp"]
 
 def motor_py_udp_port():
-    return static_ports["motor_py"]
+    return runtime_static_ports["motor_py"]
