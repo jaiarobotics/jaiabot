8a737dcd1e9ab19b01ad78d675a5d366c0591cc4
michael-twomey <michael-twomey1@outlook.com>
2025-11-05 11:38:26 -0500

    Make minor changes from code review

--- src/web/components/SimulationBanner/SimulationBanner.tsx
+++ src/web/components/SimulationBanner/SimulationBanner.tsx
@@ -1,31 +1,35 @@
-import { METADATA_POLL_TIME } from "../../utils/constants";
 import { useEffect, useState } from "react";
+import { METADATA_POLL_TIME } from "../../utils/constants";
 import { jaiaAPI } from "../../utils/jaia-api";
 import "./SimulationBanner.less";
 
+/**
+ * Displays the simulation indicator in the JCC
+ */
 export default function SimulationBanner() {
     const [simulationMode, setSimulationMode] = useState(false);
 
+    /**
+     * Retrieves the metadata message from the server to
+     * update the simulationMode property
+     *
+     * @returns {void}
+     */
     const fetchSimulationMode = async () => {
         try {
             const metadata = await jaiaAPI.getMetadata();
             setSimulationMode(metadata.is_simulation);
         } catch (error) {
-            console.error("Error fetching simulation metadata:", error);
+            console.error("Error fetching metadata for simulation banner:", error);
         }
     };
 
     useEffect(() => {
-        fetchSimulationMode();
         const interval = setInterval(fetchSimulationMode, METADATA_POLL_TIME);
         return () => clearInterval(interval);
     }, []);
 
-    if (!simulationMode) return null;
+    if (!simulationMode) return;
 
-    return (
-        <div id="simulation-banner" className="simulation-banner">
-            Simulation
-        </div>
-    );
+    return <div className="simulation-banner">Simulation</div>;
 }
--- src/web/jcc/App.tsx
+++ src/web/jcc/App.tsx
@@ -20,8 +20,8 @@ import WaypointPanel from "../components/WaypointPanel/WaypointPanel";
 import SurveyPlanner from "../components/SurveyPlanner/SurveyPlanner";
 import TaskPacketPanel from "../components/TaskPacketPanel/TaskPacketPanel";
 import DataOffloadPanel from "../components/DataOffloadPanel/DataOffloadPanel";
-import RemoteControlPanel from "../components/RemoteControlPanel/RemoteControlPanel";
 import SimulationBanner from "../components/SimulationBanner/SimulationBanner";
+import RemoteControlPanel from "../components/RemoteControlPanel/RemoteControlPanel";
 
 import "./App.less";
 
