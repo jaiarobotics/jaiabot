fee13c9b73e28b77a4c30ddf37a9a6a89062d912
Jeff Roy <jeff@roy.net>
2025-11-14 09:08:29 -0500

    fix typo

--- src/web/data/history/history.ts
+++ src/web/data/history/history.ts
@@ -4,5 +4,5 @@ import { MAX_HISTORY } from "../../utils/constants";
 
 const emptyState = {} as JaiaContextType;
 // The history singleton is being initialized with an empty state
-// It will be reset by the handleInit of the data-modle-handlers
+// It will be reset by the handleInit of the data-model-handlers
 export const jaiaStateHistory = new HistoryBuffer<JaiaContextType>(emptyState, MAX_HISTORY);
