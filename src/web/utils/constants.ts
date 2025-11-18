06e1637da175c2274f12d14225b1780b39edf0e8
michael-twomey <michael-twomey1@outlook.com>
2025-11-18 07:46:50 -0500

    Add max waypoints constraint to individual missions

--- src/web/utils/constants.ts
+++ src/web/utils/constants.ts
@@ -31,3 +31,4 @@ export const KILOMETER_FACTOR = 1000;
 export const MAX_HISTORY = 15;
 export const MISSION_ENDPOINTS = 2;
 export const DEPTH_MAP_3D_NAME = "depth-map-3d";
+export const MAX_WAYPOINTS = 80;
