192a976e4c641794d0b57eaabb3e3b6c044749ae
Edward Sanville <edsanville@gmail.com>
2025-12-08 13:55:57 -0500

    task/add-configurable-data-rate-for-imu-jaia-2102 (#1415)
    * jaiabot_imu.py has a configurable sample rate, rather than poll and respond
    
    * Add sample_rate to config options.
    
    * Test branch theft
    
    * Building off of Ed's PR. uses time.perf_counter() to keep track of time instead of just sleep. Allows for much more accurate frequencies
    
    * Continue was forcing the loop to skip over the timing logic if no command was received
    
    * Remove test branch upload
    
    ---------
    
    Co-authored-by: Nick Marshall <nickjrmarshall@gmail.com>
    Co-authored-by: Nick Marshall <98423695+marshalllele@users.noreply.github.com>

--- src/python/adafruit/remote_test.sh
+++ src/python/adafruit/remote_test.sh
@@ -2,6 +2,9 @@
 
 VENV_DIR=./venv
 
+# Make sure the services aren't running
+sudo systemctl stop jaiabot.service
+
 if [ ! -d "$VENV_DIR" ]; then
     echo "Creating virtual environment in $VENV_DIR"
     python3 -m venv $VENV_DIR
@@ -9,11 +12,9 @@ if [ ! -d "$VENV_DIR" ]; then
     $VENV_DIR/bin/pip3 install -r requirements.txt
 else
     echo "Using existing virtual environment in $VENV_DIR"
+    $VENV_DIR/bin/pip3 install -U ../pyjaiaprotobuf
 fi
 
 PYTHON=$VENV_DIR/bin/python3
 
-# Make sure the services aren't running
-sudo systemctl stop jaiabot.service
-
-$PYTHON jaiabot_imu.py -t bno085 -i -l DEBUG
+$PYTHON jaiabot_imu.py -t bno085 -i -l INFO
