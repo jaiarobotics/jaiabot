b4d96b78029ec363ae995e32a68b6b011a1a8b13
Jeff Roy <jeff@roy.net>
2025-12-08 10:09:04 -0500

    create Rally Point Data Model

---
+++ src/web/data/rally_points/rally-points.ts
@@ -0,0 +1,60 @@
+import cloneDeep from "lodash.clonedeep";
+import RallyPoint from "./rally-point";
+import { UNASSIGNED_ID } from "../../utils/constants";
+
+export interface RallyPointsSnapshot {
+    rallyPoints: Map<number, RallyPoint>;
+    nextRallyPointID: number;
+    selectedRallyPointID: number;
+}
+
+export class RallyPoints {
+    private rallyPoints: Map<number, RallyPoint>;
+    private nextRallyPointID: number;
+    private selectedRallyPointID: number;
+
+    constructor() {
+        this.rallyPoints = new Map<number, RallyPoint>();
+        this.nextRallyPointID = 1;
+        this.selectedRallyPointID = UNASSIGNED_ID;
+    }
+
+    getRallyPoints() {
+        return this.rallyPoints;
+    }
+
+    setRallyPoints(rallyPoints: Map<number, RallyPoint>) {
+        this.rallyPoints = rallyPoints;
+    }
+
+    getNextRallyPointID() {
+        return this.nextRallyPointID;
+    }
+
+    setNextRallyPointID(nextRallyPointID: number) {
+        this.nextRallyPointID = nextRallyPointID;
+    }
+
+    getSelectedRallyPointID() {
+        return this.selectedRallyPointID;
+    }
+
+    setSelectedRallyPointID(selectedRallyPointID: number) {
+        this.selectedRallyPointID = selectedRallyPointID;
+    }
+
+    captureSnapshot() {
+        const currentRallyPoints: RallyPointsSnapshot = {
+            rallyPoints: this.rallyPoints,
+            nextRallyPointID: this.nextRallyPointID,
+            selectedRallyPointID: this.selectedRallyPointID,
+        };
+        return cloneDeep(currentRallyPoints);
+    }
+
+    restoreFromSnapshot(snapshot: RallyPointsSnapshot) {
+        this.rallyPoints = snapshot.rallyPoints;
+        this.nextRallyPointID = snapshot.nextRallyPointID;
+        this.selectedRallyPointID = snapshot.selectedRallyPointID;
+    }
+}
