8ed9252977c5d8ca99e76c88383d642eb687ecd5
Toby Schneider <tsaubergine@users.noreply.github.com>
2025-12-24 02:07:48 +1100

    Task/2.y/move iridium to govcloud/jaia-2162 and Task/2.y/add support for AWS GovCloud to cloudhub/JAIA-2161 (#1427)
    * Build OVA as AMI in Govcloud east-1 as well as commercial east-1
    
    * More updates for supporting govcloud
    
    * Enable snapshot lifecycle on new servers
    
    * Fixes to support govcloud for cloudhub
    
    * Run jaia_firm_backup_date.sh on Cloudhub (and simulated machines)
    
    * Fix branch name in circleci config
    
    * Fix DLM service role for GovCloud (differs from that for commercial)
    
    * Add 'jaia admin fleet create_cloudhub' and 'jaia admin fleet delete_cloudhub' as more user-friendly entry points to create_vpc.sh and delete_vpc.sh, respectively
    
    * Output a helpful message if the required AWS profile doesn't exist
    
    * Fix regression where apache2 is not running, systemd.py script fails
    
    * Use full path to fleet config so it is correctly resolved when running create_vpc.sh
    
    * pushing to test
    
    * Remove test build
    
    ---------
    
    Co-authored-by: Nick Marshall <nickjrmarshall@gmail.com>
    Co-authored-by: Matthew Ferro <109534520+ferrom-jaia@users.noreply.github.com>
    Co-authored-by: Matthew Ferro <matt.ferro@jaia.tech>

--- config/gen/systemd.py
+++ config/gen/systemd.py
@@ -739,7 +739,8 @@ jaia_firmware = [
      'template': 'backup-date.service.in',
      'args': '',
      'runs_on': [Type.BOTH],
-     'runs_when': Mode.BOTH},
+     'runs_when': Mode.BOTH,
+     'runs_on_cloudhub': CloudHubType.SECONDARY },
      {'exe': 'jaia_firm_bno085_reset_gpio_pin.py',
      'description': 'BNO085 script to reboot imu',
      'template': 'bno085-reset-gpio-pin.service.in',
@@ -819,7 +820,7 @@ for app in jaiabot_apps:
         if app['template'] == 'jcc.conf.in':
             print('Enabling ' + service)
             subprocess.run('a2ensite ' + service, check=True, shell=True)
-            subprocess.run('systemctl reload apache2', check=True, shell=True)
+            subprocess.run('if systemctl is-active --quiet apache2; then systemctl reload apache2; else systemctl start apache2; fi', check=True, shell=True)
             
 # check if the firmware is run on this type (bot/hub), at this time (runtime/simulation), and if the system has the capability
 def is_firm_run(firm):
