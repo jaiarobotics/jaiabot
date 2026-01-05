7ca363991351c2f5f5e38e7a3d7f3f5ff3836ba1
Edward Sanville <edsanville@gmail.com>
2026-01-05 13:07:01 -0500

    Spin MissionManager class off into its own module.

---
+++ src/bin/mission_manager/groups.h
@@ -0,0 +1,16 @@
+#pragma once
+
+namespace jaiabot
+{
+namespace apps
+{
+
+namespace groups
+{
+
+extern std::unique_ptr<goby::middleware::DynamicGroup> hub_command_this_bot;
+
+} // namespace groups
+
+} // namespace apps
+} // namespace jaiabot
