8d9ddb5ccc0249c21fde4d4f8e8024192e8ae782
michael-twomey <michael-twomey1@outlook.com>
2026-01-07 16:58:35 -0500

    Reposition JCC warnings

--- src/web/jcc/App.tsx
+++ src/web/jcc/App.tsx
@@ -60,7 +60,7 @@ export default function App() {
                 <TakeControl />
             </JaiaContextProvider>
             <div id="connection-warning">Connection to Hub Dropped</div>
-            <div id="congestion-warning">Network Congestion Warning</div>
+            <div id="congestion-warning">Slow Hub WiFi Speeds</div>
         </div>
     );
 }
