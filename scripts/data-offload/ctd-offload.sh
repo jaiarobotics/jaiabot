0e39ae8f4b3cf6fa599e612fc0c4a27614770e47
michael-twomey <michael-twomey1@outlook.com>
2026-01-16 14:28:06 -0500

    Create ctd offload script

---
+++ scripts/data-offload/ctd-offload.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -e
+
+bot_id=0
+bot_ip="127.0.0.1"
+
+while [[ $# -gt 0 ]]; do
+    if [[ "$1" == "-bot_id" ]]; then
+        bot_id="$2"
+        shift 2
+    elif [[ "$1" == "-bot_ip" ]]; then
+        bot_ip="$2"
+        shift 2
+    else
+        echo "Unknown argument: $1" >&2
+        shift
+    fi
+done
+
+if [[ "$bot_id" -eq 0 ]]; then
+    echo "No Bot ID provided"
+    exit 1;
+fi
+
+destination_dir="/var/log/jaiabot/bot_offload/ctd/$bot_id/"
+ctd_dir="/var/log/jaiabot/bot/${bot_id}/ctd/"
+
+if [[ ! -d "${ctd_dir}" ]]; then
+    echo "CTD directory does not exist"
+    exit 1;
+fi
+
+if [[ ! -d "${destination_dir}" ]]; then
+  mkdir -p "${destination_dir}"
+fi
+
+if [[ "${bot_ip}" == "127.0.0.1" ]]; then
+    userat=""
+    nice -n 10 rsync -azh --info=progress2 --timeout=15 "${ctd_dir}" "${destination_dir}"
+else
+    userat="jaia@"
+    nice -n 10 rsync -azh --info=progress2 --timeout=15 "${userat}${bot_ip}:${ctd_dir}" "${destination_dir}"
+fi
