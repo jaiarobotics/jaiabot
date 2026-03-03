c917e223b4ae435bb32ec95df6db088462403130
Michael Twomey <109538793+michael-jaia@users.noreply.github.com>
2026-03-02 19:51:08 -0500

    feature/ctd-wifi-data-offload (#1465)
    * Create CTD message
    
    * Save CTD data in unpowered ascent state
    
    * Add in state reaction for measurement event
    
    * Capture data in snapshots
    
    * Rename variables
    
    * Publish CTD profile when Bot surfaces
    
    * Remove empty space
    
    * Update variable name
    
    * Change snapshots instance to snapshot
    
    * Create CTD app + run in simulation
    
    * Add jaiabot_ctd_manager to systemd config
    
    * Write CTD profile to file
    
    * Restructure CTD messages + name files
    
    * Update path to CTD files
    
    * Create ctd offload script
    
    * Create + subscribe to CTD_DATA_OFFLOAD command
    
    * Call ctd offload script from CTD manager app
    
    * Create minimal panel to send CTD offload commands
    
    * Switch offload init to Hub
    
    * Add endpoint for zip files of CTD profiles
    
    * Download zip files programmatically
    
    * Style CTD download panel
    
    * Add functionality to close button
    
    * Create secondary dialog
    
    * Add minimal functionality to delete dialog buttons
    
    * Create delete method on server
    
    * Add docstrings
    
    * Check WiFi link quality instead of battery
    
    * Use checkbox instead of dialog for deleting CTD files
    
    * Translate cast data to UNB format
    
    * Capture salinity data instead of conductivity
    
    * Make changes based on PR review
    
    * Reset import to previous version
    
    * Fix bugs found from on-the-water testing
    
    * Set start time to 0 to capture inital reading
    
    * Default last time back to now
    
    * Move ctd files to archive dir after offload + fix off by 1 bug
    
    * Fix syntax error in gen file
    
    * Update trailing slashes in paths
    
    * Updated debian build to point at 2.y branch
    
    * Updating year for copyright
    
    * Added units and used built in goby file str function
    
    * Updated spacing
    
    ---------
    
    Co-authored-by: Matthew Ferro <matt.ferro@jaia.tech>
    Co-authored-by: Michael Twomey <michael.twomey@jaia.tech>

--- src/bin/hub_manager/hub_manager.cpp
+++ src/bin/hub_manager/hub_manager.cpp
@@ -82,6 +82,7 @@ class HubManager : public ApplicationBase
 
     void intervehicle_subscribe(int bot_id, std::set<jaiabot::protobuf::Link> links);
     void hub2hub_subscribe(int other_hub_id);
+    void handle_ctd_offload_command(const jaiabot::protobuf::CommandForHub& command);
 
     void update_vfleet_shutdown_time()
     {
@@ -807,6 +808,9 @@ void jaiabot::apps::HubManager::handle_command_for_hub(
         case protobuf::CommandForHub::RESTART_ALL_SERVICES:
             interprocess().publish<jaiabot::groups::powerstate_command>(input_command_for_hub);
             break;
+        case protobuf::CommandForHub::CTD_DATA_OFFLOAD:
+            handle_ctd_offload_command(input_command_for_hub);
+            break;
         default: break;
     }
 }
@@ -1105,4 +1109,25 @@ void jaiabot::apps::HubManager::publish_hub2hub_data(jaiabot::protobuf::Hub2HubD
     hub2hub_data->set_time_with_units(goby::time::SystemClock::now<goby::time::MicroTime>());
     intervehicle().publish<jaiabot::groups::hub2hub_data>(
         *hub2hub_data, intervehicle::default_publisher<jaiabot::protobuf::Hub2HubData>);
+}
+
+void jaiabot::apps::HubManager::handle_ctd_offload_command(const jaiabot::protobuf::CommandForHub& command) 
+{
+    std::string bot_ip = cfg().class_b_network() + "." + std::to_string(cfg().fleet_id()) + "." +
+                         std::to_string((cfg().bot_start_ip() + command.scan_for_bot_id()));
+    
+    if (cfg().use_localhost_for_data_offload())
+        bot_ip = "127.0.0.1";
+    
+    std::string offload_command = cfg().ctd_offload_script()  + " -bot_id " + std::to_string(command.scan_for_bot_id()) +
+                                  " -bot_ip " + bot_ip + " 2>&1";
+
+    glog.is_debug1() && glog << "Offload command: " << offload_command << std::endl;
+
+    FILE* pipe = popen(offload_command.c_str(), "r");
+    if (!pipe)
+    {
+        glog.is_warn() && glog << "Error opening pipe to CTD offload command: "
+                                << strerror(errno) << std::endl;
+    }
 }
