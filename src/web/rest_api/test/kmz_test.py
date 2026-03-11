7a526b1217f3aa7b3a0b529442443f0e349790b6
Edward Sanville <edsanville@gmail.com>
2026-03-11 11:45:26 -0400

    Added the kmz endpoint to the APIConfig.APIKey data structures, so it can be used when an API key is required and provided.

--- src/web/rest_api/test/kmz_test.py
+++ src/web/rest_api/test/kmz_test.py
@@ -5,8 +5,12 @@ from jaiabot.messages.jaia_dccl_pb2 import TaskPacket
 
 import requests
 import datetime
+import os
 
 """A pytest script to test the KMZ generation functionality of the REST API."""
+
+API_KEY = os.getenv('JAIA_REST_API_PRIVATE_KEY', "")
+print(f'Using API key: {API_KEY}')
 
 def utime_now():
     return int(datetime.datetime.now().timestamp() * 1e6)
@@ -21,8 +25,10 @@ def test_kmz_generation():
     api_request.target.all = True
     api_request.kmz.start_time = int(now - 7 * 24 * 60 * 60 * 1e6) # 7 days ago
     api_request.kmz.end_time = int(now)
+    api_request.api_key = API_KEY
 
     res = requests.post('http://localhost:9092/jaia/v1', json=MessageToDict(api_request, preserving_proto_field_name=True))
+    assert res.ok, f"Request failed with status code {res.status_code} and message: {res.text}"
     assert res.status_code == 200
     assert isinstance(res.content, bytes)
     assert len(res.content) > 0
@@ -33,3 +39,4 @@ def test_kmz_generation():
 
 if __name__ == "__main__":
     test_kmz_generation()
+
