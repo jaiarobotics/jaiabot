bae7b4d5d5b5b7e0af364df0c495880269cdea7a
Edward Sanville <edsanville@gmail.com>
2026-03-14 12:32:39 -0400

    Fix indenting.

--- src/web/rest_api/v1/api.py
+++ src/web/rest_api/v1/api.py
@@ -72,8 +72,8 @@ def metadata(jaia_request: rest_api.APIRequest) -> rest_api.APIResponse:
     return jaia_response
 
 def task_packets(jaia_request: rest_api.APIRequest) -> rest_api.APIResponse:
-   jaia_response = rest_api.APIResponse()
-   with common.shared_data.data_lock:
+    jaia_response = rest_api.APIResponse()
+    with common.shared_data.data_lock:
         if jaia_request.target.all:
             bot_ids = None
         else:
@@ -87,7 +87,7 @@ def task_packets(jaia_request: rest_api.APIRequest) -> rest_api.APIResponse:
 
         task_packets = common.shared_data.data.get_task_packets(bot_ids, start_time, end_time, mission_names)
         jaia_response.task_packets.packets.extend(task_packets)
-   return jaia_response
+    return jaia_response
 
 def command(jaia_request: rest_api.APIRequest) -> rest_api.APIResponse:
     jaia_response = rest_api.APIResponse()
