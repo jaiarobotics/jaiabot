5d9710710b47d1d0c5cb4d4238c25de695ab99be
Michael Twomey <michael.twomey@jaia.tech>
2026-03-31 14:34:48 -0400

    Create new layout for waypoint panel

--- src/web/components/WaypointPanel/WaypointPanel.tsx
+++ src/web/components/WaypointPanel/WaypointPanel.tsx
@@ -29,13 +29,6 @@ let originalSelectedWaypoint = { ...jaiaGlobal.getSelectedWaypoint() };
 
 /**
  * Displays information about the selected waypoint such as location and task selection
- *
- * @notes
- * Waypoint location data exists in both number and string form. We utilize the number type
- * when saving to the data model and string type when working with user input. We need to use
- * strings when working with user input to allow negative signs and decimal points. As the
- * user enters a coordinate, we will check if the value can be converted to a number.
- * If it can, we will update the data model with the numerical form of the user input.
  */
 export default function WaypointPanel() {
     const jaiaContext = useContext(JaiaContext);
@@ -52,8 +45,6 @@ export default function WaypointPanel() {
         return mission.getWaypoint(jaiaContext.jaiaGlobal.getSelectedWaypoint().waypointNum);
     };
 
-    const [latInput, setLatInput] = useState(getWaypoint().getLocation().lat.toString());
-    const [lonInput, setLonInput] = useState(getWaypoint().getLocation().lon.toString());
     // Use state to initalize to null on first render + prevent unnecessary updates
     const [originalWaypoint, setOriginalWaypoint] = useState(null);
     const isDisabled =
@@ -77,69 +68,6 @@ export default function WaypointPanel() {
             setOriginalWaypoint(cloneDeep(getWaypoint()));
         }
     });
-
-    /**
-     * Compares the lat stored in state and context. If the value in context
-     * is different, the waypoint has moved via a mechanism outsie of the input box
-     * such as "tap to move". The function syncs the two sources.
-     *
-     * @returns {string} Most up to date latitude
-     */
-    const getLatInput = () => {
-        const waypoint = getWaypoint();
-
-        if (isNaN(Number(latInput))) {
-            return latInput;
-        }
-
-        if (waypoint.getLocation().lat !== Number(latInput)) {
-            const updatedLat = waypoint.getLocation().lat.toString();
-            setLatInput(updatedLat);
-            return updatedLat;
-        }
-
-        return latInput;
-    };
-
-    /**
-     * Compares the lon stored in state and context. If the value in context
-     * is different, the waypoint has moved via a mechanism outsie of the input box
-     * such as "tap to move". The function syncs the two sources.
-     *
-     * @returns {string} Most up to date longitude
-     */
-    const getLonInput = () => {
-        const waypoint = getWaypoint();
-
-        if (isNaN(Number(lonInput))) {
-            return lonInput;
-        }
-
-        if (waypoint.getLocation().lon !== Number(lonInput)) {
-            const updatedLon = waypoint.getLocation().lon.toString();
-            setLonInput(updatedLon);
-            return updatedLon;
-        }
-
-        return lonInput;
-    };
-
-    /**
-     * Gets the Bot ID assigned to the mission containing the waypoint
-     *
-     * @returns {string} Bot ID or empty string
-     */
-    const formatBotID = () => {
-        const botID = missionsManager.getBotID(
-            jaiaContext.jaiaGlobal.getSelectedWaypoint().missionID,
-        );
-
-        if (botID === UNASSIGNED_ID) {
-            return "";
-        }
-
-        return botID;
-    };
 
     /**
      * Dispatches action to delete a waypoint
@@ -187,55 +115,6 @@ export default function WaypointPanel() {
     };
 
     /**
-     * Dispatches action to select a task. This will lead to the task
-     * parameters appearing.
-     *
-     * @returns {void}
-     */
-    const handleTaskMenuSelection = (evt: SelectChangeEvent) => {
-        const selectedTaskType = evt.target.value;
-        jaiaDispatch({
-            type: JaiaActions.SELECT_TASK,
-            task: getWaypoint().getTask(),
-            taskType: selectedTaskType,
-        });
-    };
-
-    /**
-     * Updates the local copy of the coordinate on each key stroke. If the
-     * coordinate is a number, the data model and OpenLayers will be updated.
-     *
-     * @param {ChangeEvent} evt Contains the coord type + value
-     * @returns {void}
-     */
-    const handleCoordinateChange = (evt: ChangeEvent<HTMLInputElement>) => {
-        let lat = latInput;
-        let lon = lonInput;
-
-        const value = evt.target.value;
-
-        if (evt.target.name === CoordinateTypes.LAT) {
-            setLatInput(value);
-            lat = value;
-        } else {
-            setLonInput(value);
-            lon = value;
-        }
-
-        if (isNaN(Number(value))) {
-            return;
-        }
-
-        const updatedLatLon = validateCoordinate(lat, lon);
-        setLatInput(updatedLatLon[0]);
-        setLonInput(updatedLatLon[1]);
-        jaiaDispatch({
-            type: JaiaActions.MOVE_WAYPOINT,
-            location: { lat: Number(updatedLatLon[0]), lon: Number(updatedLatLon[1]) },
-        });
-    };
-
-    /**
      * Dispatches action to close the waypoint panel. If the operator
      * selects cancel, a copy of the waypoint made on the inital render
      * of the is passed to the reducer.
@@ -260,24 +139,13 @@ export default function WaypointPanel() {
 
     return (
         <div className="waypoint-panel-container">
-            <div className="waypoint-panel">
-                <div className="label">Wpt:</div>
-                <div className="waypoint-input-container">
-                    <div>{jaiaContext.jaiaGlobal.getSelectedWaypoint().waypointNum}</div>
-                    <Button
-                        className={`jaia-button delete-waypoint ${isDisabled ? "disabled" : ""}`}
-                        onClick={() => handleDeleteWaypointClick()}
-                    >
-                        <Icon path={mdiDelete} title="Delete Waypoint" />
-                    </Button>
-                </div>
-
-                <div className="line-break"></div>
-
-                <div className="label">Bot:</div>
-                <div>{formatBotID()}</div>
-
-                <div className="line-break"></div>
+            <div className="waypoint-id-container">
+                <div>Waypoint:</div>
+                <div>{jaiaContext.jaiaGlobal.getSelectedWaypoint().waypointNum}</div>
+                <div>Mission:</div>
+                <div>{jaiaContext.jaiaGlobal.getSelectedWaypoint().missionID}</div>
+            </div>
+            <div className="waypoint-toggle-container">
                 <div className="toggle-row">
                     <div className="label">Edit Mission:</div>
                     <JaiaToggle
@@ -288,8 +156,6 @@ export default function WaypointPanel() {
                         onClick={() => handleEditModeClick()}
                     />
                 </div>
-
-                <div className="line-break"></div>
                 <div className="toggle-row">
                     <div className="label">Tap to Move:</div>
                     <JaiaToggle
@@ -298,59 +164,11 @@ export default function WaypointPanel() {
                         onClick={() => handleTapToMoveClick()}
                     />
                 </div>
-
-                <div className="line-break"></div>
-
-                <div className="label">Lat:</div>
-                <input
-                    name={CoordinateTypes.LAT}
-                    value={getLatInput()}
-                    className="jaia-input coordinate"
-                    autoComplete="off"
-                    disabled={isDisabled}
-                    onChange={(evt) => handleCoordinateChange(evt)}
-                />
-
-                <div className="label">Lon:</div>
-                <input
-                    name={CoordinateTypes.LON}
-                    value={getLonInput()}
-                    className="jaia-input coordinate"
-                    autoComplete="off"
-                    disabled={isDisabled}
-                    onChange={(evt) => handleCoordinateChange(evt)}
-                />
-
-                <div className="line-break"></div>
-
-                <div className="label">Task:</div>
-                <FormControl sx={{ minWidth: 120 }} size="small">
-                    <Select
-                        value={getWaypoint().getTask().getType()}
-                        onChange={(evt: SelectChangeEvent) => handleTaskMenuSelection(evt)}
-                        disabled={isDisabled}
-                    >
-                        <MenuItem value={TaskType.NONE}>
-                            {snakeCaseToTitleCase(TaskType.NONE)}
-                        </MenuItem>
-                        <MenuItem value={TaskType.DIVE}>
-                            {snakeCaseToTitleCase(TaskType.DIVE)}
-                        </MenuItem>
-                        <MenuItem value={TaskType.SURFACE_DRIFT}>
-                            {snakeCaseToTitleCase(TaskType.SURFACE_DRIFT)}
-                        </MenuItem>
-                        <MenuItem value={TaskType.CONSTANT_HEADING}>
-                            {snakeCaseToTitleCase(TaskType.CONSTANT_HEADING)}
-                        </MenuItem>
-                        <MenuItem value={TaskType.STATION_KEEP}>
-                            {snakeCaseToTitleCase(TaskType.STATION_KEEP)}
-                        </MenuItem>
-                    </Select>
-                </FormControl>
-
-                <div className="task-parameters-container">
-                    <TaskParameters task={getWaypoint().getTask()} isDisabled={isDisabled} />
-                </div>
+            </div>
+            <div className="waypoint-button-container">
+                <button>Location</button>
+                <button>Task</button>
+                <button className="delete-button">Delete</button>
             </div>
             <div className="button-row">
                 <button onClick={() => handleClosePanelClick(PanelActions.CANCEL)}>Cancel</button>
