55912c753af29e57da763f597c1a8a256e514f1a
Edward Sanville <edsanville@gmail.com>
2026-04-14 10:59:41 -0400

    Added functions to recursively collect errors and warnings from a VehicleHealth message, and use those functions to check for battery errors. For battery errors, we issue an EvBatteryLow or EvBatteryCritical event to the state machine.

--- src/bin/mission_manager/events.h
+++ src/bin/mission_manager/events.h
@@ -108,6 +108,10 @@ STATECHART_EVENT(EvIMURestart)
 STATECHART_EVENT(EvIMURestartCompleted)
 STATECHART_EVENT(EvBottomDepthAbort)
 
+// Battery events
+STATECHART_EVENT(EvBatteryCritical)
+STATECHART_EVENT(EvBatteryLow)
+
 STATECHART_EVENT(EvLoop)
 struct EvVehicleDepth : boost::statechart::event<EvVehicleDepth>
 {
