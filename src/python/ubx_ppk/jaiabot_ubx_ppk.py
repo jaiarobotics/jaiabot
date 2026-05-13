ad3b415b5006bd47dd74644d10156a257c7f896a
Edward Sanville <edsanville@gmail.com>
2026-05-13 14:17:02 -0400

    * Add command line parameters to jaiabot_ubx_ppk.py for relaying UBXChunks to the jaiabot_udp_gateway

--- src/python/ubx_ppk/jaiabot_ubx_ppk.py
+++ src/python/ubx_ppk/jaiabot_ubx_ppk.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python3
 
 
+from io import BufferedReader
 import socket
 import logging
 import argparse
+import socket
+import time
+
+from jaiabot.messages.udp_gateway_pb2 import UDPGatewayEnvelope
+from jaiabot.messages.ppk_pb2 import UBXChunk
+
 
 lg = logging.getLogger(__name__)
 logging.basicConfig(format='%(levelname)7s %(message)s', level=logging.INFO)
@@ -72,10 +79,66 @@ class UBXMessageExtractor:
         return messages
 
 
+class GPSDClient:
+    """Simple GPSD client to read raw UBX messages."""
+    sock: socket.socket
+    extractor: UBXMessageExtractor
+
+    def __init__(self, host="127.0.0.1", port=2947):
+        self.sock = socket.create_connection((host, port), timeout=None)
+
+        f = self.sock.makefile("rwb", buffering=1)
+
+        # Read gpsd VERSION banner
+        lg.debug("banner: %s", f.readline().strip())
+
+        # Enable RAW streaming
+        f.write(b'?WATCH={"enable":true,"raw": 2}\n')
+        lg.debug("watch response: %s", f.readline().strip())
+
+        self.extractor = UBXMessageExtractor()
+
+
+    def read_messages(self) -> list[bytes]:
+        """Read from GPSD and extract complete UBX messages."""
+        chunk = self.sock.recv(1024)
+        if not chunk:
+            lg.info("Connection closed by gpsd")
+            return []
+
+        lg.debug(f"Received {len(chunk)} bytes from gpsd")
+        return self.extractor.feed(chunk)
+
+
+class GPSDClientSimulator:
+    """Simulate GPSD by reading from a local file."""
+    extractor: UBXMessageExtractor
+    file: BufferedReader
+    delay: float
+
+    def __init__(self, file_path, delay=1.0):
+        self.extractor = UBXMessageExtractor()
+        self.file = open(file_path, "rb")
+        self.delay = delay
+
+    def read_messages(self) -> list[bytes]:
+        time.sleep(self.delay)
+        chunk = self.file.read(1024)
+        if not chunk:
+            lg.info("End of file reached")
+            return []
+        lg.debug(f"Read {len(chunk)} bytes from file")
+        return self.extractor.feed(chunk)
+
+
 def main():
     parser = argparse.ArgumentParser(description="UBX PPK GPSD Client")
+    parser.add_argument("-host", "--udp_host", type=str, default="localhost", help="The hostname of the jaiabot_udp_gateway to send UBX messages to")
+    parser.add_argument("-port", "--udp_port", type=int, default=20000, help="The port of the jaiabot_udp_gateway to send UBX messages to")
+
     parser.add_argument("-d", "--debug", action="store_true", help="Enable debug logging")
     parser.add_argument("-o", "--output", type=str, help="Output file for raw UBX messages")
+    parser.add_argument("-i", "--input", type=str, help="Simulate GPSD by reading from a local file instead of connecting to GPSD")
 
     args = parser.parse_args()
 
@@ -93,35 +156,37 @@ def main():
         output_file = None
 
 
-    with socket.create_connection((HOST, PORT), timeout=None) as sock:
-        f = sock.makefile("rwb", buffering=1)
+    if args.udp_host and args.udp_port:
+        lg.debug(f"Connecting to UDP gateway at {args.udp_host}:{args.udp_port}")
+        udp_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        udp_dest = (args.udp_host, args.udp_port)
+    else:
+        udp_sock = None
+        udp_dest = None
 
-        # Read gpsd VERSION banner
-        lg.debug("banner: %s", f.readline().strip())
 
-        # Enable JSON streaming
-        f.write(b'?WATCH={"enable":true,"raw": 2}\n')
-        lg.debug("watch response: %s", f.readline().strip())
+    if args.input:
+        gpsd_client = GPSDClientSimulator(args.input)
+    else:
+        gpsd_client = GPSDClient(HOST, PORT)
 
-        extractor = UBXMessageExtractor()
 
-        while True:
-            chunk = f.read(1024)
-            if not chunk:
-                lg.info("Connection closed by gpsd")
-                break
+    while True:
+        messages = gpsd_client.read_messages()
 
-            lg.debug(f"Received {len(chunk)} bytes from gpsd")
-            messages = extractor.feed(chunk)
-            for msg in messages:
-                lg.info(f"Extracted UBX message of length {len(msg)}: {msg[:4].hex()}")
+        for msg in messages:
+            lg.info(f"Extracted UBX message of length {len(msg)}: {msg[:4].hex()}")
 
-                if output_file:
-                    output_file.write(msg)
-                    output_file.flush()
+            if output_file:
+                output_file.write(msg)
+                output_file.flush()
+
+            if udp_sock and udp_dest:
+                lg.debug(f"Sending UBX message of length {len(msg)} to UDP gateway")
+                envelope = UDPGatewayEnvelope(ubx_chunk=UBXChunk(data=msg))
+                udp_sock.sendto(envelope.SerializeToString(), udp_dest)
 
 
 if __name__ == "__main__":
     main()
 
-    
