e6d4c17acf571ebeaa8358f1ffaf0615de12f1a6
Joey Mussalli <mussallijoey@gmail.com>
2026-06-22 10:06:42 -0400

    Add onDeleted() function to Props interface in DeleteMissionSetButton.tsx

--- src/web/components/MissionsPanel/MissionSetStorage/DeleteMissionSetButton/DeleteMissionSetButton.tsx
+++ src/web/components/MissionsPanel/MissionSetStorage/DeleteMissionSetButton/DeleteMissionSetButton.tsx
@@ -1,13 +1,14 @@
 import { useState } from "react";
 import { DialogActions } from "../../../../types/context-types";
+import { deleteFromHub } from "../mission-set-storage";
 import { DisabledCodes } from "./delete-messages";
 import { DeleteMissionSetDialog } from "./DeleteMissionSetDialog";
-import { deleteFromHub } from "../mission-set-storage";
 
 interface Props {
     saveName: string;
     savedNames: string[];
     clearSaveName: () => void;
+    onDeleted: () => void;
 }
 
 /**
@@ -47,8 +48,10 @@ export default function DeleteMissionSetButton(props: Props) {
     const onDialogClose = (dialogAction: DialogActions) => {
         setIsDialogVisible(false);
         if (dialogAction === DialogActions.CONFIRMED) {
-            deleteFromHub(props.saveName);
-            props.clearSaveName();
+            deleteFromHub(props.saveName.trim()).then(() => {
+                props.clearSaveName();
+                props.onDeleted();
+            });
         }
     };
 
