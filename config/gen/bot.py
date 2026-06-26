f5d57510e01ba27f00e474bc25099cec64a912be
Matthew Ferro <109534520+ferrom-jaia@users.noreply.github.com>
2026-06-26 13:24:22 -0400

    task/use-vin-instead-of-bot-and-fleet-number-for-bot-db/SW-2467 (#1612)
    Added bot vin to the motor usage output and we are now using it to name the sql db file

--- config/gen/bot.py
+++ config/gen/bot.py
@@ -16,6 +16,7 @@ jaia_imu_type='bno055'
 jaia_arduino_type='spi'
 jaia_pam_connection_type='none'
 jaia_tail_serial_number = os.environ.get('jaia_tail_serial_number', default='unknown_serial_number')
+jaia_bot_vin = os.environ.get('jaia_bot_vin', default='unknown_vin')
 
 if "jaia_electronics_stack" in os.environ:
     jaia_electronics_stack=os.environ['jaia_electronics_stack']
@@ -284,7 +285,8 @@ elif common.app == 'jaiabot_health':
                                      ignore_powerstate_changes=ignore_powerstate_changes,
                                      is_in_sim=is_simulation(),
                                      motor_harness_type=jaia_motor_harness_type,
-                                     jaia_tail_serial_number=jaia_tail_serial_number))
+                                     jaia_tail_serial_number=jaia_tail_serial_number,
+                                     jaia_bot_vin=jaia_bot_vin))
 elif common.app == 'goby_logger':    
     print(config.template_substitute(templates_dir+'/goby_logger.pb.cfg.in',
                                      app_block=app_common,
