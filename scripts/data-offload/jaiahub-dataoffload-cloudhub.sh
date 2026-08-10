dfc3bdb2d5adddc54c4457837d3d0de8dde37ed7
Toby Schneider <tsaubergine@users.noreply.github.com>
2026-08-10 15:02:28 +1000

    Refactor CLOUDHUB_IP assignment in dataoffload script
    Removed hardcoded CLOUDHUB_ID and updated CLOUDHUB_IP assignment to use jaia_ip command.

--- scripts/data-offload/jaiahub-dataoffload-cloudhub.sh
+++ scripts/data-offload/jaiahub-dataoffload-cloudhub.sh
@@ -5,9 +5,7 @@ set -e
 set -a
 source /etc/jaiabot/runtime.env
 set +a
-
-CLOUDHUB_ID=30
-CLOUDHUB_IP=$(jaia_ip --query_type addr --node_type hub --ip_net cloudhub_vpn --fleet_id ${jaia_fleet_id} --node_id ${CLOUDHUB_ID} --ip_version ipv6)
+CLOUDHUB_IP=$(jaia_ip chf${jaia_fleet_id})
 
 # use non-s3fs temporary directory
 rsync -r -c --temp-dir=/var/log/jaiabot --info=progress2 --no-inc-recursive --timeout=15 /var/log/jaiabot/bot_offload/ jaia@[${CLOUDHUB_IP}]:/var/log/jaiabot/bot_offload/
